e244242e9ccd1c37295bea5aec7033cd3e10ff93
mmaloney <>
2014-08-29 18:17:32 +0000

    First working version for 6.0 to 6.1. Tested under Opendcs PG and Oracle.

--- src/opendcs/dbupdate/DbUpdate.java
+++ src/opendcs/dbupdate/DbUpdate.java
@@ -4,12 +4,15 @@ import ilex.util.EnvExpander;
 import ilex.util.Logger;
 
 import java.io.Console;
+import java.io.FileReader;
+import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Properties;
 
 import decodes.db.Database;
 import decodes.sql.DecodesDatabaseVersion;
+import decodes.sql.SqlDatabaseIO;
 import decodes.tsdb.BadConnectException;
 import decodes.tsdb.TsdbAppTemplate;
 import decodes.tsdb.TsdbDatabaseVersion;
@@ -46,46 +49,76 @@ public class DbUpdate extends TsdbAppTemplate
 		{
 			System.out.println("TSDB Database is currently " + theDb.getTsdbVersion());
 			System.out.println("DECODES Database is currently " + theDb.getDecodesDatabaseVersion());
-			sql("ALTER TABLE NETWORKLISTENTRY ADD COLUMN PLATFORM_NAME VARCHAR(24)");
-			sql("ALTER TABLE NETWORKLISTENTRY ADD COLUMN DESCRIPTION VARCHAR(80)");
+			if (!theDb.isOracle())
+			{
+				sql("ALTER TABLE NETWORKLISTENTRY ADD COLUMN PLATFORM_NAME VARCHAR(24)");
+				sql("ALTER TABLE NETWORKLISTENTRY ADD COLUMN DESCRIPTION VARCHAR(80)");
+			}
+			else
+			{
+				sql("ALTER TABLE NETWORKLISTENTRY ADD PLATFORM_NAME VARCHAR2(24)");
+				sql("ALTER TABLE NETWORKLISTENTRY ADD DESCRIPTION VARCHAR2(80)");
+			}
 
-			String schemaDir = EnvExpander.expand("$DCSTOOL_HOME/schema/")
-				+ (theDb.isCwms() ? "cwms30" : theDb.isOracle() ? "opendcs-oracle" : "opendcs-pg");
+			String schemaDir = EnvExpander.expand("$DCSTOOL_HOME/schema")
+				+ (theDb.isCwms() ? "/cwms30" : 
+				   theDb.isOracle() ? "/opendcs-oracle" : "/opendcs-pg");
+			System.out.println("Schema dir is '" + schemaDir + "'");
+			
+			// For Oracle, we'll need the table space originally defined on db creation.
+			String tableSpaceSpec = null;
+			if (theDb.isOracle())
+			{
+				LineNumberReader lnr = new LineNumberReader(
+					new FileReader(schemaDir + "/defines.sh"));
+				String line;
+				while((line = lnr.readLine()) != null)
+				{
+					if (line.indexOf("TBL_SPACE_DATA=") >= 0)
+					{
+						tableSpaceSpec = 
+							"tablespace " + line.substring(line.indexOf('=') + 1).trim();
+						break;
+					}
+				}
+				lnr.close();
+			}
 
 			if (!theDb.isCwms()) // CWMS doesn't support DCP Monitor Schema
 			{
-				SQLReader sqlReader = new SQLReader(schemaDir + "dcp_trans_expanded.sql");
+				SQLReader sqlReader = new SQLReader(schemaDir + "/dcp_trans_expanded.sql");
 				ArrayList<String> queries = sqlReader.createQueries();
 				for(String q : queries)
+				{
+					if (tableSpaceSpec != null)
+					{
+						// Oracle will have a table space definition that we need to substitute.
+						int tblSpecIdx = q.indexOf("&TBL_SPACE_SPEC");
+						if (tblSpecIdx != -1 && tableSpaceSpec != null)
+							q = q.substring(0, tblSpecIdx) + tableSpaceSpec;
+					}
 					sql(q);
+				}
 			}
 			
 			// The DACQ_EVENT table was not used before 11. Drop it and recreate below.
 			sql("DROP TABLE DACQ_EVENT");
 			
-			// For Oracle, we'll need the table space originally defined on db creation.
-			String tableSpaceSpec = null;
-			SQLReader sqlReader = new SQLReader(schemaDir + "defines.sql");
-			ArrayList<String> queries = sqlReader.createQueries();
-			for(String q : queries)
-				if (q.toUpperCase().startsWith("DEFINE TBL_SPACE_SPEC"))
-				{
-					tableSpaceSpec = q.substring(q.indexOf('\'') + 1);
-					tableSpaceSpec = tableSpaceSpec.substring(0, q.indexOf('\''));
-					break;
-				}
 			
-			sqlReader = new SQLReader(schemaDir + "opendcs.sql");
-			queries = sqlReader.createQueries();
+			SQLReader sqlReader = new SQLReader(schemaDir + "/opendcs.sql");
+			ArrayList<String> queries = sqlReader.createQueries();
 			for(String q : queries)
 				if (q.contains("CP_COMPOSITE_") // New tables CP_COMPOSITE_DIAGRAM and CP_COMPOSITE_MEMBER
 				 || q.contains("DACQ_EVENT")    // Modified & previously unused DACQ_EVENT was dropped above
 				 || q.contains("SERIAL_PORT_STATUS")) // New table for serial port status
 				{
-					// Oracle will have a table space definition that we need to substitute.
-					int tblSpecIdx = q.indexOf("&TBL_SPACE_SPEC");
-					if (tblSpecIdx != -1 && tableSpaceSpec != null)
-						q = q.substring(0, tblSpecIdx) + tableSpaceSpec;
+					if (tableSpaceSpec != null)
+					{
+						// Oracle will have a table space definition that we need to substitute.
+						int tblSpecIdx = q.indexOf("&TBL_SPACE_SPEC");
+						if (tblSpecIdx != -1 && tableSpaceSpec != null)
+							q = q.substring(0, tblSpecIdx) + tableSpaceSpec;
+					}
 					sql(q);
 				}
 		
@@ -122,11 +155,15 @@ public class DbUpdate extends TsdbAppTemplate
 
 
 			// Update DECODES Database Version
-			sql("UPDATE DECODESDATABASEVERSION SET VERSION_NUM = " + DecodesDatabaseVersion.DECODES_DB_11);
+			sql("UPDATE DECODESDATABASEVERSION SET VERSION_NUM = " 
+				+ DecodesDatabaseVersion.DECODES_DB_11);
 			theDb.setDecodesDatabaseVersion(DecodesDatabaseVersion.DECODES_DB_11, "");
+			((SqlDatabaseIO)Database.getDb().getDbIo()).setDecodesDatabaseVersion(
+				DecodesDatabaseVersion.DECODES_DB_11, "");
 			// Update TSDB_DATABASE_VERSION.
 			String desc = "Updated on " + new Date();
-			sql("UPDATE TSDB_DATABASE_VERSION SET DB_VERSION = " + TsdbDatabaseVersion.VERSION_10
+			sql("UPDATE TSDB_DATABASE_VERSION SET DB_VERSION = " 
+				+ TsdbDatabaseVersion.VERSION_10
 				+ ", DESCRIPTION = '" + desc + "'");
 			theDb.setTsdbVersion(TsdbDatabaseVersion.VERSION_10, desc);
 			Database.getDb().networkListList.write();
