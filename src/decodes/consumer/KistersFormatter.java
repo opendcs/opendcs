0b3a41dbfb6fd5b702c1d40e036c4ebdbd3035ff
mmaloney <>
2014-09-15 13:55:32 +0000

    Updates for AESRD

--- src/decodes/consumer/KistersFormatter.java
+++ src/decodes/consumer/KistersFormatter.java
@@ -2,6 +2,9 @@
  * $Id$
  * 
  * $Log$
+ * Revision 1.4  2014/08/22 17:23:10  mmaloney
+ * 6.1 Schema Mods and Initial DCP Monitor Implementation
+ *
  * Revision 1.3  2014/05/30 13:15:33  mmaloney
  * dev
  *
@@ -43,6 +46,7 @@ import decodes.db.SiteName;
 import decodes.decoder.DecodedMessage;
 import decodes.decoder.Sensor;
 import decodes.decoder.TimeSeries;
+import decodes.util.PropertySpec;
 
 /**
  * This class formats a DCP message for ingest into Kisters' WISKI database
@@ -68,11 +72,17 @@ public class KistersFormatter
 	/** Can be set by "RINVAL" property. */
 	private String RINVAL = "-777";
 	/** Can be set by "SiteNameType" property */
-	private String siteNameType = Constants.snt_local;
+	private String siteNameType = Constants.snt_NWSHB5;
+	/** Can be set by "SiteNameTypeAlt" property */
+	private String siteNameTypeAlt = Constants.snt_local;
 	/** Can be set by "DataTypeStandard" property */
 	private String dataTypeStandard = Constants.datatype_SHEF;
+	/** Can be set by "DataTypeStandardAlt" property */
+	private String dataTypeStandardAlt = null;
 	/** Can be set by "SitePrefix" property */
 	private String sitePrefix = "";
+	/** Can be set by "includeLayout" property */
+	private boolean includeLayout = false;
 	
 	public static final String headerDelim = "|*|";
 	
@@ -119,15 +129,24 @@ public class KistersFormatter
 		s = PropertiesUtil.getIgnoreCase(rsProps, "SiteNameType");
 		if (s != null)
 			siteNameType = s;
+		s = PropertiesUtil.getIgnoreCase(rsProps, "SiteNameTypeAlt");
+		if (s != null)
+			siteNameTypeAlt = s;
 		s = PropertiesUtil.getIgnoreCase(rsProps, "DataTypeStandard");
 		if (s != null)
 			dataTypeStandard = s;
+		s = PropertiesUtil.getIgnoreCase(rsProps, "DataTypeStandardAlt");
+		if (s != null)
+			dataTypeStandardAlt = s;
 		s = PropertiesUtil.getIgnoreCase(rsProps, "SitePrefix");
 		if (s != null)
 			sitePrefix = s;
 		s = PropertiesUtil.getIgnoreCase(rsProps, "tzName");
 		if (s != null)
 			tzName = s;
+		s = PropertiesUtil.getIgnoreCase(rsProps, "includeLayout");
+		if (s != null)
+			includeLayout = TextUtil.str2boolean(s);
 	}
 
 	@Override
@@ -179,16 +198,14 @@ public class KistersFormatter
 			
 			SiteName siteName = site.getName(siteNameType);
 			if (siteName == null)
-				siteName = sensorSite.getPreferredName();
+				if ((siteName = site.getName(siteNameTypeAlt)) == null)
+					siteName = sensorSite.getPreferredName();
 			
 			DataType dt = sensor.getDataType(dataTypeStandard);
 			if (dt == null)
-			{
-				dt = sensor.getDataType();
-				DataType ndt = dt.findEquivalent(dataTypeStandard);
-				if (ndt != null)
-					dt = ndt;
-			}
+				if (dataTypeStandardAlt == null
+					|| (dt = sensor.getDataType(dataTypeStandardAlt)) == null)
+					dt = sensor.getDataType();
 
 			StringBuilder headerLine = new StringBuilder();
 			headerLine.append("#REXCHANGE");
@@ -223,6 +240,8 @@ public class KistersFormatter
 			}
 				
 			consumer.println(headerLine.toString());
+			if (includeLayout)
+				consumer.println("#LAYOUT(timestamp,value)");
 
 			ts.sort();
 			for(int idx=0; idx<ts.size(); idx++)
@@ -240,5 +259,52 @@ public class KistersFormatter
 			}
 		}
 		consumer.endMessage();
+	}
+	
+	protected PropertySpec kfPropSpecs[] = 
+	{
+		new PropertySpec("includeTZ", PropertySpec.BOOLEAN, 
+			"Default=true. Set to false to exclude TZ specifier from the header."),
+		new PropertySpec("tzName", PropertySpec.STRING, 
+			"Default=null. Set to override the TZ name in the routing spec."
+			+ " Caution: this will not change the time values, just the name in the header."),
+		new PropertySpec("includeCNAME", PropertySpec.BOOLEAN, 
+			"Default=false. Set to true to include CNAME in the header with the DECODES sensor name."),
+		new PropertySpec("includeCUNIT", PropertySpec.BOOLEAN, 
+			"Default=false. Set to true to include CUNIT in the header with the " +
+			"engineering units."),
+		new PropertySpec("includeRINVAL", PropertySpec.BOOLEAN, 
+			"Default=true. Set to false to exclude RINVAL from the header. " +
+			"RINVAL specifies the special value to be used for missing or erroneous values."),
+		new PropertySpec("RINVAL", PropertySpec.STRING, 
+			"Default=-777. Specifies a special value to be used for missing or erroneous " +
+			"values in the output data."),
+		new PropertySpec("SiteNameType", PropertySpec.DECODES_ENUM + Constants.enum_SiteName,
+			"Specifies which DECODES site name to use in the header."),
+		new PropertySpec("SiteNameTypeAlt", PropertySpec.DECODES_ENUM + Constants.enum_SiteName,
+			"Specifies the alternate DECODES site name to use in the header " +
+			"(if the primary is undefined)."),
+		new PropertySpec("DataTypeStandard", PropertySpec.DECODES_ENUM + Constants.enum_DataTypeStd,
+			"Specifies which DECODES data type to use in the header."),
+		new PropertySpec("DataTypeStandardAlt", PropertySpec.DECODES_ENUM + Constants.enum_DataTypeStd,
+			"Specifies the alternate DECODES data type to use in the header if the primary" +
+			" is undefined."),
+		new PropertySpec("SitePrefix", PropertySpec.STRING, 
+			"A constant string to be placed before the site name in REXCHANGE values."),
+		new PropertySpec("includeLayout", PropertySpec.BOOLEAN, 
+			"Default=true. Set to true to include a separate header line with layout.")
+	};
+	
+	@Override
+	public PropertySpec[] getSupportedProps()
+	{
+		return kfPropSpecs;
+	}
+
+
+	@Override
+	public boolean additionalPropsAllowed()
+	{
+		return false;
 	}
 }
