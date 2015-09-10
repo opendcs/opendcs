83278a417d25d26f4229a188e91b455b4abe3e31
mmaloney <>
2015-09-10 21:17:35 +0000

    Dev

---
+++ src/decodes/cwms/validation/dao/TsidScreeningAssignment.java
@@ -0,0 +1,38 @@
+package decodes.cwms.validation.dao;
+
+import decodes.cwms.validation.Screening;
+import decodes.tsdb.TimeSeriesIdentifier;
+
+public class TsidScreeningAssignment
+{
+	private TimeSeriesIdentifier tsid;
+	private Screening screening;
+	private boolean active;
+	
+	public TsidScreeningAssignment(TimeSeriesIdentifier tsid, Screening screening, boolean active)
+	{
+		this.tsid = tsid;
+		this.screening = screening;
+		this.active = active;
+	}
+	
+	public TimeSeriesIdentifier getTsid()
+	{
+		return tsid;
+	}
+
+	public Screening getScreening()
+	{
+		return screening;
+	}
+
+	public boolean isActive()
+	{
+		return active;
+	}
+
+	public void setTsid(TimeSeriesIdentifier tsid)
+	{
+		this.tsid = tsid;
+	}
+}
