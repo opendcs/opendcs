6eed7a58e3f0c488b7f9a3699f8808c7356d2c41
mmaloney <>
2016-09-23 15:56:20 +0000

    Add -f appname argument.

--- src/decodes/tsdb/ShowCompDepends.java
+++ src/decodes/tsdb/ShowCompDepends.java
@@ -2,6 +2,9 @@
  * $Id$
  * 
  * $Log$
+ * Revision 1.5  2016/06/07 22:00:51  mmaloney
+ * Refactoring for efficiency, particularly with HDB.
+ *
  * Revision 1.4  2014/10/07 13:03:35  mmaloney
  * dev
  *
@@ -21,6 +24,8 @@ import opendcs.dai.ComputationDAI;
 import opendcs.dai.LoadingAppDAI;
 import opendcs.dai.TimeSeriesDAI;
 import lrgs.gui.DecodesInterface;
+import ilex.cmdline.StringToken;
+import ilex.cmdline.TokenOptions;
 import ilex.util.Logger;
 import decodes.db.Constants;
 import decodes.db.Database;
@@ -37,13 +42,9 @@ TS_Key(int)   "TS_ID(Unique String)"  COMPUTATION_ID(int) "comp name" Loading_Ap
 */
 public class ShowCompDepends extends TsdbAppTemplate
 {
-	// Local caches for computations, groups, cp_comp_depends:
-//	private ArrayList<DbComputation> compList = new ArrayList<DbComputation>();
-//	private ArrayList<CpCompDependsRecord> cpCompDependsTable = new ArrayList<CpCompDependsRecord>();
-//	ArrayList<CompAppInfo> loadingApps = new ArrayList<CompAppInfo>();
-
-
-//	private StringToken grpNameArg;
+	/** Application name - used to determine LOADING_APPLICATION_ID. */
+	protected StringToken appFilterArg  = new StringToken("f", "Application-Name (for filter)", "",
+		TokenOptions.optSwitch, "");;
 
 	public ShowCompDepends()
 	{
@@ -55,9 +56,7 @@ public class ShowCompDepends extends TsdbAppTemplate
 	 */
 	protected void addCustomArgs(CmdLineArgs cmdLineArgs)
 	{
-//		grpNameArg = new StringToken("", "Group Name", "", 
-//			TokenOptions.optArgument, null);
-//		cmdLineArgs.addToken(grpNameArg);
+		cmdLineArgs.addToken(appFilterArg);
 	}
 
 	public static void main(String args[])
@@ -77,6 +76,7 @@ public class ShowCompDepends extends TsdbAppTemplate
 		String joins = "a.computation_id = b.computation_id"
 			+ " and b.loading_application_id = c.loading_application_id";
 		
+		
 		if (theDb.isHdb())
 		{
 			tables = tables + ", cp_ts_id d, hdb_site_datatype e";
@@ -89,6 +89,10 @@ public class ShowCompDepends extends TsdbAppTemplate
 			joins = joins + " and a.ts_id = d.ts_code";
 			columns = columns + ", d.cwms_ts_id";
 		}
+		
+		if (appFilterArg.getValue().length() > 0)
+			joins = joins + " and lower(c.loading_application_name) = '"
+				+ appFilterArg.getValue().toLowerCase() + "'";
 		
 		String q = "select " + columns + " from " + tables + " where " + joins;
 		ResultSet rs = theDb.doQuery(q);
@@ -123,33 +127,6 @@ public class ShowCompDepends extends TsdbAppTemplate
 				+ appId + ",  " + "\"" + appName + "\"");
 		}
 
-//		fillLists();
-//		
-//		TimeSeriesDAI timeSeriesDAO = theDb.makeTimeSeriesDAO();
-//		for(CpCompDependsRecord ccdr : cpCompDependsTable)
-//		{
-//			DbKey tsKey = ccdr.getTsKey();
-//			TimeSeriesIdentifier tsid = timeSeriesDAO.getTimeSeriesIdentifier(tsKey);
-//			DbKey compId = ccdr.getCompId();
-//			DbComputation comp = getComp(compId);
-//			String compName = "Inaccessible";
-//			DbKey appId = Constants.undefinedId;
-//			String appName = "Inaccessible";
-//			if (comp != null)
-//			{
-//				compName = comp.getName();
-//				appId = comp.getAppId();
-//				CompAppInfo cai = getApp(appId);
-//				if (cai != null)
-//					appName = cai.getAppName();
-//			}
-//			
-//			System.out.println("" + tsKey + ",    "
-//				+ (tsid == null ? "null" : ("\""+tsid.getUniqueString()+"\"")) + ",   "
-//				+ compId + ",  " + "\"" + compName + "\"" + ",  "
-//				+ appId + ",  " + "\"" + appName + "\"");
-//		}
-//		timeSeriesDAO.close();
 	}
 
 	@Override
@@ -161,62 +138,6 @@ public class ShowCompDepends extends TsdbAppTemplate
 		db.siteList.read();
 	}
 
-	
-	
-	
-//	private void fillLists()
-//	{
-//		LoadingAppDAI loadingAppDao = theDb.makeLoadingAppDAO();
-//		ComputationDAI computationDAO = theDb.makeComputationDAO();
-//		TimeSeriesDAI timeSeriesDAO = theDb.makeTimeSeriesDAO();
-//
-//		try
-//		{
-//			info("filling TSID Cache...");
-//			timeSeriesDAO.reloadTsIdCache();
-//			
-//			info("filling Computation Cache...");
-//			
-//			
-//			
-//			List<String> compNames = loadingAppDao.listComputationsByApplicationId(
-//				Constants.undefinedId, false);
-//			for(String nm : compNames)
-//			{
-//				try
-//				{
-//					DbComputation comp = computationDAO.getComputationByName(nm);
-//					compList.add(comp);
-//				}
-//				catch (NoSuchObjectException ex)
-//				{
-//					warning("Computation '" + nm 
-//						+ "' could not be read: " + ex);
-//				}
-//			}
-//			info("After loading, " + compList.size()
-//				+ " computations in cache.");
-//
-//			info("Reloading CP_COMP_DEPENDS Cache...");
-//			loadCpCompDependsCache();
-//			loadingApps = loadingAppDao.listComputationApps(false);
-//
-//		}
-//		catch (Exception ex)
-//		{
-//			String msg = "Error refreshing caches: " + ex;
-//			Logger.instance().failure(msg);
-//			System.err.println(msg);
-//			ex.printStackTrace(System.err);
-//		}
-//		finally
-//		{
-//			timeSeriesDAO.close();
-//			loadingAppDao.close();
-//			computationDAO.close();
-//		}
-//	}
-	
 	private void info(String x)
 	{
 		Logger.instance().info("ShowCompDepends: " + x);
@@ -225,55 +146,4 @@ public class ShowCompDepends extends TsdbAppTemplate
 	{
 		Logger.instance().debug3("ShowCompDepends: " + x);
 	}
-
-//	/**
-//	 * Flush the cache and then load all the CP_COMP_DEPENDS records
-//	 * for my appId.
-//	 */
-//	private void loadCpCompDependsCache()
-//	{
-//		cpCompDependsTable.clear();
-//		
-//		CompDependsDAI compDependsDAO = theDb.makeCompDependsDAO();
-//	
-//		
-//		String tsIdCol = 
-//			theDb.isHdb() || theDb.getTsdbVersion() >= TsdbDatabaseVersion.VERSION_9 
-//			? "TS_ID" : "SITE_DATATYPE_ID";
-//		String q = "SELECT " + tsIdCol + ", COMPUTATION_ID FROM CP_COMP_DEPENDS ORDER BY "
-//				+ tsIdCol;
-//		
-//		try
-//		{
-//			ResultSet rs = theDb.doQuery(q);
-//			while (rs != null && rs.next())
-//			{
-//				CpCompDependsRecord rec = new CpCompDependsRecord(
-//					DbKey.createDbKey(rs, 1), DbKey.createDbKey(rs, 2));
-//				cpCompDependsTable.add(rec);
-//			}
-//		}
-//		catch (Exception ex)
-//		{
-//			warning("Error in query '" + q + "': " + ex);
-//			return;
-//		}
-//	}
-//	
-//	private DbComputation getComp(DbKey id)
-//	{
-//		for(DbComputation dc : compList)
-//			if (id.equals(dc.getId()))
-//				return dc;
-//		return null;
-//	}
-//	
-//	private CompAppInfo getApp(DbKey id)
-//	{
-//		for(CompAppInfo cai : loadingApps)
-//			if (id.equals(cai.getAppId()))
-//				return cai;
-//		return null;
-//	}
-
 }
