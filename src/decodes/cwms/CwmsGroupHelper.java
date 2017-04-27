2cd716006dc97fa704ee89b6a62ce2948976c8a0
mmaloney <>
2017-04-27 21:01:55 +0000

    Combine full/base/sub location/param/version with logical OR.

--- src/decodes/cwms/CwmsGroupHelper.java
+++ src/decodes/cwms/CwmsGroupHelper.java
@@ -8,6 +8,9 @@
 * Open Source Software
 * 
 * $Log$
+* Revision 1.5  2017/04/19 19:23:35  mmaloney
+* CWMS-10609 nested group evaluation in group editor bugfix.
+*
 * Revision 1.4  2017/01/10 21:14:43  mmaloney
 * Enhanced wildcard processing for CWMS as per punchlist for comp-depends project
 * for NWP.
@@ -303,16 +306,26 @@ public class CwmsGroupHelper
 
 		// User could enter an actual site plus another site with wildcard, then
 		// I would have one actual siteId and one pattern.
-		if (siteIds.size() > 0 || fullLocPatterns.size() > 0)
+		if (siteIds.size() > 0             // Fully specified sites in the group (the old way).
+		 || fullLocPatterns.size() > 0     // A full location spec with a wildcard
+		 || baseLocPatterns.size() > 0     // A base location spec, possibly with wildcard
+		 || subLocPatterns.size() > 0)     // A sub location spec, possibly with wildcard
 		{
 			boolean passedSite = false;
-			for(DbKey siteId : siteIds)
-				if (tsid.getSite() != null && siteId.equals(tsid.getSite().getId()))
-				{
-					passedSite = true;
-					break;
-				}
-			if (!passedSite)
+			
+			// Check direct site (Location) links.
+			if (siteIds.size() > 0 || fullLocPatterns.size() > 0)
+			{
+				for(DbKey siteId : siteIds)
+					if (tsid.getSite() != null && siteId.equals(tsid.getSite().getId()))
+					{
+						passedSite = true;
+						break;
+					}
+			}
+			
+			// If no match, check location with wildcard spec.
+			if (!passedSite && fullLocPatterns.size() > 0)
 			{
 				String tsidfullLoc = ctsid.getPart("location");
 				if (tsidfullLoc != null && tsidfullLoc.length() > 0)
@@ -330,57 +343,132 @@ public class CwmsGroupHelper
 				}
 			}
 			
+			// Still no match, check base location specs, possibly with wildcards.
+			if (!passedSite && baseLocPatterns.size() > 0)
+			{			
+				String tsidbaseLoc = ctsid.getBaseLoc();
+				if (tsidbaseLoc != null && tsidbaseLoc.length() > 0)
+				{
+					tsidbaseLoc = tsidbaseLoc.toUpperCase();
+					for(Pattern slp : baseLocPatterns)
+					{
+						Matcher m = slp.matcher(tsidbaseLoc);
+						if (m.matches())
+						{
+							passedSite = true;
+							break;
+						}
+					}
+				}
+			}
+			
+			// Still no match, check sub location specs.
+			if (!passedSite && subLocPatterns.size() > 0)
+			{			
+				String tsidSubLoc = ctsid.getSubLoc();
+				if (tsidSubLoc != null && tsidSubLoc.length() > 0)
+				{
+					tsidSubLoc = tsidSubLoc.toUpperCase();
+					for(Pattern slp : subLocPatterns)
+					{
+						Matcher m = slp.matcher(tsidSubLoc);
+						if (m.matches())
+						{
+							passedSite = true;
+							break;
+						}
+					}
+				}
+			}
+				
 			if (!passedSite)
-				return false;
+				return false; // None of the ways of referencing a location match.
 			matches++;
 		}
 
 		ArrayList<DbKey> dtIds = tsGroup.getDataTypeIdList();
-		if (dtIds.size() > 0 || fullParamPatterns.size() > 0)
+		if (dtIds.size() > 0                 // Full data type (CWMS Param) specified
+		 || fullParamPatterns.size() > 0     // Full Param spec possibly with wildcard
+		 || baseParamPatterns.size() > 0     // Base Param spec possibly with wildcard
+		 || subParamPatterns.size() > 0)     // Sub Param spec possibly with wildcard
 		{
 			boolean passedDT = false;
+			
+			String tsidFullParam = ctsid.getPart("param");
+			if (tsidFullParam == null || tsidFullParam.trim().length() == 0)
+			{
+				Logger.instance().warning(module + " invalid TSID '" + ctsid.getUniqueString() 
+					+ "' has no param part.");
+				return false;
+			}
+
+			// Do a text compare of the param to the data types in the list. We have
+			// seen cases where multiple datatypes differ only in case but have different
+			// surrogate keys.
 			for(DbKey dtId : dtIds)
-				if (tsid.getDataTypeId() != null && dtId.equals(tsid.getDataTypeId()))
+			{
+				DataType dt = DataType.getDataType(dtId);
+				if (dt != null && dt.getCode().equalsIgnoreCase(tsidFullParam))
 				{
 					passedDT = true;
 					break;
 				}
-			// Extra check for Param: There may be multiple data types with the
-			// same code for legacy reasons.
-			if (!passedDT)
+			}
+
+			// No match for data type, check full param with wildcard specs.
+			if (!passedDT && fullParamPatterns.size() > 0)
 			{
-				DataType tsidDt = ctsid.getDataType();
-				if (tsidDt != null)
-					for(DbKey dtId : dtIds)
+				tsidFullParam = tsidFullParam.toUpperCase();
+				for(Pattern sp : fullParamPatterns)
+				{
+					Matcher m = sp.matcher(tsidFullParam);
+					if (m.matches())
 					{
-						DataType dt = DataType.getDataType(dtId);
-						if (dt != null && dt.getCode().equalsIgnoreCase(tsidDt.getCode()))
+						passedDT = true;
+						break;
+					}
+				}
+			}
+			
+			// Still no match. Check base param with wildcard.
+			if (!passedDT && baseParamPatterns.size() > 0)
+			{			
+				String tsidbase = ctsid.getBaseParam();
+				if (tsidbase != null && tsidbase.length() > 0)
+				{
+					tsidbase = tsidbase.toUpperCase();
+					for(Pattern sp : baseParamPatterns)
+					{
+						Matcher m = sp.matcher(tsidbase);
+						if (m.matches())
 						{
 							passedDT = true;
 							break;
 						}
 					}
-				
-				if (!passedDT && fullParamPatterns.size() > 0)
+				}
+			}
+
+			// Still no match. Check sub param with wildcard.
+			if (!passedDT && subParamPatterns.size() > 0)
+			{			
+				String tsidSub = ctsid.getSubParam();
+				if (tsidSub != null && tsidSub.length() > 0)
 				{
-					// fullParam can use regex to handle wildcards
-					String tsidfull = ctsid.getPart("param");
-					if (tsidfull != null && tsidfull.length() > 0)
+					tsidSub = tsidSub.toUpperCase();
+					for(Pattern sp : subParamPatterns)
 					{
-						tsidfull = tsidfull.toUpperCase();
-						for(Pattern sp : fullParamPatterns)
+						Matcher m = sp.matcher(tsidSub);
+						if (m.matches())
 						{
-							Matcher m = sp.matcher(tsidfull);
-							if (m.matches())
-							{
-								passedDT = true;
-								break;
-							}
+							passedDT = true;
+							break;
 						}
 					}
 				}
 			}
-	
+
+			// No match on any of the ways of specifying param.
 			if (!passedDT)
 				return false;
 			matches++;
@@ -432,163 +520,69 @@ public class CwmsGroupHelper
 			matches++;
 		}
 
-		// Subloc uses regex to handle wildcards
-		if (subLocPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidSubLoc = ctsid.getSubLoc();
-			if (tsidSubLoc != null && tsidSubLoc.length() > 0)
-			{
-				tsidSubLoc = tsidSubLoc.toUpperCase();
-				for(Pattern slp : subLocPatterns)
-				{
-					Matcher m = slp.matcher(tsidSubLoc);
-					if (m.matches())
-					{
-						passed = true;
-						break;
-					}
-				}
-			}
-			if (!passed)
-				return false;
-			matches++;
-		}
-		
-		// baseloc uses regex to handle wildcards
-		if (baseLocPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidbaseLoc = ctsid.getBaseLoc();
-			if (tsidbaseLoc != null && tsidbaseLoc.length() > 0)
-			{
-				tsidbaseLoc = tsidbaseLoc.toUpperCase();
-				for(Pattern slp : baseLocPatterns)
-				{
-					Matcher m = slp.matcher(tsidbaseLoc);
-					if (m.matches())
-					{
-						passed = true;
-						break;
-					}
-				}
-			}
-			if (!passed)
-				return false;
-			matches++;
-		}
+		if (fullVersionPatterns.size() > 0
+		 || subVersionPatterns.size() > 0
+		 || baseVersionPatterns.size() > 0)
+		{
+			boolean versionPassed = false;
 
-		// SubParam uses regex to handle wildcards
-		if (subParamPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidSub = ctsid.getSubParam();
-			if (tsidSub != null && tsidSub.length() > 0)
-			{
-				tsidSub = tsidSub.toUpperCase();
-				for(Pattern sp : subParamPatterns)
+			// fullVersion uses regex to handle wildcards
+			if (fullVersionPatterns.size() > 0)
+			{			
+				String tsidfull = ctsid.getVersion();
+				if (tsidfull != null && tsidfull.length() > 0)
 				{
-					Matcher m = sp.matcher(tsidSub);
-					if (m.matches())
+					tsidfull = tsidfull.toUpperCase();
+					for(Pattern sp : fullVersionPatterns)
 					{
-						passed = true;
-						break;
+						Matcher m = sp.matcher(tsidfull);
+						if (m.matches())
+						{
+							versionPassed = true;
+							break;
+						}
 					}
 				}
 			}
-			if (!passed)
-				return false;
-			matches++;
-		}
+			
+			if (!versionPassed && subVersionPatterns.size() > 0)
+			{			
+				String tsidSub = ctsid.getSubVersion();
+				if (tsidSub != null && tsidSub.length() > 0)
+				{
+					tsidSub = tsidSub.toUpperCase();
+					for(Pattern sp : subVersionPatterns)
+					{
+						Matcher m = sp.matcher(tsidSub);
+						if (m.matches())
+						{
+							versionPassed = true;
+							break;
+						}
+					}
+				}
+			}
 		
-		// baseParam uses regex to handle wildcards
-		if (baseParamPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidbase = ctsid.getBaseParam();
-			if (tsidbase != null && tsidbase.length() > 0)
-			{
-				tsidbase = tsidbase.toUpperCase();
-				for(Pattern sp : baseParamPatterns)
+			if (!versionPassed && baseVersionPatterns.size() > 0)
+			{			
+				String tsidbase = ctsid.getBaseVersion();
+				if (tsidbase != null && tsidbase.length() > 0)
 				{
-					Matcher m = sp.matcher(tsidbase);
-					if (m.matches())
+					tsidbase = tsidbase.toUpperCase();
+					for(Pattern sp : baseVersionPatterns)
 					{
-						passed = true;
-						break;
+						Matcher m = sp.matcher(tsidbase);
+						if (m.matches())
+						{
+							versionPassed = true;
+							break;
+						}
 					}
 				}
 			}
-			if (!passed)
-				return false;
-			matches++;
-		}
-		
-		// SubVersion uses regex to handle wildcards
-		if (subVersionPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidSub = ctsid.getSubVersion();
-			if (tsidSub != null && tsidSub.length() > 0)
-			{
-				tsidSub = tsidSub.toUpperCase();
-				for(Pattern sp : subVersionPatterns)
-				{
-					Matcher m = sp.matcher(tsidSub);
-					if (m.matches())
-					{
-						passed = true;
-						break;
-					}
-				}
-			}
-			if (!passed)
-				return false;
-			matches++;
-		}
-		
-		// baseVersion uses regex to handle wildcards
-		if (baseVersionPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidbase = ctsid.getBaseVersion();
-			if (tsidbase != null && tsidbase.length() > 0)
-			{
-				tsidbase = tsidbase.toUpperCase();
-				for(Pattern sp : baseVersionPatterns)
-				{
-					Matcher m = sp.matcher(tsidbase);
-					if (m.matches())
-					{
-						passed = true;
-						break;
-					}
-				}
-			}
-			if (!passed)
-				return false;
-			matches++;
-		}
 
-		// fullVersion uses regex to handle wildcards
-		if (fullVersionPatterns.size() > 0)
-		{			
-			boolean passed = false;
-			String tsidfull = ctsid.getVersion();
-			if (tsidfull != null && tsidfull.length() > 0)
-			{
-				tsidfull = tsidfull.toUpperCase();
-				for(Pattern sp : fullVersionPatterns)
-				{
-					Matcher m = sp.matcher(tsidfull);
-					if (m.matches())
-					{
-						passed = true;
-						break;
-					}
-				}
-			}
-			if (!passed)
+			
+			if (!versionPassed)
 				return false;
 			matches++;
 		}
