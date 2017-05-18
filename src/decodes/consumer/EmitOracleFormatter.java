dd040f34a212a49017aa6426cd44a47daefc303a
mmaloney <>
2017-05-18 20:54:04 +0000

    Added new property-driven features requested by Art Armour.

--- src/decodes/consumer/EmitOracleFormatter.java
+++ src/decodes/consumer/EmitOracleFormatter.java
@@ -5,19 +5,24 @@ package decodes.consumer;
 
 import java.util.Iterator;
 import java.util.Properties;
-import java.util.Date;
-import java.util.Calendar;
 import java.text.SimpleDateFormat;
 
+import ilex.util.PropertiesUtil;
+import ilex.util.TextUtil;
 import ilex.var.TimedVariable;
 import ilex.var.IFlags;
-
-import decodes.db.*;
 import decodes.decoder.DecodedMessage;
 import decodes.decoder.TimeSeries;
 import decodes.decoder.Sensor;
 import decodes.datasource.RawMessage;
 import decodes.datasource.UnknownPlatformException;
+import decodes.db.Constants;
+import decodes.db.DataType;
+import decodes.db.EngineeringUnit;
+import decodes.db.Platform;
+import decodes.db.PresentationGroup;
+import decodes.db.SiteName;
+import decodes.db.TransportMedium;
 import decodes.util.PropertySpec;
 
 /**
@@ -27,23 +32,41 @@ import decodes.util.PropertySpec;
 public class EmitOracleFormatter extends OutputFormatter
 {
 	/** Column-delimiter specified by 'delimiter' property */
-	private String delimiter;
+	private String delimiter = " ";
+	
+	private boolean justify = true;
+	private boolean addMsgDelim = true;
+	private String dateFormat = "yyDDD/HH:mm:ss";
+	private String siteNameType = null;
+	private String sitePrefix = null;
+	private String dataType = Constants.datatype_SHEF;
 
 	/** Used to format time stamps. */
-	private SimpleDateFormat emitDateFormat;
+	private SimpleDateFormat sdf = null;
 	
 	private PropertySpec propSpecs[] = 
 	{		
 		new PropertySpec("delimiter", PropertySpec.STRING,
-			"Used between columns (default=space)")
+			"Used between columns (default=space)"),
+		new PropertySpec("justify", PropertySpec.BOOLEAN,
+			"(default=true) Pad with blanks to line up columns."),
+		new PropertySpec("addMsgDelim", PropertySpec.BOOLEAN,
+			"(default=true) Add a line with ZZZZ at the end of each message."),
+		new PropertySpec("dateFormat", PropertySpec.STRING,
+			"(default=yyDDD/HH:mm:ss) Java SimpleDateFormat spec."),
+		new PropertySpec("siteNameType", PropertySpec.STRING,
+			"(default=null meaning print DCP Address)"),
+		new PropertySpec("sitePrefix", PropertySpec.STRING,
+			"(default=none) If supplied, this is added as a prefix to site names."),
+		new PropertySpec("dataType", PropertySpec.DECODES_ENUM + "DataTypeStandard",
+			"(default=" + Constants.datatype_SHEF + ") Determines which data type to print.")
+	
 	};
 
 	/** default constructor */
 	protected EmitOracleFormatter()
 	{
 		super();
-		delimiter = " ";
-		emitDateFormat = new SimpleDateFormat("yyDDD/HH:mm:ss");
 	}
 
 	/**
@@ -59,11 +82,35 @@ public class EmitOracleFormatter extends OutputFormatter
 		PresentationGroup presGrp, Properties rsProps)
 		throws OutputFormatterException
 	{
-		String d = (String)rsProps.get("delimiter");
-		if (d != null)
-			delimiter = d;
-		Calendar cal = Calendar.getInstance(tz);
-		emitDateFormat.setCalendar(cal);
+		String s = PropertiesUtil.getIgnoreCase(rsProps, "delimiter");
+		if (s != null)
+			delimiter = s;
+		
+		s = PropertiesUtil.getIgnoreCase(rsProps, "justify");
+		if (s != null)
+			justify = TextUtil.str2boolean(s);
+		
+		s = PropertiesUtil.getIgnoreCase(rsProps, "addMsgDelim");
+		if (s != null)
+			addMsgDelim = TextUtil.str2boolean(s);
+		
+		s = PropertiesUtil.getIgnoreCase(rsProps, "dateFormat");
+		if (s != null)
+			dateFormat = s;
+		sdf = new SimpleDateFormat(dateFormat);
+		sdf.setTimeZone(tz);
+
+		s = PropertiesUtil.getIgnoreCase(rsProps, "siteNameType");
+		if (s != null)
+			siteNameType = s;
+		s = PropertiesUtil.getIgnoreCase(rsProps, "sitePrefix");
+		if (s != null)
+			sitePrefix = s;
+		
+		s = PropertiesUtil.getIgnoreCase(rsProps, "dataType");
+		if (s != null)
+			dataType = s;
+
 	}
 
 	/** Does nothing. */
@@ -101,34 +148,50 @@ public class EmitOracleFormatter extends OutputFormatter
 		}
 
 		String dcpId = tm.getMediumId();
+		if (siteNameType != null)
+		{
+			String snt[] = siteNameType.split(",");
+			for(String s : snt)
+			{
+				SiteName sn = platform.getSite().getName(s);
+				if (sn != null)
+				{
+					dcpId = sn.getNameValue();
+					break;
+				}
+			}
+			// Fell through, no match found.
+			dcpId = platform.getSiteName(false);
+		}
+		if (sitePrefix != null)
+			dcpId = sitePrefix + dcpId;
 
 		char platformType = 'I';
 		if (tm.getMediumType().equalsIgnoreCase(Constants.medium_GoesRD))
 			platformType = 'R';
 
-		String platformSiteName = platform.getSiteName(false);
-
-		for(Iterator it = msg.getAllTimeSeries(); it.hasNext(); )
+		for(Iterator<TimeSeries> it = msg.getAllTimeSeries(); it.hasNext(); )
 		{
-			TimeSeries ts = (TimeSeries)it.next();
+			TimeSeries ts = it.next();
 			Sensor sensor = ts.getSensor();
 
-			String platformName = sensor.getSensorSiteName();
-			if (platformName == null)
-				platformName = platformSiteName;
+			String siteName = dcpId;
+			String sensorSiteName = sensor.getSensorSiteName();
+			if (sensorSiteName != null)
+				siteName = (sitePrefix == null ? "" : sitePrefix) + sensorSiteName;
 
 			EngineeringUnit eu = ts.getEU();
 
-			DataType dt = sensor.getDataType(Constants.datatype_EPA);
-			String usgsCode = dt != null ? dt.getCode() : "0";
+//			DataType dt = sensor.getDataType(Constants.datatype_EPA);
+//			String usgsCode = dt != null ? dt.getCode() : "0";
 
-			dt = sensor.getDataType(Constants.datatype_SHEF);
-			String shefCode = dt != null ? dt.getCode() : "XX";
+			DataType dt = sensor.getDataType(dataType);
+			String dtCode = dt != null ? dt.getCode() : "XX";
 
 			String sensorNum = "" + ts.getSensorNumber();
-			String sensorName = sensor.getName();
+//			String sensorName = sensor.getName();
 
-			String recordingInt = "" + sensor.getRecordingInterval();
+//			String recordingInt = "" + sensor.getRecordingInterval();
 
 			int sz = ts.size();
 			for(int i=0; i<sz; i++)
@@ -138,26 +201,29 @@ public class EmitOracleFormatter extends OutputFormatter
 					continue;
 
 				sb.setLength(0);
-				sb.append(dcpId);
-				for(int j=sb.length(); j<8; j++)
-					sb.append(' ');
+				sb.append(siteName);
+				if (justify)
+					for(int j=sb.length(); j<8; j++)
+						sb.append(' ');
 
 				sb.append(delimiter);
-				sb.append(shefCode);
+				sb.append(dtCode);
 
 				sb.append(delimiter);
 				sb.append(sensorNum);
-				for(int j=sensorNum.length(); j<4; j++)
-					sb.append(' ');
+				if (justify)
+					for(int j=sensorNum.length(); j<4; j++)
+						sb.append(' ');
 			
 				sb.append(delimiter);
-				sb.append(emitDateFormat.format(tv.getTime()));
+				sb.append(sdf.format(tv.getTime()));
 
 				sb.append(delimiter);
 				String s = ts.formattedSampleAt(i);
 				sb.append(s);
-				for(int j=s.length(); j<10; j++)
-					sb.append(' ');
+				if (justify)
+					for(int j=s.length(); j<10; j++)
+						sb.append(' ');
 			
 				sb.append(delimiter);
 				sb.append(platformType);
@@ -168,7 +234,8 @@ public class EmitOracleFormatter extends OutputFormatter
 				consumer.println(sb.toString());
 			}
 		}
-		consumer.println("ZZZZ");
+		if (addMsgDelim)
+			consumer.println("ZZZZ");
 		consumer.endMessage();
 	}
 	
