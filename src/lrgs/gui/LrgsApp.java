6d0d80a515f6336e43b764184905586dffe0c175
mmaloney <>
2019-03-28 13:21:51 +0000

    Updated LRGS Minor Version to 3.

--- src/lrgs/gui/LrgsApp.java
+++ src/lrgs/gui/LrgsApp.java
@@ -15,7 +15,7 @@ public class LrgsApp
 	public static final int majorVersion = 9;
 
 	/** Minor version number */
-	public static final int minorVersion = 2;
+	public static final int minorVersion = 3;
 
 	/** Sub version number */
 	public static final String subVersion = DecodesVersion.getName() + "-" + DecodesVersion.getVersion();
