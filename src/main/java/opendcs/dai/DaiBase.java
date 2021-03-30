dbc1c08d02422b6b0b17e1d6f77166ef3205ee09
Michael Maloney <mike@covesw.com>
2021-03-30 16:13:01 -0400

    Refactoring for allowing connection pooling.

---
+++ src/main/java/opendcs/dai/DaiBase.java
@@ -0,0 +1,20 @@
+package opendcs.dai;
+
+import java.sql.ResultSet;
+
+import decodes.tsdb.DbIoException;
+
+public interface DaiBase
+{
+	public ResultSet doQuery(String q)
+		throws DbIoException;
+	
+	public ResultSet doQuery2(String q) 
+		throws DbIoException;
+
+	public int doModify(String q)
+		throws DbIoException;
+
+	public void close();
+
+}
