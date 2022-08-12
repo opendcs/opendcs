ded6eb37ea6fc16b5c697517edc4a1ba1da8c6f2
Adam Korynta <adam@rmanet.com>
2022-08-12 08:31:17 -0700

    fix test class name to adhere to xUnit standards

--- src/test/java/decodes/cwms/CwmsTimeSeriesDAOTest.java
+++ src/test/java/decodes/cwms/CwmsTimeSeriesDAOTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-final class TestCwmsTimeSeriesDAO
+final class CwmsTimeSeriesDAOTest
 {
 	@Test
 	public void testExceedsMaxTimeGap()
