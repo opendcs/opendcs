1a8cc89fc88791c8046beca3620a6253547b8957
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-06-22 17:30:15 +0000

    Initial generic value expander.
    Creates a system such that users can configure values of propreties
    with references to external systems. For example getting credentials
    from the filesystem, or environment variables.
    
    Implemented as a service provider so it is easier to expand in the future
    for OpenDCS and advanced users of OpenDCS that have a specific need.

--- core/src/test/java/org/opendcs/utils/PropertyProvider.java
+++ core/src/test/java/org/opendcs/utils/PropertyProvider.java
@@ -4,8 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.File;
+<<<<<<< HEAD:core/src/test/java/org/opendcs/utils/PropertyProvider.java
 import java.io.IOException;
 
+=======
+
+import org.jooq.exception.IOException;
+>>>>>>> 891e1381 (Initial generic value expander.):src/test/java/org/opendcs/utils/PropertyProvider.java
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,6 +18,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import fixtures.FileUtils;
+<<<<<<< HEAD:core/src/test/java/org/opendcs/utils/PropertyProvider.java
 import ilex.util.EnvExpander;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 
@@ -25,6 +31,19 @@ public class PropertyProviderTest
     private static final String PROP_VAR_NAME ="test.provider.prop";
     static File secretFileProperty = null;
 
+=======
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+
+public class PropertyProvider
+{
+    private static final String SECRET_FILE_VAR = "TEST_FILE_VAR";
+    private static final String ENV_VAR = "TEST_ENV_VAR";
+    private static final String ENV_VAR_NAME ="THE_TEST_ENV_VAR";
+    private static final String PROP_VAR = "TEST_PROP_VAR";
+    private static final String PROP_VAR_NAME ="test.provider.prop";
+    static File secretFileProperty = null; 
+    
+>>>>>>> 891e1381 (Initial generic value expander.):src/test/java/org/opendcs/utils/PropertyProvider.java
     static EnvironmentVariables envVars = new EnvironmentVariables();
 
     @ParameterizedTest
@@ -45,6 +64,26 @@ public class PropertyProviderTest
         assertEquals(value,Property.getRealPropertyValue(null, value));
     }
 
+<<<<<<< HEAD:core/src/test/java/org/opendcs/utils/PropertyProvider.java
+=======
+    @Test 
+    void test_throws_when_def_not_right() throws Exception
+    {
+        assertThrows(IOException.class, () -> {
+            String value = Property.getRealPropertyValue("${missing end curly");
+        }, "Failed to process bad definition correctly");
+    }
+
+    @Test
+    void test_throws_when_bad_provider() throws Exception
+    {
+        assertThrows(IOException.class, () -> {
+            String value = Property.getRealPropertyValue("${non existent provider.I'm the real value}");
+        }, "Failed to process bad definition correctly");
+    }
+
+
+>>>>>>> 891e1381 (Initial generic value expander.):src/test/java/org/opendcs/utils/PropertyProvider.java
     @BeforeAll
     static void setup_properties() throws Exception
     {
@@ -64,6 +103,7 @@ public class PropertyProviderTest
     @Test
     public void test_default_providers() throws Exception
     {
+<<<<<<< HEAD:core/src/test/java/org/opendcs/utils/PropertyProvider.java
         final String resultFile = Property.getRealPropertyValue("file."+secretFileProperty.getAbsolutePath());
         assertEquals(SECRET_FILE_VAR, resultFile, "Could not properly retrieve variable from file.");
 
@@ -73,6 +113,15 @@ public class PropertyProviderTest
         });
 
         final String propVar = Property.getRealPropertyValue("java." + PROP_VAR_NAME);
+=======
+        final String resultFile = Property.getRealPropertyValue("${file."+secretFileProperty.getAbsolutePath()+"}");
+        assertEquals(SECRET_FILE_VAR, resultFile, "Could not properly retrieve variable from file.");
+
+        final String envVar = Property.getRealPropertyValue("${env." + ENV_VAR_NAME + "}");
+        assertEquals(ENV_VAR, envVar, "Could not properly retrieve variable from environment.");
+
+        final String propVar = Property.getRealPropertyValue("${java." + PROP_VAR_NAME +"}");
+>>>>>>> 891e1381 (Initial generic value expander.):src/test/java/org/opendcs/utils/PropertyProvider.java
         assertEquals(PROP_VAR, propVar,"Could not properly retrieve variable from java system properties.");
 
         final String passThroughExpected = "\\${test}";
@@ -80,6 +129,7 @@ public class PropertyProviderTest
         assertEquals(passThroughExpected, passThroughVar, "Passthrough did not work.");
 
     }
+<<<<<<< HEAD:core/src/test/java/org/opendcs/utils/PropertyProvider.java
 
 
     @ParameterizedTest
@@ -93,4 +143,6 @@ public class PropertyProviderTest
             assertEquals(valueExpanded,expansion, "Could not set all values.");
         });
     }
+=======
+>>>>>>> 891e1381 (Initial generic value expander.):src/test/java/org/opendcs/utils/PropertyProvider.java
 }
