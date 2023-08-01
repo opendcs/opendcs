8f61652153c48ed303257e44fe6f8f37da61412d
hoffmanjs <scott.j.hoffman@usace.army.mil>
2023-08-01 14:46:21 -0500

    .

--- src/main/java/decodes/tsdb/compedit/ComputationsListPanel.java
+++ src/main/java/decodes/tsdb/compedit/ComputationsListPanel.java
@@ -155,13 +155,14 @@ public class ComputationsListPanel extends ListPanel
 	protected void doOpen()
 	{
 		int r = compListTable.getSelectedRow();
-		if (r == -1)
+		int rowModel = compListTable.convertRowIndexToModel(r);
+		if (rowModel== -1)
 		{
 			parentFrame.showError(
 				compLabels.getString("ComputationsFilterPanel.OpenError"));
 			return;
 		}
-		ComputationInList dc = (ComputationInList)compListTableModel.getRowObject(r);
+		ComputationInList dc = (ComputationInList)compListTableModel.getRowObject(rowModel);
 		ComputationDAI computationDAO = tsdb.makeComputationDAO();
 		try
 		{
