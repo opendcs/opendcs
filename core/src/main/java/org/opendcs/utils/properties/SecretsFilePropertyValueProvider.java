aa57bf11f3d2e370618d7c33fe900efa00c3f2f4
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-08-03 21:41:53 +0000

    Reorganize to take properties and environment as a parameter.

--- core/src/main/java/org/opendcs/utils/properties/SecretsFilePropertyValueProvider.java
+++ core/src/main/java/org/opendcs/utils/properties/SecretsFilePropertyValueProvider.java
@@ -2,6 +2,8 @@ package org.opendcs.utils.properties;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 
 import org.opendcs.spi.properties.PropertyValueProvider;
 
@@ -17,11 +19,21 @@ public class SecretsFilePropertyValueProvider implements PropertyValueProvider
         return value.startsWith(prefix);
     }
 
+
+    /**
+     * Retrieve property file named within the value.
+     * @param value actual value to decipher.
+     *
+     * @param properties ignored in this implementation.
+     * @param environment ignored in this implementation.
+     *
+     * @return the real value, or null if not found.
+     */
     @Override
-    public String processValue(String value) throws IOException
+    public String processValue(String value, Properties props, Map<String,String> environment) throws IOException
     {
         final String fileName = value.substring(prefix.length());
         return FileUtil.getFileContents(new File(fileName));
     }
-    
+
 }
