d877726cd2922763499b2a3e107f0410a8e11a00
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-08-03 21:41:53 +0000

    Reorganize to take properties and environment as a parameter.

--- src/main/java/org/opendcs/utils/properties/JavaPropertiesValueProvider.java
+++ src/main/java/org/opendcs/utils/properties/JavaPropertiesValueProvider.java
@@ -1,6 +1,8 @@
 package org.opendcs.utils.properties;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 
 import org.opendcs.spi.properties.PropertyValueProvider;
 
@@ -14,10 +16,19 @@ public class JavaPropertiesValueProvider implements PropertyValueProvider
         return value.toLowerCase().startsWith(prefix);
     }
 
+    /**
+     * Retrieve property from the provided property map
+     * @param value actual value to decipher.
+     *
+     * @param properties Properties to use for the given request.
+     * @param environment ignored in this implementation.
+     *
+     * @return the real value, or null if not found.
+     */
     @Override
-    public String processValue(String value) throws IOException
+    public String processValue(String value, Properties props, Map<String,String> environment) throws IOException
     {
         String propVar = value.substring(prefix.length());
-        return System.getProperty(propVar);
+        return props.getProperty(propVar);
     }
 }
