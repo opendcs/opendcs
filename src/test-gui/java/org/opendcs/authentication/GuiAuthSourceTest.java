6de84176533550fba3fbced34b4e07bb2fe646c2
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-08-22 15:26:20 -0700

    Allow the LoginDialog to be an AuthSource and implement our first GUI… (#281)
    * Allow the LoginDialog to be an AuthSource and implement our first GUI test.
    
    * add gui test to workflow.
    
    * add sudo.
    
    * add docs.
    
    * center dialog when AuthSource.
    
    * Correct directory.
    
    * additional test, allow faster tests.

---
+++ src/test-gui/java/org/opendcs/authentication/GuiAuthSourceTest.java
@@ -0,0 +1,103 @@
+package org.opendcs.authentication;
+
+import static org.assertj.swing.timing.Pause.pause;
+import static org.assertj.swing.timing.Timeout.timeout;
+import static org.assertj.swing.edt.GuiActionRunner.execute;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ilex.gui.LoginDialog;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.SwingWorker;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.DialogFixture;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.timing.Condition;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opendcs.spi.authentication.AuthSource;
+
+import com.github.dockerjava.api.model.Frame;
+
+
+public class GuiAuthSourceTest
+{
+    private DialogFixture dialog;
+    //private FrameFixture frame;
+    private LoginDialog loginDialog;
+    //private JFrame rootFrame;
+    private ExecutorService executor = Executors.newCachedThreadPool();
+
+    @BeforeEach
+    public void setup() throws Exception
+    {
+        loginDialog = (LoginDialog)AuthSourceService.getFromString("gui-auth-source:Login");
+        //rootFrame = GuiActionRunner.execute(() -> new JFrame("Root Window"));
+        //frame = new FrameFixture(rootFrame);
+        dialog = new DialogFixture(loginDialog);
+        //frame.show();
+    }
+
+    @AfterEach
+    public void tearDown()
+    {        
+        dialog.cleanUp();
+    }
+
+    @Test
+    public void login_accepted() throws Exception
+    {        
+        final String username = "user";
+        final String password = "password";
+    
+        Future<Properties> credentialsFuture = executor.submit(() -> loginDialog.getCredentials());
+        assertTrue(loginDialog.isValid(), "Our Dialog isn't valid.");
+        pause(new Condition("Gui visible") {
+            @Override
+            public boolean test() {
+                return execute(loginDialog::isVisible);
+            }
+        },timeout(500));
+
+        dialog.textBox("username").setText(username);
+        dialog.textBox("password").setText(password);
+        dialog.button("ok").click();
+        Properties creds = credentialsFuture.get();
+        assertNotNull(creds, "no credentials returned.");
+        assertEquals(username,creds.getProperty("username"), "Username did not match");
+        assertEquals(password,creds.getProperty("password"), "Password did not match");
+    }
+
+    @Test
+    public void login_cancelled_returns_null() throws Exception
+    {
+        Future<Properties> credentialsFuture = executor.submit(() -> loginDialog.getCredentials());
+        assertTrue(loginDialog.isValid(), "Our Dialog isn't valid.");
+        pause(new Condition("Gui visible") {
+            @Override
+            public boolean test() {
+                return execute(loginDialog::isVisible);
+            }
+        },timeout(500));
+        dialog.textBox("username").setText("doesn't matter");
+        dialog.textBox("password").setText("bad password");
+        dialog.button("cancel").click();
+        Properties creds = credentialsFuture.get();
+        assertNull(creds, "Actual values were returned during cancel.");
+    }
+}
