f0c031d48e8f1a2539a63778f5016f05d6e86b5a
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-10-23 21:47:30 +0000

    Correct typo.

--- docker_scripts/tsdb_config.sh
+++ docker_scripts/tsdb_config.sh
@@ -4,7 +4,7 @@ if [ ! -f $DCSTOOL_USERDIR/user.properties ]; then
     echo "Generating Initial properties file."
     sed -e "s/OPENDCS_DATABASE_TYPE/$DATABASE_TYPE/" \
         -e "s~OPENDCS_DATABASE_URL~$DATABASE_URL~" \
-        -e "s/OPENDCS_DATABASE_DRIVER/$DATABSE_DRIVER/" \
+        -e "s/OPENDCS_DATABASE_DRIVER/$DATABASE_DRIVER/" \
         -e "s/OPENDCS_CWMS_OFFICE/$CWMS_OFFICE/" \
         -e "s/OPENDCS_DATATYPE_STANDARD/$DATATYPE_STANDARD/" \
         -e "s/OPENDCS_KEYGENERATOR/$KEYGENERATOR/" \
