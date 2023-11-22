32b1de01cdd431c74b3be8a0487f902355b14c0a
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-11-22 16:28:56 +0000

    Initial setup of using slf4j logger in front of the current built in logger.
    Removed synchronized from ThreadLogger.log as it was causing a deadlock.
    Initial testing shows appropriate behavior with this change.

---
+++ core/src/main/java/org/opendcs/utils/logging/SLF4JLogger.java
@@ -0,0 +1,115 @@
+package org.opendcs.utils.logging;
+
+import java.io.PrintStream;
+
+import org.slf4j.Marker;
+import org.slf4j.event.Level;
+import org.slf4j.helpers.AbstractLogger;
+
+public class SLF4JLogger extends AbstractLogger
+{
+    private ilex.util.Logger realLogger;
+    private String name;
+
+    public SLF4JLogger(String name, ilex.util.Logger realLogger)
+    {
+        this.name = name;
+        this.realLogger = realLogger;
+    }
+
+    @Override
+    public boolean isDebugEnabled()
+    {
+        return realLogger.getMinLogPriority() < ilex.util.Logger.E_INFORMATION;
+    }
+
+    @Override
+    public boolean isDebugEnabled(Marker marker)
+    {
+        return isDebugEnabled();
+    }
+
+    @Override
+    public boolean isErrorEnabled()
+    {
+        return realLogger.getMinLogPriority() < ilex.util.Logger.E_FAILURE;
+    }
+
+    @Override
+    public boolean isErrorEnabled(Marker arg0)
+    {
+        return isErrorEnabled();
+    }
+
+    @Override
+    public boolean isInfoEnabled()
+    {
+        return realLogger.getMinLogPriority() < ilex.util.Logger.E_WARNING;
+    }
+
+    @Override
+    public boolean isInfoEnabled(Marker arg0)
+    {
+        return isInfoEnabled();
+    }
+
+    @Override
+    public boolean isTraceEnabled()
+    {
+        return realLogger.getMinLogPriority() <= ilex.util.Logger.E_DEBUG3;
+    }
+
+    @Override
+    public boolean isTraceEnabled(Marker arg0)
+    {
+        return isTraceEnabled();
+    }
+
+    @Override
+    public boolean isWarnEnabled()
+    {
+        return realLogger.getMinLogPriority() <= ilex.util.Logger.E_WARNING;
+    }
+
+    @Override
+    public boolean isWarnEnabled(Marker arg0)
+    {
+        return isWarnEnabled();
+    }
+
+    @Override
+    protected String getFullyQualifiedCallerName()
+    {
+        return null;
+    }
+
+    @Override
+    protected void handleNormalizedLoggingCall(Level level, Marker marker, String msg, Object[] args, Throwable ex)
+    {
+        int ilexLevel = slf4jToIlexLevel(level);
+        realLogger.log(ilexLevel,msg);
+        PrintStream out = null;
+        if (realLogger.getMinLogPriority() <= ilexLevel
+            && ex != null 
+            && (out = realLogger.getLogOutput()) != null)
+        {
+            ex.printStackTrace(out);
+        }
+    }
+
+    private int slf4jToIlexLevel(Level level)
+    {
+        {
+            switch(level)
+            {
+                case WARN: return ilex.util.Logger.E_WARNING;
+                case DEBUG: return ilex.util.Logger.E_DEBUG1;
+                case TRACE: return ilex.util.Logger.E_DEBUG3;
+                case ERROR: return ilex.util.Logger.E_FAILURE;
+                case INFO: ;// intentional pass through
+                default: return ilex.util.Logger.E_INFORMATION;
+            }
+        }
+    }
+    
+}
