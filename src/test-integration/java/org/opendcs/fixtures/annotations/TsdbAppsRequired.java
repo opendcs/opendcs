cf9d67a3cb53d649fc2f1aa0fc1fae8d29ed8642
Mike Neilson <michael.a.neilson@usace.army.mil>
2023-12-11 17:17:05 +0000

    Javadocs for annotation.

--- src/test-integration/java/org/opendcs/fixtures/annotations/TsdbAppsRequired.java
+++ src/test-integration/java/org/opendcs/fixtures/annotations/TsdbAppsRequired.java
@@ -6,6 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation will be used by the JVM to hold
+ * all configured {@link TsdbAppRequired} instances.
+ *
+ * It can be used directory but such usage is not recommended. Multiple
+ * separate {@link TsdbAppRequired} annotations are the preferred method.
+ */
 @Documented
 @Target({ElementType.METHOD,ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
