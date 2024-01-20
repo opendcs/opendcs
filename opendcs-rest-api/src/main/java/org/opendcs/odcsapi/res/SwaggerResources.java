909566922eae143c0ccdde9dfd5315c88cc4039b
wjonassen <william_jonassen@olav.com>
2024-01-20 06:38:29 -0500

    Updated target="_blank" code to use rel="noopener".

--- opendcs-rest-api/src/main/java/org/opendcs/odcsapi/res/SwaggerResources.java
+++ opendcs-rest-api/src/main/java/org/opendcs/odcsapi/res/SwaggerResources.java
@@ -1,17 +1,18 @@
 package org.opendcs.odcsapi.res;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
-import java.io.File;
 import java.io.InputStreamReader;
-import java.io.ByteArrayOutputStream;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import javax.imageio.ImageIO;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -23,111 +24,36 @@ public class SwaggerResources
     @Context ServletContext servletContext;
 
     @GET
-    @Path("swaggerui")
     @Produces(MediaType.TEXT_HTML)
+    @Path("swaggerui")
     public Response getSwagger() throws IOException
     {
-        InputStream inputStream = servletContext.getResourceAsStream("/WEB-INF/classes/swaggerui/index.html");
-        int bufferSize = 1024;
-        char[] buffer = new char[bufferSize];
-        StringBuilder out = new StringBuilder();
-        Reader in = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-        for (int numRead; (numRead = in.read(buffer, 0, buffer.length)) > 0; )
-        {
-            out.append(buffer, 0, numRead);
-        }
-        String result = "";
-        result = out.toString();
+        String result = this.getSwaggerTextResource("index.html");
+        return Response.status(Response.Status.OK).entity(result).build();
+    }
+    @GET
+    @Path("{fileName: .*\\.(css|js)$ }")
+    public Response getTextFile(@PathParam("fileName") String fileName) throws IOException
+    {
+        String result = this.getSwaggerTextResource(fileName);
         return Response.status(Response.Status.OK).entity(result).build();
     }
 
     @GET
-    @Path("index.css")
-    public Response getIndexCss() throws IOException
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{fileName: .*\\.(json)$ }")
+    public Response getJsonFile(@PathParam("fileName") String fileName) throws IOException
     {
-        String result = this.getSwaggerTextResource("index.css");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("open_api.json")
-    public Response getOpenApiJson() throws IOException
-    {
-        String result = this.getSwaggerTextResource("open_api.json");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-initializer.js")
-    public Response getSwaggerInitializer() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-initializer.js");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui.css")
-    public Response getSwaggerUiCss() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui.css");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui.js")
-    public Response getSwaggerUiJs() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui.js");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui-bundle.js")
-    public Response getSwaggerUiBundleJs() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui-bundle.js");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui-es-bundle.js")
-    public Response getSwaggerUiEsBundleJs() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui-es-bundle.js");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui-es-bundle-core.js")
-    public Response getSwaggerUiEsBundleCoreJs() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui-es-bundle-core.js");
-        return Response.status(Response.Status.OK).entity(result).build();
-    }
-
-    @GET
-    @Path("swagger-ui-standalone-preset.js")
-    public Response getSwaggerUiStandalonePresetJs() throws IOException
-    {
-        String result = this.getSwaggerTextResource("swagger-ui-standalone-preset.js");
+        String result = this.getSwaggerTextResource(fileName);
         return Response.status(Response.Status.OK).entity(result).build();
     }
 
     @GET
     @Produces("image/png")
-    @Path("favicon-16x16.png")
-    public Response getFavIcon16() throws IOException
+    @Path("{fileName: .*\\.(png|jpeg)$ }")
+    public Response getImageFile(@PathParam("fileName") String fileName) throws IOException
     {
-        byte[] imageData = getSwaggerImageResource("favicon-16x16.png");
-        return Response.ok(imageData).build();
-    }
-
-    @GET
-    @Produces("image/png")
-    @Path("favicon-32x32.png")
-    public Response getFavIcon32() throws IOException
-    {
-        byte[] imageData = getSwaggerImageResource("favicon-32x32.png");
+        byte[] imageData = getSwaggerImageResource(fileName);
         return Response.ok(imageData).build();
     }
 
