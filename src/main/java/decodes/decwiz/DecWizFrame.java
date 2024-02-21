ab4f4818edec7aff63eada2cf2c2bad7051e1cf4
Karl Tarbet <ktarbet@users.noreply.github.com>
2024-02-21 12:38:47 -0800

    UI cleanup (#486)
    * remove un-used FrameDisplayer.java
    
    * create WindowUtility.center(T)
    
    * start using WindowUtility.center(T)
    
    * more use of WindowsUtility.center(T)
    
    drop method centerOnScreen in TopFrame.java
    
    * more use of WindowsUtility.center(T)
    
    * another batch using WindowsUtility.center(T)
    
    * one more WindowsUtility.center(T)
    
    * move { to next line
    
    * move { to next line
    
    * bracket placement.
    
    ---------
    
    Co-authored-by: Mike Neilson <michael.a.neilson@usace.army.mil>

--- src/main/java/decodes/decwiz/DecWizFrame.java
+++ src/main/java/decodes/decwiz/DecWizFrame.java
@@ -10,189 +10,189 @@ import decodes.db.Database;
 
 public class DecWizFrame extends TopFrame
 {
-	private JPanel contentPane;
-	private BorderLayout borderLayout1 = new BorderLayout();
-	private JPanel southPanel = new JPanel();
-	private GridBagLayout gridBagLayout1 = new GridBagLayout();
-	private JButton backButton = new JButton();
-	private JButton nextButton = new JButton();
-	private JButton quitButton = new JButton();
-	private JPanel panelContainer = new JPanel();
-	private BorderLayout borderLayout2 = new BorderLayout();
-	private JPanel titlePanel = new JPanel();
-	private JLabel titleLabel = new JLabel();
-	private FlowLayout flowLayout1 = new FlowLayout();
+    private JPanel contentPane;
+    private BorderLayout borderLayout1 = new BorderLayout();
+    private JPanel southPanel = new JPanel();
+    private GridBagLayout gridBagLayout1 = new GridBagLayout();
+    private JButton backButton = new JButton();
+    private JButton nextButton = new JButton();
+    private JButton quitButton = new JButton();
+    private JPanel panelContainer = new JPanel();
+    private BorderLayout borderLayout2 = new BorderLayout();
+    private JPanel titlePanel = new JPanel();
+    private JLabel titleLabel = new JLabel();
+    private FlowLayout flowLayout1 = new FlowLayout();
 
-	private DecWizPanel thePanels[] = new DecWizPanel[3];
-	private int curPanel = -1;
-	private boolean exitOnClose = true;
+    private DecWizPanel thePanels[] = new DecWizPanel[3];
+    private int curPanel = -1;
+    private boolean exitOnClose = true;
 
-	public DecWizFrame()
-	{
-		try
-		{
-			setDefaultCloseOperation(EXIT_ON_CLOSE);
-			jbInit();
-		}
-		catch (Exception exception)
-		{
-			exception.printStackTrace();
-		}
-	}
+    public DecWizFrame()
+    {
+        try
+        {
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            jbInit();
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+    }
 
-	/**
-	 * Component initialization.
-	 *
-	 * @throws java.lang.Exception
-	 */
-	private void jbInit() throws Exception
-	{
-		contentPane = (JPanel) getContentPane();
-		contentPane.setLayout(borderLayout1);
-		setSize(new Dimension(548, 491));
-		setPreferredSize(new Dimension(700, 700));
-		setTitle("Decoding Wizard");
-		southPanel.setLayout(gridBagLayout1);
-		backButton.setPreferredSize(new Dimension(100, 27));
-		backButton.setText("<< Back");
-		backButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				backButton_actionPerformed(e);
-			}
-		});
-		nextButton.setPreferredSize(new Dimension(100, 27));
-		nextButton.setText("Next >>");
-		nextButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				nextButton_actionPerformed(e);
-			}
-		});
-		quitButton.setPreferredSize(new Dimension(100, 27));
-		quitButton.setText("Quit");
-		quitButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e)
-			{
-				quitButton_actionPerformed(e);
-			}
-		});
-		panelContainer.setLayout(borderLayout2);
-		panelContainer.setPreferredSize(new Dimension(560, 540));
-		panelContainer.setBorder(BorderFactory.createLoweredBevelBorder());
-		titleLabel.setFont(new java.awt.Font("Dialog", Font.BOLD, 15));
-		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		titleLabel.setText("The Panel Title");
-		titlePanel.setLayout(flowLayout1);
-		flowLayout1.setVgap(15);
-		contentPane.add(southPanel, java.awt.BorderLayout.SOUTH);
-		southPanel.add(backButton, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
-			, GridBagConstraints.WEST, GridBagConstraints.NONE,
-			new Insets(10, 10, 10, 10), 0, 0));
-		southPanel.add(nextButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
-			, GridBagConstraints.WEST, GridBagConstraints.NONE,
-			new Insets(10, 0, 10, 0), 0, 0));
-		southPanel.add(quitButton, new GridBagConstraints(2, 0, 1, 1, 1.0, 0.0
-			, GridBagConstraints.EAST, GridBagConstraints.NONE,
-			new Insets(10, 0, 10, 10), 0, 0));
-		contentPane.add(panelContainer, java.awt.BorderLayout.CENTER);
-		contentPane.add(titlePanel, java.awt.BorderLayout.NORTH);
-		titlePanel.add(titleLabel);
-	}
+    /**
+     * Component initialization.
+     *
+     * @throws java.lang.Exception
+     */
+    private void jbInit() throws Exception
+    {
+        contentPane = (JPanel) getContentPane();
+        contentPane.setLayout(borderLayout1);
+        setSize(new Dimension(548, 491));
+        setPreferredSize(new Dimension(700, 700));
+        setTitle("Decoding Wizard");
+        southPanel.setLayout(gridBagLayout1);
+        backButton.setPreferredSize(new Dimension(100, 27));
+        backButton.setText("<< Back");
+        backButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                backButton_actionPerformed(e);
+            }
+        });
+        nextButton.setPreferredSize(new Dimension(100, 27));
+        nextButton.setText("Next >>");
+        nextButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                nextButton_actionPerformed(e);
+            }
+        });
+        quitButton.setPreferredSize(new Dimension(100, 27));
+        quitButton.setText("Quit");
+        quitButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                quitButton_actionPerformed(e);
+            }
+        });
+        panelContainer.setLayout(borderLayout2);
+        panelContainer.setPreferredSize(new Dimension(560, 540));
+        panelContainer.setBorder(BorderFactory.createLoweredBevelBorder());
+        titleLabel.setFont(new java.awt.Font("Dialog", Font.BOLD, 15));
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        titleLabel.setText("The Panel Title");
+        titlePanel.setLayout(flowLayout1);
+        flowLayout1.setVgap(15);
+        contentPane.add(southPanel, java.awt.BorderLayout.SOUTH);
+        southPanel.add(backButton, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0
+            , GridBagConstraints.WEST, GridBagConstraints.NONE,
+            new Insets(10, 10, 10, 10), 0, 0));
+        southPanel.add(nextButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0
+            , GridBagConstraints.WEST, GridBagConstraints.NONE,
+            new Insets(10, 0, 10, 0), 0, 0));
+        southPanel.add(quitButton, new GridBagConstraints(2, 0, 1, 1, 1.0, 0.0
+            , GridBagConstraints.EAST, GridBagConstraints.NONE,
+            new Insets(10, 0, 10, 10), 0, 0));
+        contentPane.add(panelContainer, java.awt.BorderLayout.CENTER);
+        contentPane.add(titlePanel, java.awt.BorderLayout.NORTH);
+        titlePanel.add(titleLabel);
+    }
 
-	public void backButton_actionPerformed(ActionEvent e)
-	{
-		switchPanel(curPanel - 1);
-	}
+    public void backButton_actionPerformed(ActionEvent e)
+    {
+        switchPanel(curPanel - 1);
+    }
 
-	public void nextButton_actionPerformed(ActionEvent e)
-	{
-		if (curPanel == thePanels.length - 1)
-			switchPanel(0);
-		else
-			switchPanel(curPanel + 1);
-	}
+    public void nextButton_actionPerformed(ActionEvent e)
+    {
+        if (curPanel == thePanels.length - 1)
+            switchPanel(0);
+        else
+            switchPanel(curPanel + 1);
+    }
 
-	public void quitButton_actionPerformed(ActionEvent e)
-	{
-		if (exitOnClose)	{
-			Database db = Database.getDb();
-			db.getDbIo().close();
-			System.exit(0);
-		}
-		else
-			dispose();
-	}
+    public void quitButton_actionPerformed(ActionEvent e)
+    {
+        if (exitOnClose)    {
+            Database db = Database.getDb();
+            db.getDbIo().close();
+            System.exit(0);
+        }
+        else
+            dispose();
+    }
 
-	public void createPanels()
-	{
-		thePanels[0] = new FileIdPanel();
-		thePanels[1] = new DecodePanel();
-		thePanels[2] = new SavePanel();
-		curPanel = -1;
-	}
+    public void createPanels()
+    {
+        thePanels[0] = new FileIdPanel();
+        thePanels[1] = new DecodePanel();
+        thePanels[2] = new SavePanel();
+        curPanel = -1;
+    }
 
-	public FileIdPanel getFileIdPanel()
-	{
-		return (FileIdPanel)thePanels[0];
-	}
+    public FileIdPanel getFileIdPanel()
+    {
+        return (FileIdPanel)thePanels[0];
+    }
 
-	public DecodePanel getDecodePanel()
-	{
-		return (DecodePanel)thePanels[1];
-	}
+    public DecodePanel getDecodePanel()
+    {
+        return (DecodePanel)thePanels[1];
+    }
 
-	public SavePanel getSavePanel()
-	{
-		return (SavePanel)thePanels[2];
-	}
+    public SavePanel getSavePanel()
+    {
+        return (SavePanel)thePanels[2];
+    }
 
-	/**
-	  Called on Next or Previous buttons, switch from one panel to another.
-	  Call the current panel's deactivate button to see if it's ok to leave.
-	  Then switch and call the new panel's activate button.
-	  @param n the number of the panel to switch to
-	 */
-	public void switchPanel(int n)
-	{
-		if (curPanel != -1)
-		{
-			JPanel jp = (JPanel) thePanels[curPanel];
-			try
-			{
-				if (!((DecWizPanel) jp).deactivate())
-				{
-					return;
-				}
-			}
-			catch (Exception ex)
-			{
-				System.err.println("Exception in deactivate: " + ex);
-				ex.printStackTrace();
-			}
-			panelContainer.setVisible(false);
-			panelContainer.remove(jp);
-		}
-		curPanel = n;
-		JPanel jp = (JPanel) thePanels[curPanel];
-		try
-		{
-			((DecWizPanel) jp).activate();
-		}
-		catch (Exception ex)
-		{
-			System.err.println("Exception in activate: " + ex);
-			ex.printStackTrace();
-		}
-		panelContainer.add(jp);
+    /**
+      Called on Next or Previous buttons, switch from one panel to another.
+      Call the current panel's deactivate button to see if it's ok to leave.
+      Then switch and call the new panel's activate button.
+      @param n the number of the panel to switch to
+     */
+    public void switchPanel(int n)
+    {
+        if (curPanel != -1)
+        {
+            JPanel jp = (JPanel) thePanels[curPanel];
+            try
+            {
+                if (!((DecWizPanel) jp).deactivate())
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.err.println("Exception in deactivate: " + ex);
+                ex.printStackTrace();
+            }
+            panelContainer.setVisible(false);
+            panelContainer.remove(jp);
+        }
+        curPanel = n;
+        JPanel jp = (JPanel) thePanels[curPanel];
+        try
+        {
+            ((DecWizPanel) jp).activate();
+        }
+        catch (Exception ex)
+        {
+            System.err.println("Exception in activate: " + ex);
+            ex.printStackTrace();
+        }
+        panelContainer.add(jp);
 
-		titleLabel.setText(thePanels[curPanel].getTitle());
-		panelContainer.setVisible(true);
-		backButton.setEnabled(curPanel > 0);
-		nextButton.setText((curPanel == thePanels.length - 1) ? "Start Over" :
-						   "Next >>");
-	}
+        titleLabel.setText(thePanels[curPanel].getTitle());
+        panelContainer.setVisible(true);
+        backButton.setEnabled(curPanel > 0);
+        nextButton.setText((curPanel == thePanels.length - 1) ? "Start Over" :
+                           "Next >>");
+    }
 }
--- src/main/java/decodes/dupdcpgui/DuplicateDcpsGUI.java
+++ src/main/java/decodes/dupdcpgui/DuplicateDcpsGUI.java
@@ -2,35 +2,19 @@ package decodes.dupdcpgui;
 
 import ilex.cmdline.StringToken;
 import ilex.cmdline.TokenOptions;
+import ilex.gui.WindowUtility;
 import ilex.util.EnvExpander;
-import ilex.util.Logger;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Properties;
 
 import opendcs.dai.LoadingAppDAI;
-import decodes.db.Constants;
 import decodes.db.Database;
-import decodes.db.DatabaseException;
-import decodes.db.DatabaseIO;
 import decodes.db.NetworkList;
-import decodes.db.NetworkListEntry;
-import decodes.db.Site;
 import decodes.dcpmon.DcpMonitorConfig;
 import decodes.tsdb.CompAppInfo;
-import decodes.tsdb.DbIoException;
-import decodes.tsdb.NoSuchObjectException;
 import decodes.tsdb.TsdbAppTemplate;
 import decodes.util.CmdLineArgs;
-import decodes.util.DecodesSettings;
 
 /**
  * This class is the main class for the Duplicate DCPs GUI.
@@ -64,144 +48,132 @@ import decodes.util.DecodesSettings;
  * If this script changes we need to verify this code.
  */
 public class DuplicateDcpsGUI 
-	extends TsdbAppTemplate
+    extends TsdbAppTemplate
 {
-	private String module = "DuplicateDcpsList";
-	private DuplicateDcpsFrame dupDcpsFrame;
-	private String dcpMonConf;
-	private ArrayList<NetworkList> groups = new ArrayList<NetworkList>();
-	public static final String GROUP_DELIMITER_NAME = "-RIVERGAGES-DAS";
-	
-	//PDT File used in case there is no description when displaying
-	//the records in the table
-	private StringToken pdtFileArg = new StringToken("T", 
-		"pdt file (full file system path, " +
-		"fill descriptions if empty and PDT Owner Column)", "",
-		TokenOptions.optSwitch, 
-		"$LRGSHOME/pdt");
-	//NWS File used to get information for the dcps
-	private StringToken nwsFileArg = new StringToken("w", 
-		"nwshb5 file (full file system path, " +
-		"fill NWSHB5 name column and NWS description Column)", "",
-		TokenOptions.optSwitch, 
-		"$DECODES_INSTALL_DIR/hads");
-	private boolean _shutdown = false;
+    private String module = "DuplicateDcpsList";
+    private DuplicateDcpsFrame dupDcpsFrame;
+    private String dcpMonConf;
+    private ArrayList<NetworkList> groups = new ArrayList<NetworkList>();
+    public static final String GROUP_DELIMITER_NAME = "-RIVERGAGES-DAS";
+    
+    //PDT File used in case there is no description when displaying
+    //the records in the table
+    private StringToken pdtFileArg = new StringToken("T", 
+        "pdt file (full file system path, " +
+        "fill descriptions if empty and PDT Owner Column)", "",
+        TokenOptions.optSwitch, 
+        "$LRGSHOME/pdt");
+    //NWS File used to get information for the dcps
+    private StringToken nwsFileArg = new StringToken("w", 
+        "nwshb5 file (full file system path, " +
+        "fill NWSHB5 name column and NWS description Column)", "",
+        TokenOptions.optSwitch, 
+        "$DECODES_INSTALL_DIR/hads");
+    private boolean _shutdown = false;
 
-	private DcpMonitorConfig dcpmonConfig = new DcpMonitorConfig();
+    private DcpMonitorConfig dcpmonConfig = new DcpMonitorConfig();
 
 
 
-	/** Constructor. Initialize class */
-	public DuplicateDcpsGUI()
-	{
-		super("dupdcps.log");
-	}
+    /** Constructor. Initialize class */
+    public DuplicateDcpsGUI()
+    {
+        super("dupdcps.log");
+    }
 
-	/**
-	 * Read Group names from dcpmon.conf
-	 * Read Decodes Database.
-	 * Create the DuplicateDcpsFrame class.
-	 */
-	public void init()
-	{
-		LoadingAppDAI loadingAppDAO = Database.getDb().getDbIo().makeLoadingAppDAO();
-		try
-		{
-			CompAppInfo dcpmonAppInfo = loadingAppDAO.getComputationApp("dcpmon");
-			dcpmonConfig.loadFromProperties(dcpmonAppInfo.getProperties());
-			groups.clear();
-			for(Enumeration en = dcpmonAppInfo.getPropertyNames(); en.hasMoreElements(); )
-			{
-				String nm = (String)en.nextElement();
-				if (nm.toLowerCase().startsWith("grp:"))
-				{
-					String listName = nm.substring(4);
-					String displayName = dcpmonAppInfo.getProperty(nm);
-					NetworkList netlist = Database.getDb().networkListList.find(listName);
-					if (netlist == null)
-					{
-						System.err.println("AppInfo specifies non-existent network list '" + nm + "'");
-						continue;
-					}
-					groups.add(netlist);
-				}
-			}
-		}
-		catch (Exception e)
-		{
-			System.err.println("Error in init: " + e);
-			e.printStackTrace();
-		}
-		finally
-		{
-			loadingAppDAO.close();
-		}
-		
-		dupDcpsFrame = new DuplicateDcpsFrame(this);
-		
-		// TODO - use my status files to track changes to size & position.
-		// Center the window
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Dimension frameSize = dupDcpsFrame.getSize();
-		if (frameSize.height > screenSize.height)
-		{
-			frameSize.height = screenSize.height;
-		}
-		if (frameSize.width > screenSize.width)
-		{
-			frameSize.width = screenSize.width;
-		}
-		dupDcpsFrame.setLocation((screenSize.width - frameSize.width) / 2,
-				(screenSize.height - frameSize.height) / 2);
-	}
+    /**
+     * Read Group names from dcpmon.conf
+     * Read Decodes Database.
+     * Create the DuplicateDcpsFrame class.
+     */
+    public void init()
+    {
+        LoadingAppDAI loadingAppDAO = Database.getDb().getDbIo().makeLoadingAppDAO();
+        try
+        {
+            CompAppInfo dcpmonAppInfo = loadingAppDAO.getComputationApp("dcpmon");
+            dcpmonConfig.loadFromProperties(dcpmonAppInfo.getProperties());
+            groups.clear();
+            for(Enumeration en = dcpmonAppInfo.getPropertyNames(); en.hasMoreElements(); )
+            {
+                String nm = (String)en.nextElement();
+                if (nm.toLowerCase().startsWith("grp:"))
+                {
+                    String listName = nm.substring(4);
+                    String displayName = dcpmonAppInfo.getProperty(nm);
+                    NetworkList netlist = Database.getDb().networkListList.find(listName);
+                    if (netlist == null)
+                    {
+                        System.err.println("AppInfo specifies non-existent network list '" + nm + "'");
+                        continue;
+                    }
+                    groups.add(netlist);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("Error in init: " + e);
+            e.printStackTrace();
+        }
+        finally
+        {
+            loadingAppDAO.close();
+        }
+        
+        dupDcpsFrame = new DuplicateDcpsFrame(this);
+        
+        // TODO - use my status files to track changes to size & position.
+        WindowUtility.center(dupDcpsFrame);
+    }
 
-	
-	/** Return the PDT file full path */
-	public String getPdtFilePath()
-	{
-		return EnvExpander.expand(pdtFileArg.getValue());
-	}
-	
-	
-	/** Return the NWS file full path */
-	public String getNwsFilePath()
-	{
-		return EnvExpander.expand(nwsFileArg.getValue());
-	}
+    
+    /** Return the PDT file full path */
+    public String getPdtFilePath()
+    {
+        return EnvExpander.expand(pdtFileArg.getValue());
+    }
+    
+    
+    /** Return the NWS file full path */
+    public String getNwsFilePath()
+    {
+        return EnvExpander.expand(nwsFileArg.getValue());
+    }
 
-	/** Main method */
-	public static void main(String[] args)
-		throws Exception
-	{
-		DuplicateDcpsGUI dupDcp = new DuplicateDcpsGUI();
-		dupDcp.execute(args);
-	}
+    /** Main method */
+    public static void main(String[] args)
+        throws Exception
+    {
+        DuplicateDcpsGUI dupDcp = new DuplicateDcpsGUI();
+        dupDcp.execute(args);
+    }
 
-	@Override
-	protected void runApp() throws Exception
-	{
-		init();
-		dupDcpsFrame.setVisible(true);
-		while(!_shutdown)
-		{
-			try { Thread.sleep(1000L); } catch(Exception ex) {}
-		}
-	}
+    @Override
+    protected void runApp() throws Exception
+    {
+        init();
+        dupDcpsFrame.setVisible(true);
+        while(!_shutdown)
+        {
+            try { Thread.sleep(1000L); } catch(Exception ex) {}
+        }
+    }
 
-	@Override
-	protected void addCustomArgs(CmdLineArgs cmdLineArgs)
-	{
-		cmdLineArgs.addToken(pdtFileArg);
-		cmdLineArgs.addToken(nwsFileArg);
-	}
+    @Override
+    protected void addCustomArgs(CmdLineArgs cmdLineArgs)
+    {
+        cmdLineArgs.addToken(pdtFileArg);
+        cmdLineArgs.addToken(nwsFileArg);
+    }
 
-	public ArrayList<NetworkList> getGroups()
-	{
-		return groups;
-	}
+    public ArrayList<NetworkList> getGroups()
+    {
+        return groups;
+    }
 
-	public void shutdown()
-	{
-		this._shutdown = true;
-	}
+    public void shutdown()
+    {
+        this._shutdown = true;
+    }
 }
