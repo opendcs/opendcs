7202ff3865285873e6e1cebc549d3dfb0bd0a9ee
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-02-26 23:22:23 +0000

    Began work on moving the tasklist querying out of OpenTimeSeriesDao.

--- src/main/java/opendcs/opentsdb/OpenHydroDbTaskListDao.java
+++ src/main/java/opendcs/opentsdb/OpenHydroDbTaskListDao.java
@@ -3,7 +3,11 @@ package opendcs.opentsdb;
 import static org.slf4j.helpers.Util.getCallingClass;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.opendcs.tsdb.TaskListEntry;
@@ -12,12 +16,21 @@ import org.slf4j.LoggerFactory;
 
 import decodes.sql.DbKey;
 import decodes.tsdb.DbIoException;
+import decodes.tsdb.NoSuchObjectException;
+import decodes.tsdb.TimeSeriesIdentifier;
 import opendcs.dao.DatabaseConnectionOwner;
 import opendcs.dao.TaskListDao;
 
 public class OpenHydroDbTaskListDao extends TaskListDao
 {
     private static final Logger log = LoggerFactory.getLogger(getCallingClass());
+    final String getTaskListStmtQuery = 
+			"select a.RECORD_NUM, a.TS_ID, a.num_value, a.txt_value a.sample_time, a.LOADING_APPLICATION_ID "
+			+ "a.DELETE_FLAG, a.flags, a.date_time_loaded, a.fail_time "
+			+ "from CP_COMP_TASKLIST a "
+            + ""
+			+ "where a.LOADING_APPLICATION_ID = ?";
+	
 
     public OpenHydroDbTaskListDao(DatabaseConnectionOwner dco)
     {
@@ -37,7 +50,82 @@ public class OpenHydroDbTaskListDao extends TaskListDao
     @Override
     public List<TaskListEntry> getEntriesFor(DbKey appId, int amount, boolean includeFailed) throws DbIoException
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEntriesFor'");
+        String withFailClause = getTaskListStmtQuery + getFailTimeClause(includeFailed);
+        final String withSortAndLimit = withFailClause + 
+            (db.isOracle() ? " and ROWNUM < ? order by a.ts_id, a.sample_time"
+                           : " order by a.ts_id, a.sample_time limit ?"
+            );
+        try
+        {
+            return getResultsIgnoringNull(withSortAndLimit, rs -> rs2tasklistEntry(rs), appId, amount);
+        }
+        catch (SQLException ex)
+        {
+            throw new DbIoException("Unable to retrieve task list entries.", ex);
+        }
+    }
+
+    private TaskListEntry rs2tasklistEntry(ResultSet rs) throws SQLException
+    {
+        TaskListEntry entry = null;
+        long recordNum = rs.getLong("record_num");
+        DbKey tsKey = DbKey.createDbKey(rs, "ts_id");
+        boolean deleteFlag = rs.getString("delete_flag").equals("TRUE") ? true : false;
+        Date sampleTime = new Date(rs.getLong("sample_time"));
+        Date dateTimeLoad = new Date(rs.getLong("date_time_loaded"));
+        Date failTime = null;
+        DbKey loadingApp = DbKey.createDbKey(rs, "loading_applicaton_id");
+        long ft = rs.getLong("fail_time");
+        if(!rs.wasNull())
+        {
+            failTime = new Date(ft);
+        }
+        int flags = rs.getInt("flags");
+        /**
+         * NOTE: this isn't practical to due in a join yet. The OpenHydroDb needs a view for the ts_spec table
+         * (Or alternatively the query just gets really messy looking which is probably fine.)
+         */
+        try(OpenTimeSeriesDAO tsDao = (OpenTimeSeriesDAO)this.db.makeTimeSeriesDAO())
+        {
+            final TimeSeriesIdentifier tsId = tsDao.getTimeSeriesIdentifier(tsKey);
+
+            double val = rs.getDouble("num_value");
+            if (!rs.wasNull())
+            {
+                entry = new OpenHydroDbNumericTaskListEntry(recordNum, loadingApp, tsId, dateTimeLoad,
+                                                            sampleTime, failTime, null, val, flags, deleteFlag);
+            }
+            else
+            {
+                String txtVal = rs.getString("txt_value");
+                if(!rs.wasNull())
+                {
+                    log.warn("A text value was present in the text list. This is not yet supported.");
+                }
+            }
+        }
+        catch (DbIoException ex)
+        {
+            // how to we mark as bad (perhaps a generic BadTaskListEntry?)
+            throw new SQLException("Unable to retrieve time series identifier for a tasklist entry.", ex);
+        }
+        catch (NoSuchObjectException ex)
+        {
+            log.atWarn()
+               .setCause(ex)
+               .log("No time series exists for ts id {}", ex);
+        }
+        return entry;
+    }
+
+    private String getFailTimeClause(boolean includeFailed)
+    {
+        String retVal = " and a.FAIL_TIME is null";
+        if (includeFailed)
+        {
+            retVal = db.isOracle() ? " and (a.FAIL_TIME is null OR (current_timestamp -  a.FAIL_TIME) >= 3600000)"
+                                : " and (a.FAIL_TIME is null OR (extract(epoch from now())*100) - a.FAIL_TIME >= 3600000)" ;
+        }
+        return retVal;
     }
 }
