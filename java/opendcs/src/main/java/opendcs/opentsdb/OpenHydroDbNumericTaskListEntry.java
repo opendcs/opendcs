20b480f78bd9676550c5433ded12fe241e2011ae
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-02-26 23:22:23 +0000

    Began work on moving the tasklist querying out of OpenTimeSeriesDao.

--- java/opendcs/src/main/java/opendcs/opentsdb/OpenHydroDbNumericTaskListEntry.java
+++ java/opendcs/src/main/java/opendcs/opentsdb/OpenHydroDbNumericTaskListEntry.java
@@ -1,6 +1,7 @@
 package opendcs.opentsdb;
 
 import java.util.Date;
+import java.util.Objects;
 
 import org.opendcs.tsdb.TaskListEntry;
 
@@ -9,67 +10,82 @@ import decodes.tsdb.TimeSeriesIdentifier;
 
 public class OpenHydroDbNumericTaskListEntry implements TaskListEntry
 {
-    long recordNum;
-    DbKey loadingApp;
-    TimeSeriesIdentifier tsid;
-    Date loadedTime;
-    Date sampleTime;
-    Date failTime;
-    DbKey source;
-    double value;
-    int flags;
+    private final long recordNum;
+    private final DbKey loadingApp;
+    private final TimeSeriesIdentifier tsId;
+    private final Date loadedTime;
+    private final Date sampleTime;
+    private final Date failTime;
+    private final TsDataSource source;
+    private final double value;
+    private final int flags;
+    private final boolean deleted;
+
+    public OpenHydroDbNumericTaskListEntry(long recordNum, DbKey loadingApp, TimeSeriesIdentifier tsId, Date loadedTime,
+                                           Date sampleTime, Date failTime, TsDataSource source, double value, int flags, boolean deleted)
+    {
+        this.recordNum = recordNum;
+        this.loadingApp = Objects.requireNonNull(loadingApp, "A loading application is required.");
+        this.tsId = Objects.requireNonNull(tsId, "A timeseries identifier is required.");
+        this.loadedTime = Objects.requireNonNull(loadedTime, "Loaded time is required.");
+        this.sampleTime = Objects.requireNonNull(sampleTime, "A sample time is required");
+        this.failTime = failTime;
+        this.source = source; //TODO: NOT NULL
+        this.value = value; // TODO: can this be null? should for missing
+        this.flags = flags;
+        this.deleted = deleted;
+    }
+
 
     @Override
     public long getRecordNum()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecordNum'");
+        return this.recordNum;
     }
 
     @Override
     public DbKey getLoadingApp()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLoadingApp'");
+        return this.loadingApp;
     }
 
     @Override
     public TimeSeriesIdentifier getTimeSeriesIdentifier()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTimeSeriesIdentifier'");
+        return this.tsId;
     }
 
     @Override
     public Date getLoadedTime()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getLoadedTime'");
+        return this.loadedTime;
     }
 
     @Override
     public Date getSampleTime()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSampleTime'");
+        return this.sampleTime;
     }
 
     @Override
     public Date getFailTime()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getFailTime'");
+        return this.failTime;
     }
 
     @Override
     public boolean isDeleted()
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDeleted'");
+        return this.deleted;
     }
     
-    public int flags()
+    public int getFlags()
     {
-        return -1;
+        return this.flags;
+    }
+
+    public TsDataSource getSource()
+    {
+        return this.source;
     }
 }
