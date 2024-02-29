534cc868586f8d8d00b32bc760c64393e1d14c99
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-02-29 21:00:37 +0000

    Improvements to handling operations within a transaction.

--- src/main/java/opendcs/opentsdb/OpenTimeSeriesDAO.java
+++ src/main/java/opendcs/opentsdb/OpenTimeSeriesDAO.java
@@ -830,72 +830,90 @@ public class OpenTimeSeriesDAO
 	public void deleteTimeSeries(TimeSeriesIdentifier tsid)
 		throws DbIoException
 	{
-		AlarmDAI alarmDAO = db.makeAlarmDAO();
-		try
+		final CwmsTsId ctsid = (CwmsTsId)tsid;
+		try (AlarmDAI alarmDAO = db.makeAlarmDAO();
+			 CompDependsDAI compDependsDAO = db.makeCompDependsDAO();)
 		{
-			alarmDAO.deleteCurrentAlarm(tsid.getKey(), null);
-			alarmDAO.deleteHistoryAlarms(tsid.getKey(), null, null);
-		}
-		catch(Exception ex)
-		{
-			warning("deleteTimeSeries error deleting alarm records: " + ex);
-		}
-		finally
-		{
-			alarmDAO.close();
-		}
-
-		CwmsTsId ctsid = (CwmsTsId)tsid;
-		String tableName = makeDataTableName(ctsid);
-		String q = "delete from " + tableName
-			+ " where ts_id = " + ctsid.getKey()
-			+ " and flags & " + CwmsFlags.PROTECTED + " = 0 ";
-		doModify(q);
-		doModify("delete from ts_property where ts_id = " + ctsid.getKey());
-		
-		CompDependsDAI compDependsDAO = db.makeCompDependsDAO();
-		try
-		{
-			compDependsDAO.deleteCompDependsForTsKey(ctsid.getKey());
-		}
-		catch(Exception ex)
-		{
-			warning("deleteTimeSeries error deleting computation dependencies: " + ex);
-		}
-		finally { compDependsDAO.close(); }
-		
-		doModify("delete from cp_comp_tasklist where ts_id = " + ctsid.getKey());
-		
-		doModify("delete from ts_spec where ts_id = " + ctsid.getKey());
-		
-		q = "select num_ts_present, est_annual_values from storage_table_list "
-			+ "where table_num = " + ctsid.getStorageTable();
-		ResultSet rs = doQuery(q);
-		try
-		{
-			if (rs != null && rs.next())
+			this.inTransaction(dao ->
 			{
-				int num = rs.getInt(1);
-				int estValues = rs.getInt(2);
-				num--;
-				estValues -= interval2estAnnualValues(ctsid.getIntervalOb());
-				q = "update storage_table_list set num_ts_present = " + num
-					+ ", est_annual_values = " + estValues
-					+ " where table_num = "+ ctsid.getStorageTable();
-				doModify(q);
-			}
+				alarmDAO.inTransactionOf(dao);
+				compDependsDAO.inTransactionOf(dao);
+				try
+				{
+					alarmDAO.deleteCurrentAlarm(tsid.getKey(), null);
+					alarmDAO.deleteHistoryAlarms(tsid.getKey(), null, null);
+				}
+				catch(Exception ex)
+				{
+					throw new Exception("error deleting alarm records.", ex);
+				}
+
+				try
+				{
+					String tableName = makeDataTableName(ctsid);
+					String q = "delete from " + tableName
+						+ " where ts_id = ?"
+						+ " and flags & ? = 0 ";
+					dao.doModify(q, ctsid.getKey(), CwmsFlags.PROTECTED);
+					dao.doModify("delete from ts_property where ts_id = ?", ctsid.getKey());
+					compDependsDAO.deleteCompDependsForTsKey(ctsid.getKey());
+				}
+				catch(Exception ex)
+				{
+					throw new Exception("deleteTimeSeries error deleting computation dependencies.",ex);
+				}
+
+				try
+				{
+					dao.doModify("delete from cp_comp_tasklist where ts_id = ?", ctsid.getKey());
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Error removing tasklist entries", ex);
+				}
+
+				try
+				{
+					dao.doModify("delete from ts_spec where ts_id = ?", ctsid.getKey());
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Error deleting ts_spec entry", ex);
+				}
+
+				try
+				{
+					String q = "select num_ts_present, est_annual_values from storage_table_list "
+					+ "where table_num = ?" + ctsid.getStorageTable();
+					int num_andEstimate[] = getSingleResult(q, rs -> new int[]{ rs.getInt(1), rs.getInt(2)}, ctsid.getStorageTable());
+					num_andEstimate[0]--;
+					num_andEstimate[1] -= interval2estAnnualValues(ctsid.getIntervalOb());
+					q = "update storage_table_list set num_ts_present = ?"
+						+ ", est_annual_values = ?"
+						+ " where table_num = ?";
+					doModify(q, num_andEstimate[0], num_andEstimate[1], ctsid.getStorageTable());
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Unable to modify storage table", ex);
+				}
+
+				try(CompDependsNotifyDAI dai = db.makeCompDependsNotifyDAO())
+				{
+					CpDependsNotify cdn = new CpDependsNotify();
+					cdn.setEventType(CpDependsNotify.TS_DELETED);
+					cdn.setKey(ctsid.getKey());
+					dai.saveRecord(cdn);
+				}
+				catch (Exception ex)
+				{
+					throw new Exception("Unable to create comp depends notification.", ex);
+				}
+			});
 		}
-		catch(SQLException ex)
+		catch (Exception ex)
 		{
-			warning("Error in query '" + q + "': " + ex);
-		}
-		
-		try(CompDependsNotifyDAI dai = db.makeCompDependsNotifyDAO())
-		{
-			CpDependsNotify cdn = new CpDependsNotify();
-			cdn.setEventType(CpDependsNotify.TS_DELETED);
-			cdn.setKey(ctsid.getKey());
-			dai.saveRecord(cdn);
+			throw new DbIoException(String.format("Error deleting time series. %s",tsid.toString()), ex);
 		}
 	}
 
