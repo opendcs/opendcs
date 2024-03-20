c30fd7cf4de367dbda453a85a209ddbccd8b8636
M. Allan Neilson <m.allan.neilson@gmail.com>
2024-03-20 13:46:56 -0700

    Start of framework for LRGS.

--- src/test-integration/java/org/opendcs/fixtures/lrgs/LrgsTestInstance.java
+++ src/test-integration/java/org/opendcs/fixtures/lrgs/LrgsTestInstance.java
@@ -1,17 +1,33 @@
 package org.opendcs.fixtures.lrgs;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
+import ilex.util.FileLogger;
+import ilex.util.QueueLogger;
 import lrgs.archive.MsgArchive;
 import lrgs.lrgsmain.LrgsMain;
 
 public class LrgsTestInstance
 {
-    private LrgsMain lrgs;
-    private MsgArchive archive;
+    final private LrgsMain lrgs;
+    final private MsgArchive archive;
+    final private QueueLogger queueLogger;
+    final private FileLogger fileLogger;
+    final private File configFile;
 
-    public LrgsTestInstance(File lrgsHome)
+    public LrgsTestInstance(File lrgsHome) throws FileNotFoundException
     {
-        //lrgs = new LrgsMain()
+        if (!lrgsHome.canRead())
+        {
+            throw new FileNotFoundException(
+                String.format("Directory '%s' doesn't exist or can't be read.", lrgsHome.getAbsolutePath())
+            );
+        }
+        configFile = new File(lrgsHome,"lrgsconf");
+        queueLogger = new QueueLogger("");
+        fileLogger = new FileLogger("lrgs", new File(lrgsHome,"lrgslog").getAbsolutePath(), 200*1024*1024);
+        lrgs = new LrgsMain(queueLogger,"-", configFile.getAbsolutePath(), fileLogger);
+        this.archive = lrgs.msgArchive;
     }
 }
