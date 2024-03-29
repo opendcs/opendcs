fe836f205b7e88ac5efc54b2d0803b916fc2d728
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-03-29 22:14:06 +0000

    Add queue and create writer thread for simple async logging.

--- src/main/java/decodes/dbeditor/TraceLogger.java
+++ src/main/java/decodes/dbeditor/TraceLogger.java
@@ -1,6 +1,11 @@
 
 package decodes.dbeditor;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import ilex.util.Logger;
 
 /**
@@ -11,6 +16,10 @@ public class TraceLogger extends Logger
 	/** The trace dialog. */
 	TraceDialog dlg;
 
+	private BlockingQueue<String> queue = new ArrayBlockingQueue<>(5000);
+	private Thread writerThread;
+	private AtomicBoolean closeOperations = new AtomicBoolean(false);
+
 	/**
 	 Constructor.
 	*/
@@ -18,6 +27,28 @@ public class TraceLogger extends Logger
 	{
 		super(procName);
 		this.dlg = null;
+		writerThread = new Thread(() ->
+		{
+			while (closeOperations.get() == false)
+			{
+				try
+				{
+					String line = queue.poll(1, TimeUnit.SECONDS);
+					if (dlg != null)
+					{
+						dlg.addText(line);
+					}
+				}
+				catch (InterruptedException ex)
+				{
+					// go back to operation, in this scenario most likely closeOperations is true and
+					// and we're ditching the logging
+				}
+			}
+		},
+		"TraceDialog-Writer");
+		writerThread.setDaemon(true);
+		writerThread.start();
 	}
 
 	/**
@@ -32,11 +63,11 @@ public class TraceLogger extends Logger
 	/** Does nothing. */
 	public void close()
 	{
+		closeOperations.set(true);
 	}
 
 	public synchronized void doLog(int priority, String text)
 	{
-		if (dlg != null)
-			dlg.addText(text + "\n");
+		queue.add(text + "\n");
 	}
 }
