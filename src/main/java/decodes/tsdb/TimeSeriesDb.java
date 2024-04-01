c707b6e962e7c72e7ff6cce92fae4c23e50e5902
M. Allan Neilson <m.allan.neilson@gmail.com>
2024-04-01 15:13:09 -0700

    Fix logic mishap.

--- src/main/java/decodes/tsdb/TimeSeriesDb.java
+++ src/main/java/decodes/tsdb/TimeSeriesDb.java
@@ -597,7 +597,7 @@ public abstract class TimeSeriesDb extends Database
         {
             throw new DatabaseException("Unable to initialize database.", ex);
         }
-        cpCompDepends_col1 = isHdb() ? "TS_ID" : "SITE_DATATYPE_ID";
+        cpCompDepends_col1 = isHdb() ? "SITE_DATATYPE_ID" : "TS_ID";
         getLogDateFormat().setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
