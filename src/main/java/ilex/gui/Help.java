e9501c68d525c0394ce0522a030ef31d3de58f0a
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-05-31 16:33:01 -0700

    Log error

--- src/main/java/ilex/gui/Help.java
+++ src/main/java/ilex/gui/Help.java
@@ -3,13 +3,12 @@ package ilex.gui;
 import javax.swing.*;
 import java.awt.Desktop;
 import java.net.URI;
-
-
+import org.slf4j.LoggerFactory;
 
 public class Help
 {
     static final String HELP_URL = "https://opendcs-env.readthedocs.io/";
-
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(Help.class);
     public static void open(){
         try {
             if(  Desktop.isDesktopSupported())
@@ -18,7 +17,10 @@ public class Help
             }
         }
         catch( Throwable e) {
-            JOptionPane.showMessageDialog(null, "Error opening "+HELP_URL, "Error", JOptionPane.ERROR_MESSAGE);
+            if(  Desktop.isDesktopSupported()){
+                JOptionPane.showMessageDialog(null, "Error opening " + HELP_URL, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            log.error("Error opening '{}' ",HELP_URL,e);
         }
     }
 
