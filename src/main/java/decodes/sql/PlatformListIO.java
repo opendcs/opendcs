efdca0091a726cf9c208f5202023cb88a7026f78
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-06-05 15:09:53 -0700

    clean imports. issue #523

--- src/main/java/decodes/sql/PlatformListIO.java
+++ src/main/java/decodes/sql/PlatformListIO.java
@@ -1,9 +1,7 @@
 package decodes.sql;
 
-import java.sql.Array;
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLDataException;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -11,7 +9,6 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.Vector;
 
-import decodes.db.Database;
 import opendcs.dai.DacqEventDAI;
 import opendcs.dai.PlatformStatusDAI;
 import opendcs.dai.PropertiesDAI;
