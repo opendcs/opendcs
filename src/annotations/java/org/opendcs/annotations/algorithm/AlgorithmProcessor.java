c6d66f5c63f8e019d57596228da34601ae7f5276
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-06-18 15:34:14 +0000

    Update/Add copyright notice to files.

--- src/annotations/java/org/opendcs/annotations/algorithm/AlgorithmProcessor.java
+++ src/annotations/java/org/opendcs/annotations/algorithm/AlgorithmProcessor.java
@@ -1,17 +1,27 @@
+/**
+ * Copyright 2024 The OpenDCS Consortium and contributors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.opendcs.annotations.algorithm;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.Element;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
