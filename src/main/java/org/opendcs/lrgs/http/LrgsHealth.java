48a7140307570687878210ab34d3b498eea716b3
Michael Neilson <m.allan.neilson@gmail.com>
2024-07-07 10:16:35 -0700

    Jax-RS implementation of health and status behave as expected.

--- src/main/java/org/opendcs/lrgs/http/LrgsHealth.java
+++ src/main/java/org/opendcs/lrgs/http/LrgsHealth.java
@@ -1,10 +1,10 @@
 package org.opendcs.lrgs.http;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -18,23 +18,24 @@ import lrgs.lrgsmain.LrgsMain;
 public class LrgsHealth
 {
     private static final Logger log = LoggerFactory.getLogger(LrgsHealth.class);
+
     @Context
-    Configuration configuration;
+    ServletContext servletContext;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response get()
     {
         log.info("Sending Health status.");
-        LrgsMain lrgs = (LrgsMain)configuration.getProperty("lrgs");
+        LrgsMain lrgs = (LrgsMain)servletContext.getAttribute("lrgs");
         if (lrgs != null && lrgs.getDdsServer().statusProvider.getStatusSnapshot().isUsable)
         {
-            return Response.ok("Active").build();
+            return Response.ok("\"Active\"").build();
         }
         else
         {
             return Response.status(HttpServletResponse.SC_SERVICE_UNAVAILABLE)
-                           .entity("Inactive")
+                           .entity("\"Inactive\"")
                            .build();
         }
     }
