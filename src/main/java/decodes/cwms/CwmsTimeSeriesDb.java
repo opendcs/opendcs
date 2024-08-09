6ed885905789560f1179e0fce919211af7086109
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-08-09 17:33:14 +0000

    Correct logic in GUI login sequence for CWMS.

--- src/main/java/decodes/cwms/CwmsTimeSeriesDb.java
+++ src/main/java/decodes/cwms/CwmsTimeSeriesDb.java
@@ -826,7 +826,7 @@ public class CwmsTimeSeriesDb
 				if (!cgl.isLoginSuccess())
 				{
 					cgl.doLogin(null);
-					if (!cgl.isLoginSuccess()) // user hit cancel
+					if (!cgl.wasOk()) // user hit cancel
 					{
 						throw new BadConnectException("Login aborted by user.");
 					}
