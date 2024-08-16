718355a17032e317a34be5ffcee697ba92e98587
Michael Neilson <michael.a.neilson@usace.army.mil>
2024-08-16 14:24:23 -0700

    Use wild card classpath.

--- install/bin/decj.bat
+++ install/bin/decj.bat
@@ -7,13 +7,12 @@ pushd "%BIN_PATH%.."
 set "APP_PATH=%CD%"
 popd
 
-set "CLASSPATH=%BIN_PATH%opendcs.jar;%BIN_PATH%hibernate.cfg.xml;%APP_PATH%/dep/*"
-
+set "CLASSPATH=%BIN_PATH%opendcs.jar;%BIN_PATH%hibernate.cfg.xml"
 
 set "CLASSPATH=!CLASSPATH!"
 
 if not defined DCSTOOL_USERDIR (
-  set DCSTOOL_USERDIR="%APPDATA%\.opendcs"
+  set "DCSTOOL_USERDIR=%APPDATA%\.opendcs"
 )
 
 if not exist %DCSTOOL_USERDIR%\ (
@@ -23,9 +22,9 @@ if not exist %DCSTOOL_USERDIR%\ (
   copy %APP_PATH%\decodes.properties %DCSTOOL_USERDIR%\user.properties
   xcopy %APP_PATH%\edit-db %DCSTOOL_USERDIR%\edit-db /E /I
 )
-REM Use short name as the classpath has gotten beyond the windows environment variable support
-if exists %DCSTOOL_USERDIR%/dep/ do (
-  set "CLASSPATH=!CLASSPATH!;%DCSTOOL_USERDIR%/dep"
+
+if exist "%DCSTOOL_USERDIR%/dep\" do (
+  set "CLASSPATH=!CLASSPATH!;%DCSTOOL_USERDIR%/dep/*;%APP_PATH%/dep/*"
   )
 
 java -Xmx240m %DECJ_MAXHEAP% %DECJ_OPTS% -cp "!CLASSPATH!" -DDCSTOOL_HOME="%APP_PATH%" -DDECODES_INSTALL_DIR="%APP_PATH%" -DDCSTOOL_USERDIR="%DCSTOOL_USERDIR%" %*%
