7574e9b021c302ddb3bf7197eedd817a0483375c
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-08-22 15:20:34 +0000

    Redirect message to error channel. Bring back CP_SHARED path.

--- install/bin/decj.bat
+++ install/bin/decj.bat
@@ -16,15 +16,24 @@ if not defined DCSTOOL_USERDIR (
 )
 
 if not exist %DCSTOOL_USERDIR%\ (
-  echo "Creating Local User Directory and initial properties in %DCSTOOL_USERDIR%"
-  echo "The default XML database has been copied to this directory."
+  echo "Creating Local User Directory and initial properties in %DCSTOOL_USERDIR%" 1>&2
+  echo "The default XML database has been copied to this directory." 1>&2
   mkdir %DCSTOOL_USERDIR%
   copy %APP_PATH%\decodes.properties %DCSTOOL_USERDIR%\user.properties
   xcopy %APP_PATH%\edit-db %DCSTOOL_USERDIR%\edit-db /E /I
 )
 
+if exist "%CP_SHARED_JAR_DIR" do (
+  set "CLASSPATH=!CLASSPATH!;%CP_SHARED_JAR_DIR/*"
+  echo "The need of this is superseded by using a DCSTOOL_USERDIR for configuration." 1>&2
+  echo "Support for this variable will be moved in a future release. Please update " 1>&2
+  echo "your configuration." 1>&2
+)
+
 if exist "%DCSTOOL_USERDIR%/dep\" do (
-  set "CLASSPATH=!CLASSPATH!;%DCSTOOL_USERDIR%/dep/*;%APP_PATH%/dep/*"
+  set "CLASSPATH=!CLASSPATH!;%DCSTOOL_USERDIR%/dep/*"
   )
+
+set "CLASSPATH=!CLASSPATH!;%APP_PATH%/dep/*"
 
 java -Xmx240m %DECJ_MAXHEAP% %DECJ_OPTS% -cp "!CLASSPATH!" -DDCSTOOL_HOME="%APP_PATH%" -DDECODES_INSTALL_DIR="%APP_PATH%" -DDCSTOOL_USERDIR="%DCSTOOL_USERDIR%" %*%
