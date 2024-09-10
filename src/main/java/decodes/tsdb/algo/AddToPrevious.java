57d2cdec7b55e32d07e5f5263315e5652bf49c3a
David Ho <david.ho@usace.army.mil>
2024-09-10 11:31:10 -0700

    Add annotations to AddToPrevious

--- src/main/java/decodes/tsdb/algo/AddToPrevious.java
+++ src/main/java/decodes/tsdb/algo/AddToPrevious.java
@@ -1,82 +1,50 @@
 package decodes.tsdb.algo;
 
-import java.util.Date;
-
 import opendcs.dai.TimeSeriesDAI;
 
-import ilex.var.NamedVariableList;
 import ilex.var.NamedVariable;
 import ilex.var.TimedVariable;
-import decodes.tsdb.BadTimeSeriesException;
 import decodes.tsdb.CTimeSeries;
-import decodes.tsdb.DbAlgorithmExecutive;
 import decodes.tsdb.DbCompException;
-import decodes.tsdb.DbIoException;
 import decodes.tsdb.ParmRef;
-import decodes.tsdb.VarFlags;
+import org.opendcs.annotations.PropertySpec;
+import org.opendcs.annotations.algorithm.Algorithm;
+import org.opendcs.annotations.algorithm.Input;
+import org.opendcs.annotations.algorithm.Output;
 
-//AW:IMPORTS
-// Place an import statements you need here.
-//AW:IMPORTS_END
+@Algorithm(
+		description = "Adds the current value to the previous value in the database\n" +
+				"\t\tand outputs the sum. Works on any time-series, any interval.\n" +
+				"\t\tThis algorithm does assume that you are calling it with a \n" +
+				"\t\tseries of contiguous values, like you would get out of a DCP\n" +
+				"\t\tmessage.")
 
-//AW:JAVADOC
-/**
-Adds the current value to the previous value in the database
-and outputs the sum. Works on any time-series, any interval.
-This algorithm does assume that you are calling it with a 
-series of contiguous values, like you would get out of a DCP
-message.
- */
-//AW:JAVADOC_END
-public class AddToPrevious
-	extends decodes.tsdb.algo.AW_AlgorithmBase
+public class AddToPrevious extends decodes.tsdb.algo.AW_AlgorithmBase
 {
-//AW:INPUTS
-	public double input;	//AW:TYPECODE=i
-	String _inputNames[] = { "input" };
-//AW:INPUTS_END
-
-//AW:LOCALVARS
-	// Enter any local class variables needed by the algorithm.
+	@Input
+	public double input;
 	private double prevVal = 0.0;
 	private boolean justStarted = true;
-//AW:LOCALVARS_END
 
-//AW:OUTPUTS
+	@Output
 	public NamedVariable output = new NamedVariable("output", 0);
-	String _outputNames[] = { "output" };
-//AW:OUTPUTS_END
-
-//AW:PROPERTIES
-	String _propertyNames[] = { };
-//AW:PROPERTIES_END
 
 	// Allow javac to generate a no-args constructor.
 
 	/**
 	 * Algorithm-specific initialization provided by the subclass.
 	 */
-	protected void initAWAlgorithm( )
-		throws DbCompException
-	{
-//AW:INIT
+	protected void initAWAlgorithm()
+			throws DbCompException {
 		_awAlgoType = AWAlgoType.TIME_SLICE;
-//AW:INIT_END
-
-//AW:USERINIT
-		// Code here will be run once, after the algorithm object is created.
-//AW:USERINIT_END
 	}
 	
 	/**
 	 * This method is called once before iterating all time slices.
 	 */
 	protected void beforeTimeSlices()
-		throws DbCompException
-	{
-//AW:BEFORE_TIMESLICES
+			throws DbCompException {
 		justStarted = true;
-//AW:BEFORE_TIMESLICES_END
 	}
 
 	/**
@@ -90,75 +58,34 @@ public class AddToPrevious
 	 *        algorithm is to be aborted.
 	 */
 	protected void doAWTimeSlice()
-		throws DbCompException
-	{
-//AW:TIMESLICE
-		if (justStarted)
-		{
+			throws DbCompException {
+		if (justStarted) {
 			ParmRef inputParmRef = getParmRef("input");
 			CTimeSeries inputTS = inputParmRef.timeSeries;
 			TimeSeriesDAI timeSeriesDAO = tsdb.makeTimeSeriesDAO();
-			try
-			{
-				TimedVariable prevInput = 
-					timeSeriesDAO.getPreviousValue(inputTS, 
-						inputParmRef.compParm.baseTimeToParamTime(
-							this._timeSliceBaseTime, aggCal));
+			try {
+				TimedVariable prevInput =
+						timeSeriesDAO.getPreviousValue(inputTS,
+								inputParmRef.compParm.baseTimeToParamTime(
+										this._timeSliceBaseTime, aggCal));
 				prevVal = prevInput.getDoubleValue();
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				warning("Can't get prev value, time-slice at "
-					+ debugSdf.format(_timeSliceBaseTime));
+						+ debugSdf.format(_timeSliceBaseTime));
 				prevVal = 0.0;
-			}
-			finally
-			{
+			} finally {
 				timeSeriesDAO.close();
 			}
 			justStarted = false;
 		}
 		setOutput(output, input + prevVal);
 		prevVal = input;
-//AW:TIMESLICE_END
 	}
 
 	/**
 	 * This method is called once after iterating all time slices.
 	 */
 	protected void afterTimeSlices()
-		throws DbCompException
-	{
-//AW:AFTER_TIMESLICES
-		// This code will be executed once after each group of time slices.
-		// For TimeSlice algorithms this is done once after all slices.
-		// For Aggregating algorithms, this is done after each aggregate
-		// period.
-//AW:AFTER_TIMESLICES_END
-	}
-
-	/**
-	 * Required method returns a list of all input time series names.
-	 */
-	public String[] getInputNames()
-	{
-		return _inputNames;
-	}
-
-	/**
-	 * Required method returns a list of all output time series names.
-	 */
-	public String[] getOutputNames()
-	{
-		return _outputNames;
-	}
-
-	/**
-	 * Required method returns a list of properties that have meaning to
-	 * this algorithm.
-	 */
-	public String[] getPropertyNames()
-	{
-		return _propertyNames;
+			throws DbCompException {
 	}
 }
