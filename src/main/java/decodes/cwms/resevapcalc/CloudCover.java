6be6297fbe60654d33743ace58a258c4205252a6
oskarhurst <48417811+oskarhurst@users.noreply.github.com>
2024-11-20 16:31:36 -0800

    default units and cloud cover change

--- src/main/java/decodes/cwms/resevapcalc/CloudCover.java
+++ src/main/java/decodes/cwms/resevapcalc/CloudCover.java
@@ -16,18 +16,35 @@ public class CloudCover
     public double fractionCloudCover;
     public double height;
     public CloudHeightType cloudType;
-    
-    // these are the default cloud cover values
-    // set in solflx.f
-    private static final double MISSING_VALUE_LOW = 0.54;
-    private static final double MISSING_VALUE_MED = 0.0;
-    private static final double MISSING_VALUE_HI = 0.0;
    
     public enum CloudHeightType
     {
-        HEIGHT_LOW,
-        HEIGHT_MED,
-        HEIGHT_HIGH
+        HEIGHT_LOW("Height Low", 4, 0.54),
+        HEIGHT_MED("Height Med", 3, 0.0),
+        HEIGHT_HIGH("Height High", 2, 0.0);
+
+        private final String name;
+        private final int flag;
+        private final double defaultFraction;
+
+        CloudHeightType(String name, int flag, double defaultFraction){
+            this.name = name;
+            this.flag = flag;
+            this.defaultFraction = defaultFraction;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getFlag(){
+            return flag;
+        }
+
+        public double getDefaultFraction(){
+            return defaultFraction;
+        }
+
     }
 
     public CloudCover( double fraction, double height, CloudHeightType type)
@@ -42,17 +59,9 @@ public class CloudCover
         // ICLD is cloud type.  2 means cirrus high clouds, 3 means
         // altocumulus middle clouds, and 3 means stratus low clouds.
         int[] icld = { 2, 3, 4 };
-        if ( cloudType == CloudHeightType.HEIGHT_LOW)
+        if ( cloudType != null )
         {
-            return 4;
-        }
-        else if ( cloudType == CloudHeightType.HEIGHT_MED)
-        {
-            return 3;
-        }
-        else if ( cloudType == CloudHeightType.HEIGHT_HIGH)
-        {
-            return 2;
+            return cloudType.getFlag();
         }
         
         return 3;
@@ -60,34 +69,18 @@ public class CloudCover
     
     public String getTypeName()
     {
-        if ( this.cloudType == CloudHeightType.HEIGHT_LOW)
+        if ( cloudType != null )
         {
-            return "Height Low";
-        }
-        else if ( this.cloudType == CloudHeightType.HEIGHT_MED)
-        {
-            return "Height Med";
-        }
-        else if ( this.cloudType == CloudHeightType.HEIGHT_HIGH)
-        {
-            return "Height High";
+            return cloudType.getName();
         }
         return "";
     }
     
     public double getDefaultFractionCloudCover()
     {
-         if ( this.cloudType == CloudHeightType.HEIGHT_LOW)
+        if ( cloudType != null )
         {
-            return MISSING_VALUE_LOW;
-        }
-        else if ( this.cloudType == CloudHeightType.HEIGHT_MED)
-        {
-            return MISSING_VALUE_MED;
-        }
-        else if ( this.cloudType == CloudHeightType.HEIGHT_HIGH)
-        {
-            return MISSING_VALUE_HI;
+            return cloudType.getDefaultFraction();
         }
         return 0.0;       
     }
