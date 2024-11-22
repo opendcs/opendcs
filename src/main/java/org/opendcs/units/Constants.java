8528757c5f4d6d2d8a3f845a700c2e97a071dc3e
oskarhurst <48417811+oskarhurst@users.noreply.github.com>
2024-11-22 11:58:38 -0800

    ResEvap create and delete Constants

---
+++ src/main/java/org/opendcs/units/Constants.java
@@ -0,0 +1,16 @@
+package org.opendcs.units;
+
+public class Constants {
+
+    public static final double PI  = 3.141592654;
+
+    /*  K is the von Karman constant. */
+    public static final double CONST_K = 0.4;
+
+    /*  G is the acceleration of gravity. */
+    public static final double CONST_G = 9.81;
+
+    /* Stephan-Boltzman const.(W/m^2*K^4) */
+    public static final double SIGMA = 5.669e-8;
+
+}
