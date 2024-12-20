de754803c870f13ff85ce09ec4e595bbd7993621
zack-rma <zack@rmanet.com>
2024-12-20 11:58:10 -0800

    Added data mapping with tests

---
+++ opendcs-rest-api/src/test/java/org/opendcs/odcsapi/res/OdcsapiResourcesTest.java
@@ -0,0 +1,160 @@
+package org.opendcs.odcsapi.res;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+
+import decodes.datasource.RawMessage;
+import decodes.decoder.DecodedMessage;
+import decodes.decoder.TimeSeries;
+import decodes.util.PropertySpec;
+import org.apache.commons.codec.binary.Base64;
+import org.junit.jupiter.api.Test;
+import org.opendcs.odcsapi.beans.ApiDecodedMessage;
+import org.opendcs.odcsapi.beans.ApiDecodesTSValue;
+import org.opendcs.odcsapi.beans.ApiDecodesTimeSeries;
+import org.opendcs.odcsapi.beans.ApiLogMessage;
+import org.opendcs.odcsapi.beans.ApiPropSpec;
+import org.opendcs.odcsapi.beans.ApiRawMessage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.opendcs.odcsapi.res.OdcsapiResource.map;
+
+final class OdcsapiResourcesTest
+{
+	@Test
+	void testPropertyMap()
+	{
+		ApiPropSpec[] apiPropSpecs = new ApiPropSpec[1];
+		ApiPropSpec apiPropSpec
+				= new ApiPropSpec("Specification", "Property", "A property specification");
+		apiPropSpecs[0] = apiPropSpec;
+
+		PropertySpec[] propertySpecs = map(apiPropSpecs);
+		assertNotNull(propertySpecs);
+		PropertySpec propertySpec = propertySpecs[0];
+		assertNotNull(propertySpec);
+		assertEquals(apiPropSpec.getDescription(), propertySpec.getDescription());
+		assertEquals(apiPropSpec.getName(), propertySpec.getName());
+		assertEquals(apiPropSpec.getType(), propertySpec.getType());
+	}
+
+	@Test
+	void testDecodedMessageMap() throws Exception
+	{
+		ApiDecodedMessage message = new ApiDecodedMessage();
+		message.setMessageTime(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		ArrayList<ApiLogMessage> logMessages = new ArrayList<>();
+		ApiLogMessage logMessage = new ApiLogMessage();
+		logMessage.setPriority("High");
+		logMessage.setText("Error encountered while reading depth value");
+		logMessage.setTimeStamp(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		logMessages.add(logMessage);
+		message.setLogMessages(logMessages);
+		ArrayList<ApiDecodesTimeSeries> timeSeries = new ArrayList<>();
+		ApiDecodesTimeSeries decodesTimeSeries = new ApiDecodesTimeSeries();
+		decodesTimeSeries.setSensorName("Depth Sensor");
+		decodesTimeSeries.setUnits("m");
+		decodesTimeSeries.setSensorNum(556);
+		ArrayList<ApiDecodesTSValue> values = new ArrayList<>();
+		ApiDecodesTSValue value = new ApiDecodesTSValue();
+		value.setValue("1.0");
+		value.setTime(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		values.add(value);
+		decodesTimeSeries.setValues(values);
+		message.setTimeSeries(timeSeries);
+		ApiRawMessage rawMessage = new ApiRawMessage();
+		rawMessage.setBase64(Base64.encodeBase64String("base64_encoded_text".getBytes()));
+		rawMessage.setXmitTime(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		rawMessage.setPlatformId("126484");
+
+		DecodedMessage decodedMessage = map(message, rawMessage);
+
+		assertNotNull(decodedMessage);
+		assertEquals(message.getMessageTime(), decodedMessage.getMessageTime());
+		assertEquals(rawMessage.getBase64().getBytes().length, decodedMessage.getRawMessage().getMessageData().length);
+		int i = 0;
+		for(Byte b : rawMessage.getBase64().getBytes())
+		{
+			assertEquals(b, decodedMessage.getRawMessage().getMessageData()[i]);
+			i++;
+		}
+		assertEquals(Long.parseLong(rawMessage.getPlatformId()),
+				decodedMessage.getRawMessage().getPlatform().getId().getValue());
+		assertEquals(rawMessage.getXmitTime(), decodedMessage.getRawMessage().getTimeStamp());
+		for (ApiDecodesTimeSeries timeSeries1 : message.getTimeSeries())
+		{
+			TimeSeries timeSeries1Decoded = decodedMessage.getTimeSeries(timeSeries1.getSensorNum());
+			assertNotNull(timeSeries1Decoded);
+			assertEquals(timeSeries1.getSensorName(), timeSeries1Decoded.getSensorName());
+			assertEquals(timeSeries1.getUnits(), timeSeries1Decoded.getUnits());
+			int j = 0;
+			for (ApiDecodesTSValue value1 : timeSeries1.getValues())
+			{
+				assertEquals(value1.getTime(), timeSeries1Decoded.getBeginTime());
+				assertEquals(value1.getValue(), timeSeries1Decoded.sampleAt(j).valueString());
+				j++;
+			}
+		}
+	}
+
+	@Test
+	void testRawMessageMap() throws Exception
+	{
+		ApiRawMessage rawMessage = new ApiRawMessage();
+		rawMessage.setBase64(Base64.encodeBase64String("base64_encoded_text".getBytes()));
+		rawMessage.setXmitTime(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		rawMessage.setPlatformId("126484");
+
+		RawMessage decodedMessage = map(rawMessage);
+		assertNotNull(decodedMessage);
+		assertEquals(rawMessage.getBase64().getBytes().length, decodedMessage.getMessageData().length);
+		int i = 0;
+		for(Byte b : rawMessage.getBase64().getBytes())
+		{
+			assertEquals(b, decodedMessage.getMessageData()[i]);
+			i++;
+		}
+		assertEquals(Long.parseLong(rawMessage.getPlatformId()), decodedMessage.getPlatform().getId().getValue());
+		assertEquals(rawMessage.getXmitTime(), decodedMessage.getTimeStamp());
+	}
+
+	@Test
+	void testTimeSeriesMap() throws Exception
+	{
+		ArrayList<ApiDecodesTimeSeries> timeSeries = new ArrayList<>();
+		ApiDecodesTimeSeries decodesTimeSeries = new ApiDecodesTimeSeries();
+		decodesTimeSeries.setSensorName("Flow Sensor");
+		decodesTimeSeries.setUnits("cms");
+		decodesTimeSeries.setSensorNum(556);
+		ArrayList<ApiDecodesTSValue> values = new ArrayList<>();
+		ApiDecodesTSValue value = new ApiDecodesTSValue();
+		value.setValue("1.0");
+		value.setTime(Date.from(Instant.parse("2021-07-01T00:00:00Z")));
+		values.add(value);
+		decodesTimeSeries.setValues(values);
+		timeSeries.add(decodesTimeSeries);
+
+		ArrayList<TimeSeries> decodedTimeSeries = map(timeSeries);
+
+		assertNotNull(decodedTimeSeries);
+		assertEquals(timeSeries.size(), decodedTimeSeries.size());
+		ApiDecodesTimeSeries apiTS = timeSeries.get(0);
+		TimeSeries timeSeries1Decoded = decodedTimeSeries.get(0);
+		assertNotNull(timeSeries1Decoded);
+		assertNotNull(apiTS);
+		assertEquals(apiTS.getSensorName(), timeSeries1Decoded.getSensorName());
+		assertEquals(apiTS.getUnits(), timeSeries1Decoded.getUnits());
+		int i = 0;
+		for (ApiDecodesTSValue value1 : apiTS.getValues())
+		{
+			assertEquals(value1.getTime(), timeSeries1Decoded.getBeginTime());
+			assertEquals(value1.getValue(), timeSeries1Decoded.sampleAt(i).valueString());
+			i++;
+		}
+	}
+
+
+
+}
