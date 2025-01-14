a98047d527a213fa270455c4e313d591a8a11034
Mike Neilson <m.allan.neilson@gmail.com>
2025-01-14 15:25:53 -0800

    Notes and additional information in output.

--- java/opendcs/src/test/java/decodes/datasource/DataSourceTest.java
+++ java/opendcs/src/test/java/decodes/datasource/DataSourceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
@@ -44,6 +45,10 @@ public class DataSourceTest
         DataSourceException result = testDS.getRawMessages()
                                            .filter(msg -> msg.isSuccess())
                                            .map(msg -> msg.getSuccess())
+                                           .map(msg -> {
+                                                System.out.println("Decoded Message.");
+                                                return msg;
+                                           })
                                            .collect(new MessageCollector());
         assertNotNull(result);
         
@@ -90,6 +95,16 @@ public class DataSourceTest
             {
                 throw new DataSourceEndException("No more data.");
             }
+            if (index > 0)
+            {
+                /* outputing message with some delay to see the affects */
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
             RawMessage msg = new RawMessage(messages[index].getBytes());
             index++;
             return msg;
@@ -102,6 +117,10 @@ public class DataSourceTest
         @Override
         public Supplier<List<RawMessage>> supplier()
         {
+            /*
+             This type is arbitrary but the examples used Lists. I'm thinking the instance of the collector
+             takes a "StatusReporter" object as a constructor parameter and this is what's returned.
+            */
             return () -> new ArrayList<RawMessage>();
         }
 
@@ -110,14 +129,24 @@ public class DataSourceTest
         {
             return (list,msg) ->
             {
-                System.out.println("Adding message");
+                /** Here instead of "Accumulating" we'd be updating a status object */
+                System.out.println(new Date().toString() + ": Adding message: " + msg.toString());
                 list.add(msg);
             };
         }
 
+        /**
+         * This only comes up when doing parallel, but for our purpose the same logic as above would seem to
+         * be able to apply
+         */
         @Override
         public BinaryOperator<List<RawMessage>> combiner() {
-            return (left,right) -> {left.addAll(right);return left;};
+            return (left,right) ->
+            {
+                System.out.println("Combining");
+                left.addAll(right);
+                return left;
+            };
         }
 
         @Override
@@ -125,7 +154,11 @@ public class DataSourceTest
         {            
             return (list) -> 
             {
-                System.out.println("Finishing.");
+                for (RawMessage rm :list)
+                {
+                    System.out.println(new Date().toString() + ": " + rm);
+                }
+                System.out.println(new Date().toString() + ": Finishing.");
                 return new DataSourceEndException("The end");
             };    
         }
