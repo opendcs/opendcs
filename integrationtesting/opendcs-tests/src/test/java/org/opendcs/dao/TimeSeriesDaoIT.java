1c2c6abac44923a74d74d0b61df7710eafc364be
zack-rma <zack@rmanet.com>
2025-02-24 09:17:46 -0800

    Updated test case to expect thrown exception, cleaned up class

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/TimeSeriesDaoIT.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/TimeSeriesDaoIT.java
@@ -1,15 +1,13 @@
 package org.opendcs.dao;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.opendcs.fixtures.assertions.TimeSeries.assertEquals;
-import static org.opendcs.fixtures.helpers.TestResources.getResource;
 
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.TimeZone;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.opendcs.fixtures.AppTestBase;
@@ -32,26 +30,26 @@ import opendcs.dai.TimeSeriesDAI;
 
 @DecodesConfigurationRequired({
     "shared/test-sites.xml"})
-public class TimeSeriesDaoIT extends AppTestBase
+class TimeSeriesDaoIT extends AppTestBase
 {
-    private Logger log = LoggerFactory.getLogger(TimeSeriesDaoIT.class);
+    private final Logger log = LoggerFactory.getLogger(TimeSeriesDaoIT.class);
 
     @ConfiguredField
     private TimeSeriesDb tsDb;
 
     /**
      * This just tests that timeseries can be saved to the database, retrieved, and totally deleted.
-     * @param inputFile
-     * @throws Exception
+     * @param inputFile The input file containing the timeseries data to import.
+     * @throws Exception If there is an error importing the timeseries data.
      */
     @ParameterizedTest
     @CsvSource({
         "timeseries/${impl}/regular_ts.tsimport"
     })
     @EnableIfTsDb
-    public void test_timeseries_operations(String inputFile) throws Exception
+    void test_timeseries_operations(String inputFile) throws Exception
     {
-        TsImporter importer = new TsImporter(TimeZone.getTimeZone("UTC"), null, (tsIdStr) -> 
+        TsImporter importer = new TsImporter(TimeZone.getTimeZone("UTC"), null, tsIdStr ->
         {
             try
             {
@@ -84,13 +82,12 @@ public class TimeSeriesDaoIT extends AppTestBase
                                       true, true, false);
                 log.info("Data loaded.");
                 assertEquals(tsIn, result, "Timeseries round trip did not work.");
-                tsDao.deleteTimeSeries(tsIn.getTimeSeriesIdentifier());
+                tsDao.deleteTimeSeries(tsIn.getTimeSeriesIdentifier()); // This will also delete the timeseries identifier.
                 final CTimeSeries result2 = tsDao.makeTimeSeries(tsIn.getTimeSeriesIdentifier());
-                tsDao.fillTimeSeries(result2,
+                assertThrows(BadTimeSeriesException.class, () -> tsDao.fillTimeSeries(result2,
                                      tsIn.sampleAt(0).getTime(), 
                                      tsIn.sampleAt(tsIn.size()-1).getTime(),
-                                      true, true, false);
-                assertTrue(result2.size() == 0, "Time series elements were left in the database.");
+                                      true, true, false));
             }
         }
     }
