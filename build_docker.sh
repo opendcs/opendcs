444a0b9f79417ef5c891dc7039ced1a852bec2ce
Karl Tarbet <ktarbet@users.noreply.github.com>
2025-05-31 18:24:47 -0700

    Include lrgs subdirectory in copy command (#1133)
    Fixes #1132
    
    ## Solution
    
    include lrgs sub-directory
    
    ## how you tested the change
    building image locally and running
    
    (cherry picked from commit ca7aaa56ae9eb4c7537570271a5436372406a88e)

--- build_docker.sh
+++ build_docker.sh
@@ -3,6 +3,11 @@
 # 
 # I may make this an ant task but for the moment this is just easier to manage
 
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <tag> <version>"
+    exit 1
+fi
+
 images=("lrgs"  "routingscheduler"  "compproc" "compdepends")
 tag=$1
 version=$2
