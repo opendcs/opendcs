225e2d704c55f9b7ee9884bc6e0a74066d3158a9
Mike Neilson <m.allan.neilson@gmail.com>
2025-06-25 16:18:23 -0700

    Improve error handling in IridiumPMParser. (#1198)
    ## Problem Description
    
    <!-- if your PR does not address an open issue you can remove this line
    -->
    Fixes #1197.
    
    <!-- if you are referencing a specific issue a problem description is
    not required -->
    Describe the problem you are trying to solve.
    
    ## Solution
    
    Wrap Iridium PM Parsing in Throwable catch all so that downstream code
    can behave correctly.
    There's probably room for improved parsing but I'm not familiar enough
    with irridium data, and how the IridiumSDB LrgsInput assigns data, to go
    through that in detail promptly.
    
    ## how you tested the change
    
    Unit tests were created to verify basic processing and error handling.
    The input data contains both valid and invalid Iridium messages to check
    on error handling.
    
    ## Where the following done:
    
    - [ ] Tests. Check all that apply:
       - [x] Unit tests created or modified that run during ant test.
    - [ ] Integration tests created or modified that run during integration
    testing
             (Formerly called regression tests.)
       - [ ] Test procedure descriptions for manual testing
    - [ ] Was relevant documentation updated?
    - [ ] Were relevant config element (e.g. XML data) updated as
    appropriate
    
    If you aren't sure leave unchecked and we will help guide you to want
    needs changing where.
    
    (cherry picked from commit cc7355b189566efa5a004e0b468970834980cb59)

--- java/opendcs/src/main/java/decodes/datasource/IridiumPMParser.java
+++ java/opendcs/src/main/java/decodes/datasource/IridiumPMParser.java
@@ -1,21 +1,28 @@
 /*
-*  $Id$
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
 */
+
 package decodes.datasource;
 
-import java.util.HashMap;
+import org.slf4j.LoggerFactory;
+
 import java.util.Date;
-import java.util.Calendar;
-import java.text.ParsePosition;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import lrgs.common.DcpMsg;
-import lrgs.common.DcpAddress;
 
-import ilex.util.ArrayUtil;
-import ilex.util.ByteUtil;
-import ilex.util.Logger;
 import ilex.var.Variable;
 
 import decodes.db.Constants;
@@ -26,6 +33,7 @@ import decodes.db.Constants;
 */
 public class IridiumPMParser extends PMParser
 {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(IridiumPMParser.class);
 	// Performance Measurement tags:
 	public static final String LATITUDE = "latitude";
 	public static final String LONGITUDE = "longitude";
@@ -47,164 +55,178 @@ public class IridiumPMParser extends PMParser
 	  Sets the mediumID to the DCP's IMEI.
 	  @param msg the message to parse.
 	*/
-	public void parsePerformanceMeasurements(RawMessage msg)
-		throws HeaderParseException
+	public void parsePerformanceMeasurements(RawMessage msg) throws HeaderParseException
 	{
-		DcpMsg origMsg = msg.getOrigDcpMsg();
-		byte data[] = msg.getData();
-		if (data == null || data.length < 75)
-			throw new HeaderParseException(
-				"Header too short, 75 bytes is required");
-		msg.setPM(GoesPMParser.MESSAGE_LENGTH, new Variable(data.length));
-		String hdr = new String(data);
-
-		if (origMsg != null)
+		String hdr = null;
+		try
 		{
-			String emei = origMsg.getDcpAddress().toString();
-			msg.setMediumId(emei);
-			msg.setPM(GoesPMParser.DCP_ADDRESS, new Variable(emei));
-			Date d = origMsg.getXmitTime();
-			msg.setPM(GoesPMParser.MESSAGE_TIME, new Variable(d));
-			msg.setTimeStamp(d);
-			int sessionStat = origMsg.getSessionStatus();
-			if (sessionStat == 0 || sessionStat == 1 || sessionStat == 2)
-				msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('G'));
-			else
-				msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('?'));
-			msg.setPM(GoesPMParser.SPACECRAFT, new Variable('I'));
-			msg.setPM(GoesPMParser.UPLINK_CARRIER, new Variable("IR"));
-			msg.setPM(GoesPMParser.DCP_MSG_FLAGS,
-				new Variable(origMsg.flagbits));
-		}
-		else
-		{
-			// Parse information out of the ASCII-ized msg header. Example:
-			// ID=300034012724030,TIME=08229165743,STAT=00,MO=00551,MT=00000,CDR=01D3997C 
-			int idx = hdr.indexOf("ID=");
-			if (idx == -1)
-				throw new HeaderParseException(
-					"No ID (EMEI) in Iridium header");
-			String s = hdr.substring(idx+3, idx+3+15);
-			msg.setMediumId(s);
-			msg.setPM(GoesPMParser.DCP_ADDRESS, new Variable(s));
-
-			// The TIME in the header is the time the message was sent from the
-			// SBD gateway (over the internet). It is not the DCP's original
-			// transmission time. The difference between the two times can vary
-			// from a few seconds to several minutes. Users decoding these
-			// messages and, in particular, use F(MOFF) must be aware of this.
-			// DECODES F(MHD) and F(MHT) operations can be used to change this
-			// time in the decoded message (the raw value doesn't change).
-			idx = hdr.indexOf("TIME=");
-			if (idx == -1)
-				throw new HeaderParseException(
-					"No TIME in Iridium header");
-			s = hdr.substring(idx+5, idx+5+11);
-			try
+			DcpMsg origMsg = msg.getOrigDcpMsg();
+			byte data[] = msg.getData();
+			if (data == null || data.length < 75)
 			{
-				Date d = goesDateFormat.parse(s);
+				throw new HeaderParseException("Header too short, 75 bytes is required");
+			}
+			msg.setPM(GoesPMParser.MESSAGE_LENGTH, new Variable(data.length));
+			hdr = new String(data);
+
+			if (origMsg != null)
+			{
+				String emei = origMsg.getDcpAddress().toString();
+				msg.setMediumId(emei);
+				msg.setPM(GoesPMParser.DCP_ADDRESS, new Variable(emei));
+				Date d = origMsg.getXmitTime();
 				msg.setPM(GoesPMParser.MESSAGE_TIME, new Variable(d));
+				msg.setTimeStamp(d);
+				int sessionStat = origMsg.getSessionStatus();
+				if (sessionStat == 0 || sessionStat == 1 || sessionStat == 2)
+				{
+					msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('G'));
+				}
+				else
+				{
+					msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('?'));
+				}
+				msg.setPM(GoesPMParser.SPACECRAFT, new Variable('I'));
+				msg.setPM(GoesPMParser.UPLINK_CARRIER, new Variable("IR"));
+				msg.setPM(GoesPMParser.DCP_MSG_FLAGS,new Variable(origMsg.flagbits));
 			}
-			catch(Exception ex)
+			else
 			{
-				throw new HeaderParseException(
-					"Cannot parse time from Iridium header '" + s + "'");
+				// Parse information out of the ASCII-ized msg header. Example:
+				// ID=300034012724030,TIME=08229165743,STAT=00,MO=00551,MT=00000,CDR=01D3997C
+				int idx = hdr.indexOf("ID=");
+				if (idx == -1)
+				{
+					throw new HeaderParseException("No ID (EMEI) in Iridium header");
+				}
+				String s = hdr.substring(idx+3, idx+3+15);
+				msg.setMediumId(s);
+				msg.setPM(GoesPMParser.DCP_ADDRESS, new Variable(s));
+
+				// The TIME in the header is the time the message was sent from the
+				// SBD gateway (over the internet). It is not the DCP's original
+				// transmission time. The difference between the two times can vary
+				// from a few seconds to several minutes. Users decoding these
+				// messages and, in particular, use F(MOFF) must be aware of this.
+				// DECODES F(MHD) and F(MHT) operations can be used to change this
+				// time in the decoded message (the raw value doesn't change).
+				idx = hdr.indexOf("TIME=");
+				if (idx == -1)
+				{
+					throw new HeaderParseException("No TIME in Iridium header");
+				}
+				s = hdr.substring(idx+5, idx+5+11);
+				try
+				{
+					Date d = goesDateFormat.parse(s);
+					msg.setPM(GoesPMParser.MESSAGE_TIME, new Variable(d));
+				}
+				catch (Exception ex)
+				{
+					throw new HeaderParseException("Cannot parse time from Iridium header '" + s + "'", ex);
+				}
+
+				idx = hdr.indexOf("STAT=");
+				if (idx != -1)
+				{
+					int comma = hdr.indexOf(',', idx);
+					try
+					{
+						int sessionStat = Integer.parseInt(hdr.substring(idx+5, comma));
+						if (sessionStat == 0 || sessionStat == 1 || sessionStat == 2)
+						{
+							msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('G'));
+						}
+					}
+					catch (Exception ex)
+					{
+						logger.atWarn()
+						  	  .setCause(ex)
+						  	  .log("Bad STAT field in Iridium header '{}'",hdr);
+					}
+				}
 			}
-			
-			idx = hdr.indexOf("STAT=");
+
+			// Process Location info if it is present.
+			int idx = hdr.indexOf("LAT=");
 			if (idx != -1)
 			{
 				int comma = hdr.indexOf(',', idx);
 				try
 				{
-					int sessionStat = Integer.parseInt(hdr.substring(idx+5, comma));
-					if (sessionStat == 0 || sessionStat == 1 || sessionStat == 2)
-						msg.setPM(GoesPMParser.FAILURE_CODE, new Variable('G'));
+					msg.setPM(LATITUDE, new Variable(Double.parseDouble(hdr.substring(idx+4, comma))));
+				}
+				catch (Exception ex)
+				{
+					logger.atWarn()
+						  .setCause(ex)
+						  .log("Bad LAT field in Iridium header '{}'",hdr);
+				}
+			}
+			idx = hdr.indexOf("LON=");
+			if (idx != -1)
+			{
+				int comma = hdr.indexOf(',', idx);
+				try
+				{
+					msg.setPM(LONGITUDE, new Variable(Double.parseDouble(hdr.substring(idx+4, comma))));
+				}
+				catch (Exception ex)
+				{
+					logger.atWarn()
+						  .setCause(ex)
+						  .log("Bad LON field in Iridium header '{}'", hdr);
+				}
+			}
+			idx = hdr.indexOf("RAD=");
+			if (idx != -1)
+			{
+				int comma = hdr.indexOf(',', idx);
+				try
+				{
+					msg.setPM(CEP_RADIUS, new Variable(Double.parseDouble(hdr.substring(idx+4, comma))));
 				}
 				catch(Exception ex)
 				{
-					Logger.instance().warning("Bad STAT field in Iridium header '"
-						+ hdr + "'");
+					logger.atWarn()
+						  .setCause(ex)
+						  .log("Bad RAD field in Iridium header '{}'", hdr);
 				}
 			}
-		}
-		
-		// Process Location info if it is present.
-		int idx = hdr.indexOf("LAT=");
-		if (idx != -1)
-		{
-			int comma = hdr.indexOf(',', idx);
-			try
-			{
-				msg.setPM(LATITUDE, 
-					new Variable(
-						Double.parseDouble(hdr.substring(idx+4, comma))));
-			}
-			catch(Exception ex)
-			{
-				Logger.instance().warning("Bad LAT field in Iridium header '"
-					+ hdr + "'");
-			}
-		}
-		idx = hdr.indexOf("LON=");
-		if (idx != -1)
-		{
-			int comma = hdr.indexOf(',', idx);
-			try
-			{
-				msg.setPM(LONGITUDE, 
-					new Variable(
-						Double.parseDouble(hdr.substring(idx+4, comma))));
-			}
-			catch(Exception ex)
-			{
-				Logger.instance().warning("Bad LON field in Iridium header '"
-					+ hdr + "'");
-			}
-		}
-		idx = hdr.indexOf("RAD=");
-		if (idx != -1)
-		{
-			int comma = hdr.indexOf(',', idx);
-			try
-			{
-				msg.setPM(CEP_RADIUS, 
-					new Variable(
-						Double.parseDouble(hdr.substring(idx+4, comma))));
-			}
-			catch(Exception ex)
-			{
-				Logger.instance().warning("Bad RAD field in Iridium header '"
-					+ hdr + "'");
-			}
-		}
 
-		idx = hdr.indexOf("PLEN=");
-		if (idx != -1)
-		{
-			// Currently this is the last header so there's no comma after, but there is a space
-			int comma = hdr.indexOf(' ', idx);
-			try
+			idx = hdr.indexOf("PLEN=");
+			if (idx != -1)
 			{
-				msg.setPM(PAYLOAD_LEN, 
-					new Variable(
-						Integer.parseInt(hdr.substring(idx+5, comma))));
+				// Currently this is the last header so there's no comma after, but there is a space
+				int comma = hdr.indexOf(' ', idx);
+				try
+				{
+					msg.setPM(PAYLOAD_LEN, new Variable(Integer.parseInt(hdr.substring(idx+5, comma))));
+				}
+				catch (Exception ex)
+				{
+					logger.atWarn()
+						.setCause(ex)
+						.log("Bad PLEN field in Iridium header '{}'", hdr);
+				}
 			}
-			catch(Exception ex)
+
+			// If iridiumIEInPayload is true, this length does not include the IE:02 portion;
+			// if false, the IE:02 part is removed, and this really is the length of the header.
+			idx = hdr.indexOf(" ");
+			if (!hdr.substring(idx+1, idx+6).equals("IE:02"))
 			{
-				Logger.instance().warning("Bad PLEN field in Iridium header '"
-					+ hdr + "'");
+				idx++;	// include the ending space in the header portion
 			}
+
+			if (idx < 75) idx = 75;
+			msg.setHeaderLength(idx);
+			headerLength = idx;
 		}
-
-		// If iridiumIEInPayload is true, this length does not include the IE:02 portion;
-		// if false, the IE:02 part is removed, and this really is the length of the header.
-		idx = hdr.indexOf(" ");
-		if (!hdr.substring(idx+1, idx+6).equals("IE:02")) idx++;	// include the ending space in the header portion
-
-		if (idx < 75) idx = 75;
-		msg.setHeaderLength(idx);
-		headerLength = idx;
+		catch (Throwable ex)
+		{
+			throw new HeaderParseException("Error parsing header '" + hdr + "'", ex);
+		}
 	}
 
 	public boolean containsExplicitLength()
@@ -235,4 +257,3 @@ public class IridiumPMParser extends PMParser
 		return goesDateFormat;
 	}
 }
-
