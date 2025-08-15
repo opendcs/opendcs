0aad44c5ebc78e67102953c725a3e979168af371
Karl Tarbet <ktarbet@users.noreply.github.com>
2025-08-15 14:14:23 -0700

    Task/1268 update logging part 6  (into main branch) (#1297)
    note: This PR builds on https://github.com/opendcs/opendcs/pull/1296
    (consider reviewing that PR first)
    
    progress on #1268 
    progress on #1270 
    
    ## Solution
    
    using standard logging.

--- java/opendcs/src/main/java/decodes/cwms/CwmsTsId.java
+++ java/opendcs/src/main/java/decodes/cwms/CwmsTsId.java
@@ -1,77 +1,30 @@
-/**
- * $Id: CwmsTsId.java,v 1.7 2020/01/31 19:32:13 mmaloney Exp $
- * 
- * $Log: CwmsTsId.java,v $
- * Revision 1.7  2020/01/31 19:32:13  mmaloney
- * Store Duration object
- *
- * Revision 1.6  2019/01/10 16:01:17  mmaloney
- * Added toString() and hashCode().
- *
- * Revision 1.5  2018/05/01 17:35:01  mmaloney
- * init offsetErrorAction to IGNORE.
- *
- * Revision 1.4  2016/10/17 17:49:38  mmaloney
- * Add sub/base accessors for OpenDCS 6.3 CWMS Naming Standards
- *
- * Revision 1.3  2016/09/29 18:54:36  mmaloney
- * CWMS-8979 Allow Database Process Record to override decodes.properties and
- * user.properties setting. Command line arg -Dsettings=appName, where appName is the
- * name of a process record. Properties assigned to the app will override the file(s).
- *
- * Revision 1.2  2015/10/26 12:49:24  mmaloney
- * compareTo method must compare TSID not public name.
- *
- * Revision 1.1.1.1  2014/05/19 15:28:59  mmaloney
- * OPENDCS 6.0 Initial Checkin
- *
- * Revision 1.18  2013/03/21 18:27:40  mmaloney
- * DbKey Implementation
- *
- * Revision 1.17  2012/09/11 12:48:02  mmaloney
- * dev
- *
- * Revision 1.16  2012/08/01 14:24:10  mmaloney
- * Implement equals(Object) to allow storage in HashSet.
- *
- * Revision 1.15  2012/07/23 15:20:44  mmaloney
- * Refactor group evaluation for HDB.
- *
- * Revision 1.14  2012/07/05 18:24:02  mmaloney
- * CWMS location names may contain spaces and multiple hyphens.
- * ts key is stored as a long.
- *
- * Revision 1.13  2012/06/18 13:20:43  mmaloney
- * minRecNum in cp_comp_tasklist must be defined as long
- *
- * Revision 1.12  2012/06/13 14:38:50  mmaloney
- * Added get/set tabsel methods
- *
- * Revision 1.11  2012/05/15 14:08:11  mmaloney
- * Added checkValid method.
- *
- * Revision 1.10  2011/03/22 14:13:25  mmaloney
- * Added caching for DbComputations and CWMS Time Series Identifiers.
- *
- * Revision 1.9  2011/02/02 20:42:28  mmaloney
- * bug fixes
- *
- * This software was written by Cove Software, LLC ("COVE") under contract 
- * to the United States Government. 
- * 
- * No warranty is provided or implied other than specific contractual terms
- * between COVE and the U.S. Government
- * 
- * Copyright 2016 U.S. Army Corps of Engineers, Hydrologic Engineering Center.
- * All rights reserved.
- */
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations 
+* under the License.
+*
+* This software was written by Cove Software, LLC ("COVE") under contract 
+* to the United States Government. 
+*/
 package decodes.cwms;
 
 import java.util.Date;
 
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
+
 import opendcs.opentsdb.Interval;
 import opendcs.opentsdb.OffsetErrorAction;
-import ilex.util.Logger;
 import ilex.util.TextUtil;
 import decodes.db.Constants;
 import decodes.db.DataType;
@@ -87,9 +40,9 @@ import decodes.tsdb.TimeSeriesIdentifier;
  * Implements TimeSeriesIdentifier for CWMS database.
  * Uniquely identifies a CWMS Time Series.
  */
-public class CwmsTsId 
-	implements TimeSeriesIdentifier
+public class CwmsTsId implements TimeSeriesIdentifier
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	private DbKey tsCode = Constants.undefinedId;
 	private String siteName = null; 
 	private DataType dataType = null;
@@ -171,7 +124,6 @@ public class CwmsTsId
 			utcOffset, storageUnits);
 		ret.setSite(site);
 		ret.setSiteDisplayName(siteDisplayName);
-//Logger.instance().debug3("CwmsTsId.copyNoKey new path=" + ret.getUniqueString());
 		
 		return ret;
 	}
@@ -191,8 +143,6 @@ public class CwmsTsId
 	public void setUniqueString(String uniqueId)
 	{
 		String parts[] = uniqueId.split("\\.");
-//Logger.instance().debug3("CwmsTsId.setUniqueString str='" + uniqueId+ "': #parts=" + parts.length);
-//for(int i=0; i<parts.length; i++)Logger.instance().debug3("part[" + i + "]='" + parts[i] + "'");
 		if (parts.length > 0)
 			setLocation(parts[0]);
 		if (parts.length > 1)
@@ -279,8 +229,7 @@ public class CwmsTsId
 		else if (part.equalsIgnoreCase("version"))
 			setVersion(value);
 		else
-			Logger.instance().warning("CwmsTsId setPart " + part 
-				+ ": invalid part");
+			log.warn("CwmsTsId setPart {}: invalid part", part);
 	}
 
 	/* (non-Javadoc)
