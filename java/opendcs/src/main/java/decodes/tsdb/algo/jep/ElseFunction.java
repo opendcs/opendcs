fcfa2de7521252d33f20b287e0cb8f4c4ffff43e
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-09-04 20:33:28 +0000

    Update logging to slf4j.

--- java/opendcs/src/main/java/decodes/tsdb/algo/jep/ElseFunction.java
+++ java/opendcs/src/main/java/decodes/tsdb/algo/jep/ElseFunction.java
@@ -1,12 +1,23 @@
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 package decodes.tsdb.algo.jep;
-
-import ilex.util.Logger;
-import ilex.util.TextUtil;
 
 import org.nfunk.jep.EvaluatorI;
 import org.nfunk.jep.Node;
 import org.nfunk.jep.ParseException;
-import org.nfunk.jep.ParserVisitor;
 import org.nfunk.jep.function.CallbackEvaluationI;
 import org.nfunk.jep.function.PostfixMathCommand;
 
@@ -17,9 +28,7 @@ import org.nfunk.jep.function.PostfixMathCommand;
  * If the previous to the cond() function resulted in false, then expression is evaluated
  * and returned. Otherwise a parse exception is thrown.
  */
-public class ElseFunction
-	extends PostfixMathCommand
-	implements CallbackEvaluationI
+public class ElseFunction extends PostfixMathCommand implements CallbackEvaluationI
 {
 	public static final String funcName = "else";
 	private JepContext ctx = null;
@@ -30,13 +39,13 @@ public class ElseFunction
 		this.numberOfParameters = 1;
 		this.ctx = ctx;
 	}
-	
+
 	@Override
 	public boolean checkNumberOfParameters(int n)
 	{
 		return n == 1;
 	}
-	
+
 	@Override
 	public Object evaluate(Node node, EvaluatorI evaluator) throws ParseException
 	{
@@ -44,14 +53,14 @@ public class ElseFunction
 		{
 			throw new ParseException("else function requires 1 argument!");
 		}
-		
+
 		if (ctx.getLastConditionFailed())
 		{
 			return evaluator.eval(node.jjtGetChild(0));
 		}
 		else
 		{
-			return Double.valueOf(0.0);	
+			return Double.valueOf(0.0);
 		}
 	}
 }
--- java/opendcs/src/main/java/decodes/tsdb/algo/jep/JepContext.java
+++ java/opendcs/src/main/java/decodes/tsdb/algo/jep/JepContext.java
@@ -1,11 +1,24 @@
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 package decodes.tsdb.algo.jep;
 
-import ilex.util.Logger;
 import ilex.util.TextUtil;
 import ilex.var.TimedVariable;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.TimeZone;
 import java.util.TreeSet;
@@ -14,6 +27,8 @@ import opendcs.dai.TimeSeriesDAI;
 
 import org.nfunk.jep.JEP;
 import org.nfunk.jep.ParseException;
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
 
 import decodes.cwms.validation.Screening;
 import decodes.cwms.validation.ScreeningCriteria;
@@ -35,6 +50,7 @@ import decodes.util.TSUtil;
  */
 public class JepContext
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	private JEP parser = null;
 	private boolean exitCalled = false;
 	private boolean lastConditionFailed = false;
@@ -45,7 +61,7 @@ public class JepContext
 	private String onErrorLabel = null;
 	private boolean lastStatementWasCond = false;
 	private HashSet<String> rolesInitializedForScreening = null;
-	
+
 	public JepContext(TimeSeriesDb tsdb, AW_AlgorithmBase algo)
 	{
 		this.tsdb = tsdb;
@@ -59,11 +75,15 @@ public class JepContext
 		parser.addFunction(ConditionFunction.funcName, new ConditionFunction(this));
 		parser.addFunction(ElseFunction.funcName, new ElseFunction(this));
 		parser.addFunction(ExitFunction.funcName, new ExitFunction(this));
-		parser.addFunction("debug3", new LogFunction(this, Logger.E_DEBUG3));
-		parser.addFunction("debug2", new LogFunction(this, Logger.E_DEBUG2));
-		parser.addFunction("debug1", new LogFunction(this, Logger.E_DEBUG1));
-		parser.addFunction("info", new LogFunction(this, Logger.E_INFORMATION));
-		parser.addFunction("warning", new LogFunction(this, Logger.E_WARNING));
+		// Old level name functions kept to not break existing code... yet.
+		parser.addFunction("trace", new LogFunction(this, () -> log.atTrace()));
+		parser.addFunction("debug3", new LogFunction(this, () -> log.atTrace()));
+		parser.addFunction("debug2", new LogFunction(this, () -> log.atTrace()));
+		parser.addFunction("debug1", new LogFunction(this, () -> log.atDebug()));
+		parser.addFunction("debug", new LogFunction(this, () -> log.atDebug()));
+		parser.addFunction("info", new LogFunction(this, () -> log.atInfo()));
+		parser.addFunction("warning", new LogFunction(this, () -> log.atWarn()));
+		parser.addFunction("warn", new LogFunction(this, () -> log.atWarn()));
 		parser.addFunction(OnErrorFunction.funcName, new OnErrorFunction(this));
 		parser.addFunction(DatchkFunction.funcName, new DatchkFunction(this));
 		parser.addFunction(IsRejectedFunction.funcName, new IsRejectedFunction(this));
@@ -85,7 +105,7 @@ public class JepContext
 		lastStatementWasCond = false;
 		gotoLabel = null;
 	}
-	
+
 	public boolean isExitCalled()
 	{
 		return exitCalled;
@@ -155,7 +175,7 @@ public class JepContext
 	{
 		this.lastStatementWasCond = lastStatementWasCond;
 	}
-	
+
 	public void initForScreening(String inputRole, Screening screening)
 		throws ParseException
 	{
@@ -163,18 +183,18 @@ public class JepContext
 			rolesInitializedForScreening = new HashSet<String>();
 		else if (rolesInitializedForScreening.contains(inputRole))
 			return; // already done.
-		
-		algo.debug3("Retrieving additional data needed for checks for '" + inputRole + "'.");
+
+		log.trace("Retrieving additional data needed for checks for '{}'.", inputRole);
 		rolesInitializedForScreening.add(inputRole);
-		
+
 		ParmRef inputParm = algo.getParmRef(inputRole);
 		TimeSeriesIdentifier inputTsid = inputParm.timeSeries.getTimeSeriesIdentifier();
-		
+
 		Site site = inputTsid.getSite();
 		if (site != null && site.timeZoneAbbr != null && site.timeZoneAbbr.length() > 0)
 		{
 			TimeZone tz = TimeZone.getTimeZone(site.timeZoneAbbr);
-			algo.debug1("Setting criteria season timezone to " + tz.getID());
+			log.debug("Setting criteria season timezone to {}", tz.getID());
 		}
 
 		// Using the tests, determine the amount of past-data needed at each time-slice.
@@ -195,8 +215,8 @@ public class JepContext
 				prevcrit = crit;
 			}
 		}
-		algo.debug3("additional data done for '" + inputRole + "', #times needed=" + needed.size());
-		
+		log.trace("additional data done for '{}', #times needed={}", inputRole, needed.size());
+
 		if (needed.size() > 0)
 		{
 			TimeSeriesDAI timeSeriesDAO = tsdb.makeTimeSeriesDAO();
@@ -216,16 +236,19 @@ public class JepContext
 			}
 			catch (Exception ex)
 			{
-				throw new ParseException(ex.toString());
+				// log here because this ParseException doesn't allow us to
+				ParseException toThrow = new ParseException("Unable to initialize contenxt, could not fill in time series.");
+				toThrow.addSuppressed(ex);
+				throw toThrow;
 			}
 			finally
 			{
 				timeSeriesDAO.close();
 			}
 		}
-		
+
 		String euAbbr = screening.getCheckUnitsAbbr();
-		if (euAbbr != null 
+		if (euAbbr != null
 		 && TextUtil.strCompareIgnoreCase(euAbbr, inputParm.timeSeries.getUnitsAbbr()) != 0)
 		{
 			// Need to convert the input param into the proper units.
