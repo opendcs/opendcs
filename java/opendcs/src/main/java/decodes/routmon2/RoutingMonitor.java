bb9ae28b5f3bb37ecd5502f5df918bec6b4360a4
Karl Tarbet <ktarbet@users.noreply.github.com>
2025-09-16 13:26:14 -0700

    update logging part 23  (into main) (#1366)
    progress on #1268

--- java/opendcs/src/main/java/decodes/routmon2/RoutingMonitor.java
+++ java/opendcs/src/main/java/decodes/routmon2/RoutingMonitor.java
@@ -1,3 +1,18 @@
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations 
+* under the License.
+*/
 package decodes.routmon2;
 
 import java.awt.event.WindowAdapter;
@@ -5,14 +20,17 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
+
 import ilex.util.EnvExpander;
-import ilex.util.Logger;
 import lrgs.gui.DecodesInterface;
 import decodes.tsdb.TimeSeriesDb;
 import decodes.tsdb.TsdbAppTemplate;
 
 public class RoutingMonitor extends TsdbAppTemplate
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	public static final String module = "RoutingMonitor";
 	private boolean exitOnClose = true;
 	private RoutingMonitorFrame frame = null;
@@ -75,10 +93,7 @@ public class RoutingMonitor extends TsdbAppTemplate
 		} 
 		catch (Exception ex)
 		{
-			String msg = module + " Can not initialize. " + ex.getMessage();
-			Logger.instance().failure(msg);
-			System.err.println(msg);
-			ex.printStackTrace(System.err);
+			log.atError().setCause(ex).log("Can not initialize.");
 		}
 	}
 
