2f9bf3414efd573e35445d7d5c121bec6b3026aa
Mike Neilson <m.allan.neilson@gmail.com>
2025-09-19 19:53:56 -0700

    initial database interfaces.

---
+++ java/opendcs/src/main/java/org/opendcs/database/dai/UserManagement.java
@@ -0,0 +1,28 @@
+package org.opendcs.database.dai;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.opendcs.database.api.DataTransaction;
+import org.opendcs.database.api.OpenDcsDao;
+import org.opendcs.database.api.OpenDcsDataException;
+import org.opendcs.database.model.IdentityProvider;
+import org.opendcs.database.model.User;
+
+import decodes.sql.DbKey;
+
+public interface UserManagement extends OpenDcsDao
+{
+    List<User> getUsers(DataTransaction tx) throws OpenDcsDataException;
+    User addUser(DataTransaction tx, User user) throws OpenDcsDataException;
+    Optional<User> getUser(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+    void updateUser(DataTransaction tx, DbKey id, User user) throws OpenDcsDataException;
+    void deleteUser(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+
+
+    List<IdentityProvider> getIdentityProviders(DataTransaction tx) throws OpenDcsDataException;
+    IdentityProvider addIdentityProvider(DataTransaction tx, IdentityProvider provider) throws OpenDcsDataException;
+    Optional<IdentityProvider> getIdentityProvider(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+    void updateIdentityProvider(DataTransaction tx, DbKey id, IdentityProvider provider) throws OpenDcsDataException;
+    void deleteIdentityProvider(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+}
