c3fb931b2f697456c308156aedc35cb0408c17c7
Mike Neilson <m.allan.neilson@gmail.com>
2025-09-21 19:17:42 -0700

    Initial interface design.

--- java/opendcs/src/main/java/org/opendcs/database/dai/UserManagement.java
+++ java/opendcs/src/main/java/org/opendcs/database/dai/UserManagement.java
@@ -7,12 +7,14 @@ import org.opendcs.database.api.DataTransaction;
 import org.opendcs.database.api.OpenDcsDao;
 import org.opendcs.database.api.OpenDcsDataException;
 import org.opendcs.database.model.IdentityProvider;
+import org.opendcs.database.model.Role;
 import org.opendcs.database.model.User;
 
 import decodes.sql.DbKey;
 
 public interface UserManagement extends OpenDcsDao
 {
+    // todo: add paging?
     List<User> getUsers(DataTransaction tx) throws OpenDcsDataException;
     User addUser(DataTransaction tx, User user) throws OpenDcsDataException;
     Optional<User> getUser(DataTransaction tx, DbKey id) throws OpenDcsDataException;
@@ -25,4 +27,11 @@ public interface UserManagement extends OpenDcsDao
     Optional<IdentityProvider> getIdentityProvider(DataTransaction tx, DbKey id) throws OpenDcsDataException;
     void updateIdentityProvider(DataTransaction tx, DbKey id, IdentityProvider provider) throws OpenDcsDataException;
     void deleteIdentityProvider(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+
+    // todo: add paging?
+    List<Role> getRoles();
+    Role addRole(DataTransaction tx, Role role);
+    Optional<Role> getRole(DataTransaction tx, DbKey id);
+    void updateRole(DataTransaction tx, DbKey id, Role role);
+    void deleteRole(DataTransaction tx, DbKey id);
 }
