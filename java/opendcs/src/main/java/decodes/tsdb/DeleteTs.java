ba2d41bc88759efa74ee7b124aa057d3bd645e37
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-09-25 16:29:00 +0000

    Updating logging to slf4j.

--- java/opendcs/src/main/java/decodes/tsdb/DeleteTs.java
+++ java/opendcs/src/main/java/decodes/tsdb/DeleteTs.java
@@ -1,33 +1,17 @@
 /*
-* $Id$
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
 *
-* $Log$
-* Revision 1.1.1.1  2014/05/19 15:28:59  mmaloney
-* OPENDCS 6.0 Initial Checkin
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
 *
-* Revision 1.7  2012/10/12 19:50:41  mmaloney
-* Added -T argument to completely delete a time series from the DB
-* including all its data and meta data.
+*   http://www.apache.org/licenses/LICENSE-2.0
 *
-* Revision 1.6  2012/08/29 14:20:22  mmaloney
-* refactor silent mode.
-*
-* Revision 1.5  2012/08/29 13:59:06  mmaloney
-* code cleanup.
-*
-* Revision 1.4  2012/08/09 15:42:13  mmaloney
-* Use makeTimeSeries in TSDB.
-*
-* Revision 1.3  2012/07/25 19:47:30  mmaloney
-* debug DeleteTs
-*
-* Revision 1.2  2012/07/25 18:09:10  mmaloney
-* Bug fix for HDB.
-*
-* Revision 1.1  2010/12/28 17:56:12  mmaloney
-* Created
-*
-* 
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
 */
 package decodes.tsdb;
 
@@ -38,10 +22,12 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
+
 import opendcs.dai.TimeSeriesDAI;
 
 import ilex.cmdline.*;
-import ilex.util.Logger;
 
 import decodes.util.CmdLineArgs;
 
@@ -58,9 +44,9 @@ Main Command Line Args include:
 
 Following the options are any number of data IDs.
 */
-public class DeleteTs
-	extends TsdbAppTemplate
+public class DeleteTs extends TsdbAppTemplate
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	private StringToken sinceArg = null;
 	private StringToken untilArg = null;
 	private StringToken tzArg = null;
@@ -82,22 +68,22 @@ public class DeleteTs
 	/** For cmdline version, adds argument specifications. */
 	protected void addCustomArgs(CmdLineArgs cmdLineArgs)
 	{
-		sinceArg = new StringToken("S", "Since Time dd-MMM-yyyy/HH:mm", "", 
+		sinceArg = new StringToken("S", "Since Time dd-MMM-yyyy/HH:mm", "",
 			TokenOptions.optSwitch, "");
 		cmdLineArgs.addToken(sinceArg);
-		untilArg = new StringToken("U", "Until Time dd-MMM-yyyy/HH:mm", "", 
+		untilArg = new StringToken("U", "Until Time dd-MMM-yyyy/HH:mm", "",
 			TokenOptions.optSwitch, "");
 		cmdLineArgs.addToken(untilArg);
-		tzArg = new StringToken("Z", "Time Zone", "", 
+		tzArg = new StringToken("Z", "Time Zone", "",
 			TokenOptions.optSwitch, "UTC");
 		cmdLineArgs.addToken(tzArg);
 		lookupTypeArg = new StringToken("L", "Lookup Type", "", TokenOptions.optSwitch, "id");
 		cmdLineArgs.addToken(lookupTypeArg);
-		deleteTsidArg = new BooleanToken("T", "Delete all data and TSID", "", 
+		deleteTsidArg = new BooleanToken("T", "Delete all data and TSID", "",
 			TokenOptions.optSwitch, false);
 		cmdLineArgs.addToken(deleteTsidArg);
 
-		outArg = new StringToken("", "time-series-IDs", "", 
+		outArg = new StringToken("", "time-series-IDs", "",
 			TokenOptions.optArgument|TokenOptions.optRequired
 			|TokenOptions.optMultiple, "");
 		cmdLineArgs.addToken(outArg);
@@ -110,8 +96,7 @@ public class DeleteTs
 
 		String s = sinceArg.getValue().trim();
 		Date since = s != null && s.length() > 0 ? convert2Date(s, false) : null;
-//System.out.println("since string='" + s + "', tz='" + tz.getID() + "', converted since=" + since);
-		
+
 		s = untilArg.getValue().trim();
 		Date until = s != null && s.length() > 0 ? convert2Date(s, true) : null;
 
@@ -129,7 +114,7 @@ public class DeleteTs
 			}
 			catch(NoSuchObjectException ex)
 			{
-				Logger.instance().warning("No such time series for '" + outTS + "': " + ex);
+				log.atWarn().setCause(ex).log("No such time series for '{}'", outTS);
 			}
 		}
 		timeSeriesDAO.close();
@@ -194,16 +179,16 @@ public class DeleteTs
 			}
 			else
 			{
-				Logger.instance().warning("Unknown time '" + s + "'");
+				log.warn("Unknown time '{}'", s);
 				return isTo ? new Date() : convert2Date("yesterday", false);
 			}
 		}
-		try 
+		try
 		{
 			Date d = timeSdf.parse(s);
 			return d;
 		}
-		catch(ParseException ex) 
+		catch(ParseException ex)
 		{
 			try
 			{
@@ -213,14 +198,15 @@ public class DeleteTs
 					d.setTime(d.getTime() + (MS_PER_DAY-1));
 				return d;
 			}
-			catch(ParseException ex2) 
+			catch(ParseException ex2)
 			{
-				Logger.instance().warning("Bad time format '" + s + "'");
+				ex2.addSuppressed(ex);
+				log.atWarn().setCause(ex).log("Bad time format '{}'", s);
 				return isTo ? new Date() : convert2Date("yesterday", false);
 			}
 		}
 	}
-	
+
 	public static void main(String args[])
 		throws Exception
 	{
