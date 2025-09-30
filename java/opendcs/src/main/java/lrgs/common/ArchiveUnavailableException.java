79268303c2bcf44b1c82d2ffeb1b8eb6bb233a49
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-09-30 22:30:10 +0000

    Update logging to slf4j.

--- java/opendcs/src/main/java/lrgs/common/ArchiveUnavailableException.java
+++ java/opendcs/src/main/java/lrgs/common/ArchiveUnavailableException.java
@@ -1,4 +1,19 @@
 
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations 
+* under the License.
+*/
 package lrgs.common;
 
 /**
@@ -13,5 +28,15 @@ public class ArchiveUnavailableException extends ArchiveException
 	public ArchiveUnavailableException(String msg, int errorCode)
 	{
 		super(msg, errorCode, true);
+	}
+
+	public ArchiveUnavailableException(String msg, int errorCode, Throwable cause)
+	{
+		super(msg, errorCode, true, cause);
+	}
+
+	public ArchiveUnavailableException(String msg, ArchiveException cause)
+	{
+		super(msg, cause.errorCode, true, cause);
 	}
 }
