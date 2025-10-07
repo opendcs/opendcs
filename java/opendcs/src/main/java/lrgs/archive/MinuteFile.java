56e584fc3b3643dd501bd1053a30d3a80d03450d
Karl Tarbet <ktarbet@users.noreply.github.com>
2025-10-07 08:06:27 -0700

    update logging part 40 (into main) (#1407)
    progress on #1268

--- java/opendcs/src/main/java/lrgs/archive/MinuteFile.java
+++ java/opendcs/src/main/java/lrgs/archive/MinuteFile.java
@@ -1,35 +1,25 @@
 /*
-*  $Id$
-*
-*  This is open-source software written by ILEX Engineering, Inc., under
-*  contract to the federal government. You are free to copy and use this
-*  source code for your own purposes, except that no part of this source
-*  code may be claimed to be proprietary.
-*
-*  Except for specific contractual terms between ILEX and the federal 
-*  government, this source code is provided completely without warranty.
-*  For more information contact: info@ilexeng.com
-*
-*  $Log$
-*  Revision 1.1  2008/04/04 18:21:11  cvs
-*  Added legacy code to repository
-*
-*  Revision 1.4  2005/10/20 18:01:05  mmaloney
-*  event nums
-*
-*  Revision 1.3  2005/09/19 21:31:50  mmaloney
-*  dev
-*
-*  Revision 1.2  2005/06/24 15:57:28  mjmaloney
-*  Java-Only-Archive implementation.
-*
-*  Revision 1.1  2005/06/06 21:15:26  mjmaloney
-*  Added new Java-Only Archiving Package
-*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+* 
+*   http://www.apache.org/licenses/LICENSE-2.0
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations 
+* under the License.
 */
 package lrgs.archive;
 
 import java.io.IOException;
+
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
+
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.BufferedInputStream;
@@ -37,7 +27,6 @@ import java.io.DataOutputStream;
 import java.io.FileOutputStream;
 import java.io.BufferedOutputStream;
 
-import ilex.util.Logger;
 
 /**
 This class contains static methods for reading and writing a day file's
@@ -45,6 +34,7 @@ minute-index. The file is a binary array of 1440 MsgIndexMinute objects.
 */
 public class MinuteFile
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	/**
 	 * Loads the array of MsgIndexMinute entries from the named file.
 	 * @param fn the file name.
@@ -53,12 +43,11 @@ public class MinuteFile
 	public static void load( String fn, MsgIndexMinute[] mns )
 		throws IOException
 	{
-		DataInputStream dis = null;
-		try
-		{
-			dis = new DataInputStream(
+		
+		try (DataInputStream dis = new DataInputStream(
 					new BufferedInputStream(
-						new FileInputStream(fn)));
+						new FileInputStream(fn))))
+		{
 
 			for(int i=0; i<mns.length; i++)
 			{
@@ -66,19 +55,13 @@ public class MinuteFile
 				mns[i].reserved = dis.readInt();
 				mns[i].oldestDapsTime = dis.readInt();
 			}
-			dis.close();
 		}
 		catch(IOException ex)
 		{
 			long t = System.currentTimeMillis();
 			if ((t % (24L * 3600000L)) > 60000L) // after 1st minute of day?
-				Logger.instance().warning(
-					MsgArchive.module + ":" + MsgArchive.EVT_BAD_MINUTE_FILE
-					+ "- Error reading '" + fn + "': " + ex);
-			if (dis != null)
 			{
-				try { dis.close(); }
-				catch(IOException ignore) {}
+				log.atWarn().setCause(ex).log("{} - Error reading '{}'", MsgArchive.EVT_BAD_MINUTE_FILE, fn);
 			}
 			throw ex;
 		}
@@ -92,31 +75,20 @@ public class MinuteFile
 	public static void save( String fn, MsgIndexMinute[] mns )
 		throws IOException
 	{
-		DataOutputStream dos = null;
-		try
-		{
-			dos = new DataOutputStream(
+		try (DataOutputStream dos = new DataOutputStream(
 					new BufferedOutputStream(
-						new FileOutputStream(fn)));
-
+						new FileOutputStream(fn))))
+		{
 			for(int i=0; i<mns.length; i++)
 			{
 				dos.writeInt(mns[i].startIndexNum);
 				dos.writeInt(mns[i].reserved);
 				dos.writeInt(mns[i].oldestDapsTime);
 			}
-			dos.close();
 		}
 		catch(IOException ex)
 		{
-			Logger.instance().warning(
-				MsgArchive.module + ":" + MsgArchive.EVT_BAD_MINUTE_FILE
-				+ "- Error writing '" + fn + "': " + ex);
-			if (dos != null)
-			{
-				try { dos.close(); }
-				catch(IOException ignore) {}
-			}
+			log.atWarn().setCause(ex).log("{} - Error writing '", MsgArchive.EVT_BAD_MINUTE_FILE, fn);
 			throw ex;
 		}
 	}
