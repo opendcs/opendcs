ff29dc6519782cd127bf0b63542860f8a5baf581
Mike Neilson <m.allan.neilson@gmail.com>
2025-10-11 14:37:01 -0700

    dao handles role operations.

--- java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/UserManagementImpl.java
+++ java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/UserManagementImpl.java
@@ -1,8 +1,19 @@
 package org.opendcs.database.impl.opendcs.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.ColumnMapper;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
 import org.opendcs.database.api.DataTransaction;
 import org.opendcs.database.api.OpenDcsDataException;
 import org.opendcs.database.dai.UserManagementDao;
@@ -14,6 +25,8 @@ import decodes.sql.DbKey;
 
 public class UserManagementImpl implements UserManagementDao
 {
+
+    private static final RoleMapper ROLE_MAPPER = new RoleMapper();
 
     @Override
     public List<User> getUsers(DataTransaction tx, int limit, int offset)
@@ -34,9 +47,9 @@ public class UserManagementImpl implements UserManagementDao
     }
 
     @Override
-    public void updateUser(DataTransaction tx, DbKey id, User user) throws OpenDcsDataException
+    public User updateUser(DataTransaction tx, DbKey id, User user) throws OpenDcsDataException
     {
-        
+        return null;
     }
 
     @Override
@@ -66,10 +79,10 @@ public class UserManagementImpl implements UserManagementDao
     }
 
     @Override
-    public void updateIdentityProvider(DataTransaction tx, DbKey id, IdentityProvider provider)
+    public IdentityProvider updateIdentityProvider(DataTransaction tx, DbKey id, IdentityProvider provider)
             throws OpenDcsDataException
     {
-        
+        return null;
     }
 
     @Override
@@ -87,26 +100,63 @@ public class UserManagementImpl implements UserManagementDao
     @Override
     public Role addRole(DataTransaction tx, Role role) throws OpenDcsDataException
     {
-        
-        return null;
+        Connection conn = tx.connection(Connection.class).get();
+        Handle handle = Jdbi.open(conn);
+        return
+            handle.createQuery("insert into opendcs_role(name, description, updated_at) values (:name, :description, now()) returning id, name, description,updated_at")
+              .bind("name", role.name)
+              .bind("description", role.description)
+              .map(ROLE_MAPPER).one();
     }
 
     @Override
     public Optional<Role> getRole(DataTransaction tx, DbKey id) throws OpenDcsDataException
     {
-        return Optional.empty();
+        Connection conn = tx.connection(Connection.class).get();
+        Handle handle = Jdbi.open(conn);
+        return handle.createQuery("select id, name, description, updated_at from opendcs_role where id = :id")
+              .bind("id", id.getValue()) // todo: create create arg handler
+              .map(ROLE_MAPPER).findOne();
     }
 
     @Override
-    public void updateRole(DataTransaction tx, DbKey id, Role role) throws OpenDcsDataException
+    public Role updateRole(DataTransaction tx, DbKey id, Role role) throws OpenDcsDataException
     {
-        
+        Connection conn = tx.connection(Connection.class).get();
+        Handle handle = Jdbi.open(conn);
+        return
+            handle.createQuery("update opendcs_role set name =:name, description = :description where id=:id returning id, name, description,updated_at")
+              .bind("name", role.name)
+              .bind("description", role.description)
+              .bind("id", id.getValue())
+              .map(ROLE_MAPPER).one();
     }
 
     @Override
     public void deleteRole(DataTransaction tx, DbKey id) throws OpenDcsDataException
     {
+        Connection conn = tx.connection(Connection.class).get();
+        Handle handle = Jdbi.open(conn);
+        handle.createUpdate("delete from opendcs_role where id = :id")
+              .bind("id", id.getValue())
+              .execute();
+    }
+ 
+    
+    private static class RoleMapper implements RowMapper<Role>
+    {
+
+        @Override
+        public Role map(ResultSet rs, StatementContext ctx) throws SQLException
+        {
+            DbKey key = DbKey.createDbKey(rs, "id");
+            String name = rs.getString("name");
+            String description = rs.getString("description");
+            ColumnMapper<ZonedDateTime> columnMapperForZDT = ctx.findColumnMapperFor(ZonedDateTime.class)
+                                                                .get();
+            ZonedDateTime updatedAt = columnMapperForZDT.map(rs, "updated_at", ctx);
+            return new Role(key, name, description, updatedAt);
+        }
         
     }
-    
 }
