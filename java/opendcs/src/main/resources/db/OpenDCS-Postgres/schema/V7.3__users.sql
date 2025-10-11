46a498b5d44109c7791e14a6819cded94e9d7b05
Mike Neilson <m.allan.neilson@gmail.com>
2025-10-11 08:16:58 -0700

    javadoc and pagination hooks.

--- java/opendcs/src/main/resources/db/OpenDCS-Postgres/schema/V7.3__users.sql
+++ java/opendcs/src/main/resources/db/OpenDCS-Postgres/schema/V7.3__users.sql
@@ -1,5 +1,5 @@
 create table identity_provider (
-    id uuid default gen_random_uuid() primary key,
+    id bigserial primary key,
     name varchar(256) not null unique,
     type varchar(256) not null,
     config jsonb default '{}'::jsonb,
@@ -7,8 +7,8 @@ create table identity_provider (
 );
 
 create table opendcs_user(
-    id uuid default gen_random_uuid() primary key,
-    preferred_name_provider uuid references identity_provider(id),
+    id bigserial primary key,
+    preferred_name_provider bigint references identity_provider(id),
     email text not null,
     preferences jsonb default '{}'::jsonb,
     created_at timestamptz not null default now()
@@ -16,28 +16,28 @@ create table opendcs_user(
 );
 
 create table opendcs_user_password(
-    user_id uuid references opendcs_user(id) not null,
+    user_id bigint references opendcs_user(id) not null,
     password text,
     updated_at timestamptz not null --- TODO make trigger
 );
 
 create table opendcs_role(
-    id uuid default gen_random_uuid() primary key,
+    id bigserial primary key,
     name varchar(128) not null unique,
     description text,
     updated_at timestamptz not null --- TODO make trigger
 );
 
 create table user_roles(
-    user_id uuid references opendcs_user(id) not null,
-    role_id uuid references opendcs_role(id) not null,
+    user_id bigint references opendcs_user(id) not null,
+    role_id bigint references opendcs_role(id) not null,
     primary key (user_id, role_id)
 );
 
 
 create table user_identity_provider(
-    user_id uuid references opendcs_user(id) not null,
-    identity_provider_id uuid references identity_provider(id) not null,
+    user_id bigint references opendcs_user(id) not null,
+    identity_provider_id bigint references identity_provider(id) not null,
     primary key (user_id, identity_provider_id),
     created_at timestamptz not null default now()
 );
