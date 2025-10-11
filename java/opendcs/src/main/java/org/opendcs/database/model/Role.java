96fc26944d486d12ceb93a6a62239402a6e60b45
Mike Neilson <m.allan.neilson@gmail.com>
2025-10-11 08:16:58 -0700

    javadoc and pagination hooks.

--- java/opendcs/src/main/java/org/opendcs/database/model/Role.java
+++ java/opendcs/src/main/java/org/opendcs/database/model/Role.java
@@ -1,17 +1,20 @@
 package org.opendcs.database.model;
 
 import java.time.ZonedDateTime;
-import java.util.UUID;
+import decodes.sql.DbKey;
 
+/**
+ * Defined Access Role.
+ */
 public class Role
 {
-    final UUID id;
+    final DbKey id;
     final String name;
     final String description;
     final ZonedDateTime updatedAt;
 
 
-    public Role(UUID id, String name, String description, ZonedDateTime updatedAt)
+    public Role(DbKey id, String name, String description, ZonedDateTime updatedAt)
     {
         this.id = id;
         this.name = name;
