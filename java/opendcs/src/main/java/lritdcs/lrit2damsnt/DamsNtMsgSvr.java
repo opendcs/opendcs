e390923df22627f9477f46a66a73869c6924af61
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2025-10-14 12:17:41 -0700

    update logging

--- java/opendcs/src/main/java/lritdcs/lrit2damsnt/DamsNtMsgSvr.java
+++ java/opendcs/src/main/java/lritdcs/lrit2damsnt/DamsNtMsgSvr.java
@@ -1,3 +1,18 @@
+/*
+* Where Applicable, Copyright 2025 OpenDCS Consortium and/or its contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy
+* of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 package lritdcs.lrit2damsnt;
 
 import java.io.IOException;
@@ -5,12 +20,15 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import org.opendcs.utils.logging.OpenDcsLoggerFactory;
+import org.slf4j.Logger;
+
 import ilex.net.BasicServer;
 import ilex.net.BasicSvrThread;
 
-public class DamsNtMsgSvr
-	extends BasicServer
+public class DamsNtMsgSvr extends BasicServer
 {
+	private static final Logger log = OpenDcsLoggerFactory.getLogger();
 	static final byte[] none = "NONE\r\n".getBytes();
 	private Lrit2DamsNt parent = null;
 
@@ -20,7 +38,7 @@ public class DamsNtMsgSvr
 		super(port, bindaddr);
 		this.parent = parent;
 	}
-	
+
 	@Override
 	protected BasicSvrThread newSvrThread(Socket sock)
 		throws IOException
@@ -33,16 +51,16 @@ public class DamsNtMsgSvr
 	 */
 	public void sendNone()
 	{
-		parent.debug("Sending NONE to clients.");
+		log.debug("Sending NONE to clients.");
 		sendToClients(none);
 	}
 
 	public void distribute(byte[] msgData)
 	{
-		parent.debug("Distributing DCP Message '" + new String(msgData) + "'");
+		log.debug("Distributing DCP Message '{}'", new String(msgData));
 		sendToClients(msgData);
 	}
-	
+
 	private void sendToClients(byte [] data)
 	{
 		ArrayList<DamsNtMsgSvrThread> badClients = new ArrayList<DamsNtMsgSvrThread>();
@@ -55,13 +73,14 @@ public class DamsNtMsgSvr
 			}
 			catch (IOException ex)
 			{
-				parent.warning("Error sending data to client '"
-					+ damsNtMsgSvrThread.getClientName() + "': " + ex 
-					+ " -- will disconnect.");
+				log.atWarn()
+				   .setCause(ex)
+				   .log("Error sending data to client '{}' -- will disconnect.",
+				   		damsNtMsgSvrThread.getClientName());
 				badClients.add(damsNtMsgSvrThread);
 			}
 		}
-		
+
 		// Can't do this in above loop because it would modify the collection that
 		// I'm iterating.
 		for(DamsNtMsgSvrThread client : badClients)
