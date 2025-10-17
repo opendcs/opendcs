9cbf97b1eb169c1d17ed0738af2a8153be6d620a
M. Allan Neilson <m.allan.neilson@gmail.com>
2025-10-17 08:06:10 -0700

    Initial setup of user element mappers.

--- java/opendcs/src/main/java/org/opendcs/database/model/mappers/RoleMapper.java
+++ java/opendcs/src/main/java/org/opendcs/database/model/mappers/RoleMapper.java
@@ -13,17 +13,29 @@ import decodes.sql.DbKey;
 
 public class RoleMapper implements RowMapper<Role>
 {
+    private final String prefix;
+
+    private RoleMapper(String prefix)
+    {
+        this.prefix = prefix == null ? ""
+                    : (prefix.endsWith("_") ? prefix : prefix+"_");
+    }
 
     @Override
     public Role map(ResultSet rs, StatementContext ctx) throws SQLException
     {
-        DbKey key = DbKey.createDbKey(rs, "id");
-        String name = rs.getString("name");
-        String description = rs.getString("description");
+        DbKey key = DbKey.createDbKey(rs, prefix+"id");
+        String name = rs.getString(prefix+"name");
+        String description = rs.getString(prefix+"description");
         ColumnMapper<ZonedDateTime> columnMapperForZDT = ctx.findColumnMapperFor(ZonedDateTime.class)
                                                             .get();
-        ZonedDateTime updatedAt = columnMapperForZDT.map(rs, "updated_at", ctx);
+        ZonedDateTime updatedAt = columnMapperForZDT.map(rs, prefix+"updated_at", ctx);
         return new Role(key, name, description, updatedAt);
     }
+ 
     
+    public static RoleMapper withPrefix(String prefix)
+    {
+        return new RoleMapper(prefix);
+    }
 }
---
+++ java/opendcs/src/main/java/org/opendcs/database/model/mappers/user/UserBuilderMapper.java
@@ -0,0 +1,61 @@
+package org.opendcs.database.model.mappers.user;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+import org.jdbi.v3.core.mapper.ColumnMapper;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.opendcs.database.model.User;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import decodes.sql.DbKey;
+
+public class UserBuilderMapper implements RowMapper<User.Builder>
+{
+    private final String prefix;
+    private final ObjectMapper om = new ObjectMapper();
+
+    private UserBuilderMapper(String prefix)
+    {
+        this.prefix = prefix == null ? ""
+                    : (prefix.endsWith("_") ? prefix : prefix+"_");
+    }
+
+
+    public static UserBuilderMapper withPrefix(String prefix)
+    {
+        return new UserBuilderMapper(prefix);
+    }
+
+    @Override
+    public User.Builder map(ResultSet rs, StatementContext ctx) throws SQLException
+    {
+        User.Builder builder = new User.Builder();
+        ColumnMapper<ZonedDateTime> columnMapperForZDT 
+                = ctx.findColumnMapperFor(ZonedDateTime.class)
+                     .get();
+        
+        builder.withId(DbKey.createDbKey(rs, prefix+"id"))
+               .withEmail(rs.getString(prefix+"email"))
+               .withUpdatedAt(columnMapperForZDT.map(rs, prefix+"updated_at", ctx))
+               .withCreatedAt(columnMapperForZDT.map(rs, prefix+"create_at", ctx))
+                ;
+        String jsonData = rs.getString("config");
+        Map<String, Object> preferences;
+        try
+        {
+            preferences = om.readValue(jsonData, new TypeReference<Map<String, Object>>() {});
+            return builder.withPreferences(preferences);
+        }
+        catch (JsonProcessingException ex)
+        {
+            throw new SQLException("Unable to parse provided json preferences", ex);
+        }
+    }
+}
