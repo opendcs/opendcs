a6765b4e4d76a2259a4433ac8da69f9eee807d8c
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-10-30 13:07:37 -0700

    Task/1276 logs in gui (#1482)
    Fixes #1276 in 7.0.
    
    Create mechanism to allow TraceDialog, and other systems, to acquire log
    information in a standard format.
    
    ---------
    
    Co-authored-by: Karl Tarbet <ktarbet@users.noreply.github.com>
    (cherry picked from commit 3d2b893549cf67d31d646bab90ac3edce7dc50be)

---
+++ java/opendcs-api/src/main/java/org/opendcs/util/RingBuffer.java
@@ -0,0 +1,201 @@
+package org.opendcs.util;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * A circular buffer.
+ */
+public class RingBuffer<T> implements List<T>
+{
+    // Performance hit, but easier to deal with initially
+    private final LinkedList<T> list = new LinkedList<>();
+    private int maxSize;
+
+    public RingBuffer(int size)
+    {
+        this.maxSize = size;
+    }
+
+
+    /**
+     * Set the size of this Buffer, NOTE: will grow but not shrink the backing array
+     * @param size
+     */
+    public void setSize(int size)
+    {
+        this.maxSize = size;
+    }
+
+    @Override
+    public boolean add(T element)
+    {
+        while (list.size() >= maxSize)
+        {
+            list.removeFirst();
+        }
+        return list.add(element);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> elements)
+    {
+        boolean ret = true;
+        for (T element: elements)
+        {
+            if (!add(element))
+            {
+                ret = false;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Return the number of active elements
+     * @return
+     */
+    public int size()
+    {
+        return list.size();
+    }
+
+    public T get(int index)
+    {
+        return list.get(index);
+    }
+
+    @Override
+    public Iterator<T> iterator()
+    {
+        return list.iterator();
+    }
+
+
+    @Override
+    public boolean isEmpty()
+    {
+        return list.isEmpty();
+    }
+
+
+    @Override
+    public boolean contains(Object o)
+    {
+        return list.contains(o);
+    }
+
+
+    @Override
+    public Object[] toArray()
+    {
+        return list.toArray();
+    }
+
+
+    @Override
+    public <T> T[] toArray(T[] a)
+    {
+        return list.toArray(a);
+    }
+
+
+    @Override
+    public boolean remove(Object o)
+    {
+        throw new UnsupportedOperationException("Only the buffer is allowed to remove elements.");
+    }
+
+
+    @Override
+    public boolean containsAll(Collection<?> c)
+    {
+        return list.containsAll(c);
+    }
+
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c)
+    {
+        throw new UnsupportedOperationException("Adding from index is not supported");
+    }
+
+
+    @Override
+    public boolean removeAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException("Only the buffer is allowed to remove elements.");
+    }
+
+
+    @Override
+    public boolean retainAll(Collection<?> c)
+    {
+        throw new UnsupportedOperationException("Unimplemented method 'retainAll'");
+    }
+
+
+    @Override
+    public void clear()
+    {
+        throw new UnsupportedOperationException("Only the buffer is allowed to remove elements.");
+    }
+
+
+    @Override
+    public T set(int index, T element)
+    {
+        throw new UnsupportedOperationException("setting element at index is not supported.");
+    }
+
+
+    @Override
+    public void add(int index, T element)
+    {
+        throw new UnsupportedOperationException("adding element at index is not supported.");
+    }
+
+
+    @Override
+    public T remove(int index)
+    {
+        throw new UnsupportedOperationException("Only the buffer is allowed to remove elements.");
+    }
+
+
+    @Override
+    public int indexOf(Object o)
+    {
+        return list.indexOf(o);
+    }
+
+
+    @Override
+    public int lastIndexOf(Object o)
+    {
+        return list.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<T> listIterator()
+    {   
+        return list.listIterator();
+    }
+
+
+    @Override
+    public ListIterator<T> listIterator(int index)
+    {
+        return list.listIterator(index);
+    }
+
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex)
+    {
+        return list.subList(fromIndex, toIndex);
+    }
+}
---
+++ java/opendcs/src/main/java/org/opendcs/gui/models/LoggingEventListModel.java
@@ -0,0 +1,62 @@
+package org.opendcs.gui.models;
+
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+import javax.swing.SwingUtilities;
+
+import org.opendcs.logging.LoggingEvent;
+import org.opendcs.logging.spi.LoggingEventProvider;
+import org.opendcs.utils.logging.LoggingEventBuffer;
+
+public final class LoggingEventListModel extends AbstractListModel<LoggingEvent>
+{
+    LoggingEventBuffer buffer;
+    List<LoggingEvent> events;
+
+    public LoggingEventListModel()
+    {
+        // arguably there should only be one buffer shared by all instances of this model
+        // but this currently works and future improvements should come later.
+        buffer = new LoggingEventBuffer.Builder()
+            .withProvider(LoggingEventProvider.getProvider())
+            .build();
+        events = buffer.getEvents();
+        final Thread t = new Thread(() -> 
+        {
+            while (true)
+            {
+                try
+                {
+                    final int size = getSize();
+                    SwingUtilities.invokeLater(() -> this.fireIntervalAdded(this, 0, size));
+                    Thread.sleep(500);
+                }
+                catch (InterruptedException ex)
+                {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }, "LoggingEventModel-update-thread");
+        t.setDaemon(true);
+        t.start();
+    }
+
+    public void setSize(int size)
+    {
+        buffer.setSize(size);
+    }
+
+    @Override
+    public int getSize()
+    {
+        return events.size();
+    }
+
+    @Override
+    public LoggingEvent getElementAt(int index)
+    {
+        return events.get(index);
+    }
+    
+}
