4c3bc6866facc1df148ebe38c88dbefbc35bef0d
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-11-13 16:55:45 +0000

    Modify RingBuffer interface to be inline with 7.0
    * Expose only Publisher instead of SubmissionPublisher.
    * Changes to account for the more generic publisher interface.
    * Move LoggingEvent and LoggedThrowable to records now that we're on
      Java 21.

--- java/opendcs-api/src/main/java/org/opendcs/logging/LoggingEvent.java
+++ java/opendcs-api/src/main/java/org/opendcs/logging/LoggingEvent.java
@@ -2,45 +2,33 @@ package org.opendcs.logging;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
 import java.util.Map;
 
 /**
  * Based on https://logback.qos.ch/manual/encoders.html#JsonEncoder as it is sufficiently
  * complete to allow any logger backend to map to this structure
  */
-public final class LoggingEvent {
-    public final Long sequence;
-    public final ZonedDateTime timestamp;
-    public final String level;
-    public final String threadName;
-    public final String loggerName;
-    public final Map<String, String> context;
-    public final Map<String, String> diagnosticContext;
-    public final Map<String, String> keyValuePairs;
-    public final String message;
-    public final LoggedThrowable throwable;
-
-
-    public LoggingEvent(Long sequence, ZonedDateTime timestamp, String level, String threadName, String loggerName,
-                        Map<String, String> context, Map<String, String> diagnosticContext,
-                        Map<String, String> keyValuePairs, String message, LoggedThrowable throwable)
-    {
-        this.sequence = sequence;
-        this.timestamp = timestamp;
-        this.level = level;
-        this.threadName = threadName;
-        this.loggerName = loggerName;
-        this.context = Collections.unmodifiableMap(context);
-        this.diagnosticContext = Collections.unmodifiableMap(diagnosticContext);
-        this.keyValuePairs = Collections.unmodifiableMap(keyValuePairs);
-        this.message = message;
-        this.throwable = throwable;
-    }
+public record LoggingEvent (
+    Long sequence, ZonedDateTime timestamp, String level, String threadName, String loggerName,
+    Map<String, String> context, Map<String, String> diagnosticContext,
+    Map<String, String> keyValuePairs, String message, LoggedThrowable throwable)
+{
 
     @Override
     public String toString()
     {
         return String.format("%s %s [%s] %s %s", timestamp.format(DateTimeFormatter.ISO_ZONED_DATE_TIME), level, threadName, loggerName, message);
+    }
+
+    public static final LoggingEvent of(String msg)
+    {
+        return of(msg, null);
+    }
+
+    public static final LoggingEvent of(String msg, Throwable throwable)
+    {
+        LoggedThrowable lt = LoggedThrowable.from(throwable);
+        return new LoggingEvent(0L, ZonedDateTime.now(), "INFO", Thread.currentThread().getName(),
+                         "manual", null, null, null, msg, lt);
     }
 }
