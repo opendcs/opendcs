b608084efe7a9f1adc818c41b264c52f5f39a80b
Mike Neilson <m.allan.neilson@gmail.com>
2025-11-15 07:55:27 -0800

    sql correction.

--- schema/opendcs-schemas/src/main/resources/db/OpenDCS-Postgres/schema/V7.4__tsdb_property_name_length.sql
+++ schema/opendcs-schemas/src/main/resources/db/OpenDCS-Postgres/schema/V7.4__tsdb_property_name_length.sql
@@ -1,2 +1,3 @@
-alter table tsdb_property alter column prop_name varchar(512);
-alter table tsdb_property alter column prop_value varchar(1024) drop not null;
+alter table tsdb_property alter column prop_name type varchar(512),
+                          alter column prop_value type varchar(1024),
+                          alter column prop_value drop not null;
