5e23b1a63d81bd2c84179bdc150d0f14344df732
Mike Neilson <m.allan.neilson@gmail.com>
2025-11-14 19:47:23 -0800

    \add simple 'doesn't fail' test of insert each configurations decodes settings.

--- schema/opendcs-schemas/src/main/resources/db/OpenDCS-Postgres/schema/V7.4__tsdb_property_name_length.sql
+++ schema/opendcs-schemas/src/main/resources/db/OpenDCS-Postgres/schema/V7.4__tsdb_property_name_length.sql
@@ -1,2 +1,2 @@
 alter table tsdb_property alter column prop_name type varchar(512);
-alter table tsdb_property alter column prop_value type varchar(1024);
+alter table tsdb_property alter column prop_value type varchar(1024) drop not null;
