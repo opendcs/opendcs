407ddfd128e4ba2eb65700f28e5e17a3bf6f8c97
Mike Neilson <m.allan.neilson@gmail.com>
2025-11-15 10:10:22 -0800

    actually correct sql

--- schema/opendcs-schemas-cwms-oracle/src/main/resources/db/CWMS-Oracle/schema/V7.4__tsdb_property_name_length.sql
+++ schema/opendcs-schemas-cwms-oracle/src/main/resources/db/CWMS-Oracle/schema/V7.4__tsdb_property_name_length.sql
@@ -1,2 +1,2 @@
-alter table tsdb_property modify (prop_name varchar2(512));
-alter table tsdb_property modify (prop_value varchar2(1024) nullable);
+alter table tsdb_property modify prop_name varchar2(512);
+alter table tsdb_property modify prop_value varchar2(1024) null;
