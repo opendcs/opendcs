3aab1d65660470e1ffcc1910fd8ad8aa728025d7
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-11-26 16:43:18 +0000

    Move provider to different package.

--- java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/builtin/BuiltInIdentityProvider.java
+++ java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/builtin/BuiltInIdentityProvider.java
@@ -13,7 +13,7 @@
 * License for the specific language governing permissions and limitations
 * under the License.
 */
-package org.opendcs.database.impl.opendcs;
+package org.opendcs.authentication.identityprovider.impl.builtin;
 
 import java.time.ZonedDateTime;
 import java.util.Map;
