b929b075f094f626dc5f014ba7e6effd9c89f950
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-12-04 22:52:47 +0000

    Update to Jakarta EE 11 for lrgs web interface.

--- java/lrgs-web/src/main/java/org/opendcs/lrgs/http/LrgsStatus.java
+++ java/lrgs-web/src/main/java/org/opendcs/lrgs/http/LrgsStatus.java
@@ -2,13 +2,13 @@ package org.opendcs.lrgs.http;
 
 import java.io.ByteArrayOutputStream;
 
-import javax.servlet.ServletContext;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.servlet.ServletContext;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import ilex.xml.XmlOutputStream;
 import lrgs.lrgsmain.LrgsMain;
