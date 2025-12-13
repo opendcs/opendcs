3e9083d874f578e601c7309324b976ac21004e57
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-13 08:35:21 -0800

    Correct test behavior.

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/regression_tests/SettingsTestIT.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/regression_tests/SettingsTestIT.java
@@ -5,9 +5,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Properties;
 
+import org.jdbi.v3.core.Handle;
 import org.junit.jupiter.api.Test;
 import org.opendcs.database.DatabaseService;
 import org.opendcs.database.SimpleOpenDcsDatabaseWrapper;
+import org.opendcs.database.api.DatabaseEngine;
 import org.opendcs.database.api.OpenDcsDatabase;
 import org.opendcs.fixtures.AppTestBase;
 import org.opendcs.fixtures.annotations.ConfiguredField;
@@ -20,6 +22,17 @@ class SettingsTestIT extends AppTestBase
     @ConfiguredField
     private OpenDcsDatabase db;
 
+    private static final String MERGE_QUERY = """
+                    merge into tsdb_property p
+                    using (select :name prop_name, :value prop_value <dual>) input
+                    on (p.prop_name = input.prop_name)
+                    when matched then
+                    update set prop_value = input.prop_value
+                    when not matched then
+                    insert (prop_name, prop_value)
+                    values(input.prop_name, input.prop_value)
+                    """;
+
     @EnableIfSql
     @Test
     void test_load_settings() throws Exception
@@ -31,17 +44,25 @@ class SettingsTestIT extends AppTestBase
             var props = new Properties();
             var settings = db.getSettings(DecodesSettings.class).orElseThrow();
             settings.saveToProps(props);
-            try (var tx = db.newTransaction();
-                var conn = tx.connection(Connection.class).orElseThrow();
-                PreparedStatement stmt = conn.prepareStatement("insert into tsdb_property(prop_name,prop_value) values(?,?)"))
-            {
-                for (var key: props.keySet())
+            try (var tx = db.newTransaction())
+		    {
+                // looks odd, need ro make surer the transaction closes it.
+                var conn = tx.connection(Handle.class).orElseThrow();
+                try (var upsertProp = conn.prepareBatch(MERGE_QUERY)
+                                          .define("dual", tx.getContext().getDatabase() == DatabaseEngine.ORACLE ? "from dual" : ""))
                 {
-                    stmt.setString(1, key.toString());
-                    stmt.setString(2, props.getProperty(key.toString()));
-                    stmt.addBatch();
+                    settings.saveToProps(props);
+                    for(var k: props.keySet())
+                    {
+                        final String value = props.getProperty(k.toString(), null);
+                        upsertProp.bind("name", k.toString())
+                                .bind("value", value)
+                                .add();
+                    }
+                    
+                    upsertProp.execute();
+                
                 }
-                stmt.executeBatch();
             }
         });
 
