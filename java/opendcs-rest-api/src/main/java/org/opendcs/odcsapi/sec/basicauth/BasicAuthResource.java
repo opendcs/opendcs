af8c763a703d90588437ea4c8820dd7bd4c5626f
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-13 17:50:02 -0800

    Various behavior correction.

--- java/opendcs-rest-api/src/main/java/org/opendcs/odcsapi/sec/basicauth/BasicAuthResource.java
+++ java/opendcs-rest-api/src/main/java/org/opendcs/odcsapi/sec/basicauth/BasicAuthResource.java
@@ -261,6 +261,10 @@ public final class BasicAuthResource extends OpenDcsResource
 	{
 		OpenDcsDatabase db = createDb();
 		ApiAuthorizationDAI dao = getAuthDao(db);
+		if (dao == null)
+		{
+			return Set.of(OpenDcsApiRoles.ODCS_API_GUEST);
+		}
 		try(DataTransaction tx = db.newTransaction())
 		{
 			return dao.getRoles(tx, username, organizationId);
@@ -274,6 +278,7 @@ public final class BasicAuthResource extends OpenDcsResource
 	private ApiAuthorizationDAI getAuthDao(OpenDcsDatabase db)
 	{
 		String databaseType = db.getSettings(DecodesSettings.class).orElseThrow().editDatabaseType;
+		log.info("Getting Auth DAO for {}", databaseType);
 		// Username+Password login only supported by OpenTSDB
 		if("opentsdb".equalsIgnoreCase(databaseType))
 		{
@@ -283,6 +288,6 @@ public final class BasicAuthResource extends OpenDcsResource
 		{
 			return new CwmsAuthorizationDAO();
 		}
-		throw new UnsupportedOperationException("Endpoint is unsupported by the OpenDCS REST API for database implementation '" + databaseType + "'");
+		return null;
 	}
 }
