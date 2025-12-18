e7199701f73ba1b2da019b78ee30d4e7bec4bc4d
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-12-18 23:26:06 +0000

    Very rough layout of part of what exists. user image fadeIn works.

--- javascript/opendcs-web-ui-react/src/pages/Login.tsx
+++ javascript/opendcs-web-ui-react/src/pages/Login.tsx
@@ -1,5 +1,8 @@
 import { useContext, type FormEvent } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { Container, Image } from "react-bootstrap";
+import './Login.css';
+import user_img from '../assets/img/user_profile_image_large.png'
 
 export default function Login() {
     const user = useContext(AuthContext);
@@ -12,12 +15,26 @@ export default function Login() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleLogin}>
-                Username: <input name="username" defaultValue={user?.user}/>
-                <button type="submit">Login</button>
-            </form>
+        <Container className="page-content">
+            <Container className="content-wrapper">
+                
+                <Container className="content loginPageBackground">
+                    <div className="fadeIn first">
+                        <Image src={user_img} id="icon" alt="User icon" />
+                    </div>
+                    <form onSubmit={handleLogin}>
+                        Username: <input name="username" defaultValue={user?.user}/><br/>
+                        Password: TODO<br/>
+                        Organization: TODO (select)<br/>
+                        <button type="submit">Login</button>
+                    </form>
+                </Container>
+            </Container>
+            <div>
+            
             
         </div>
+        </Container>
+        
     );
 }
