215d4705ffb5b84d4d679226cb2d19bf8fabc4c5
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-12-19 22:51:16 +0000

    Rought application/auth context behavior in place.

--- javascript/opendcs-web-ui-react/src/main.tsx
+++ javascript/opendcs-web-ui-react/src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './main.css'
+import './assets/opendcs-shim.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
