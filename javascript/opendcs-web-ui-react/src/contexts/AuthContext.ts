a699710b00cb4f626e921a089f86af5ee54b7f76
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-21 10:48:59 -0800

    Improved auth context management. added logout placeholder.

--- javascript/opendcs-web-ui-react/src/contexts/AuthContext.ts
+++ javascript/opendcs-web-ui-react/src/contexts/AuthContext.ts
@@ -1,15 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, type SetStateAction } from "react";
 
 export type User = {
-    username?: string
-}
+    username: string
+};
 
 export interface AuthContextType {
-    user: User,
-    setUser: React.Dispatch<React.SetStateAction<User>>
-}
+    user?: User;
+    setUser: React.Dispatch<React.SetStateAction<User|undefined>>;
+    logout: () => void;
+};
 
-export const AuthContext = createContext<AuthContextType|undefined>(undefined);
+const defaultValue: AuthContextType = {
+    user: undefined,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setUser: (_value: SetStateAction<User|undefined>) => {},
+    logout: () => {}
+};
+
+export const AuthContext = createContext<AuthContextType>(defaultValue);
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
