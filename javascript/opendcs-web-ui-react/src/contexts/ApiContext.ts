6aedc448e046f9aa04079d0b97efc488e77891f8
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-21 13:27:22 -0800

    Flow of Login -> logout behaves correctly.

--- javascript/opendcs-web-ui-react/src/contexts/ApiContext.ts
+++ javascript/opendcs-web-ui-react/src/contexts/ApiContext.ts
@@ -1,33 +1,34 @@
 import type { Configuration } from "opendcs-api";
-import { createConfiguration, ServerConfiguration } from "opendcs-api";
+import {createConfiguration, ServerConfiguration} from 'opendcs-api'
 import { createContext, useContext, type SetStateAction } from "react";
 
 export type ApiSetup = {
-  conf: Configuration;
-  org: string | undefined;
+    conf: Configuration;
+    org: string | undefined
 };
 
 export interface ApiContextType {
-  conf: Configuration;
-  org: string | undefined;
-  setOrg: React.Dispatch<React.SetStateAction<string | undefined>>;
-}
+    conf: Configuration;
+    org: string | undefined;
+    setOrg: React.Dispatch<React.SetStateAction<string|undefined>>;
+};
 
 export const defaultValue: ApiContextType = {
-  conf: createConfiguration({
-    baseServer: new ServerConfiguration("/odcsapi", {}),
-  }),
-  org: undefined,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setOrg: (_value: SetStateAction<string | undefined>): void => {},
+    conf: createConfiguration({
+        baseServer: new ServerConfiguration("/odcsapi", {}),
+    }),
+    org: undefined,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setOrg: (_value: SetStateAction<string | undefined>): void => {}
 };
 
 export const ApiContext = createContext<ApiContextType>(defaultValue);
 
 export const useApi = () => {
-  const context = useContext(ApiContext);
-  if (context == undefined) {
-    throw new Error("Api isn't defined?");
-  }
-  return context;
-};
+    const context = useContext(ApiContext);
+    if (context == undefined)
+    {
+        throw new Error("Api isn't defined?");
+    }
+    return context;
+}
