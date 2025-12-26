ee7042cabdeaf5b385d061ea4f5b8969b3e13ed5
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-25 16:25:54 -0800

    Initial setup of storybook/playright framework.

--- javascript/opendcs-web-ui-react/eslint.config.js
+++ javascript/opendcs-web-ui-react/eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
--- javascript/opendcs-web-ui-react/vite.config.ts
+++ javascript/opendcs-web-ui-react/vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest/config" />
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), svgr()],
   optimizeDeps: {
@@ -12,28 +20,49 @@ export default defineConfig({
   server: {
     fs: {
       // Allow serving files from one level up the project root
-      allow: [".", resolve(__dirname, "../../java/api-clients/api-client-typescript/build/generated/openApi")],
+      allow: [".", resolve(__dirname, "../../java/api-clients/api-client-typescript/build/generated/openApi")]
       // Or explicitly allow the path to your linked package
       // allow: [path.resolve(__dirname, '../path/to/your/linked-package')]
     },
     proxy: {
       // Proxy requests starting with '/api'
       '/odcsapi': {
-        target: 'http://localhost:7000', // The address of your backend server
+        target: 'http://localhost:7000' // The address of your backend server
         //changeOrigin: true, // Needed for virtual hosted sites
-      },
-    }      
+      }
+    }
   },
   resolve: {
     alias: {
       'opendcs-api': resolve(__dirname, "../../java/api-clients/api-client-typescript/build/generated/openApi")
     }
   },
-  build:
-  {
-    commonjsOptions:
-    {
-          include:[/opendcs-api/, /node_modules/],
-    },
+  build: {
+    commonjsOptions: {
+      include: [/opendcs-api/, /node_modules/]
+    }
   },
-})
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
