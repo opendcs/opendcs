cd25d677e410f6b3bb56d0b3d720b7b730cf6d16
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-26 13:42:33 -0800

    Initial properties table and story setup.

--- javascript/opendcs-web-ui-react/src/components/Properties.stories.tsx
+++ javascript/opendcs-web-ui-react/src/components/Properties.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import Properties from './Properties';
+import Properties, { type Property } from './Properties';
 
 const meta = {
   component: Properties,
@@ -10,6 +10,34 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {}
+const theProps: Property[] = [];
+
+export const Empty: Story = {
+  args: {
+    theProps: theProps,
+    addProp: () => {theProps.push({
+      name: 'test',
+      value: 'test value'
+    })},
+    classes: ""
+  }
+};
+
+
+export const NotEmpty: Story = {
+  args: {
+    theProps: [
+      {name: 'prop1', value: 'val1'},
+      {name: 'prop2', value: 'val2'},
+      {name: 'prop3', value: 'val3'},
+      {name: 'prop4', value: 'val4'},
+      {name: 'prop5', value: 'val5'},
+      {name: 'prop6', value: 'val6'},
+      {name: 'prop7', value: 'val7'},
+      {name: 'prop8', value: 'val8'},
+      
+    ],
+    addProp: () => {},
+    classes: ""
+  }
 };
