dd778bae436bf847fa795fe4d0c5bfa820d1555f
Mike Neilson <m.allan.neilson@gmail.com>
2025-12-26 13:55:18 -0800

    Apply formatting.

--- javascript/opendcs-web-ui-react/src/App.tsx
+++ javascript/opendcs-web-ui-react/src/App.tsx
@@ -1,54 +1,53 @@
-import { useEffect } from 'react'
-import './App.css'
-import { useAuth } from './contexts/AuthContext';
-import Login from './pages/Login';
-import { TopBar } from './components/TopBar';
-import { ModeIcons } from './components/ModeIcon';
+import { useEffect } from "react";
+import "./App.css";
+import { useAuth } from "./contexts/AuthContext";
+import Login from "./pages/Login";
+import { TopBar } from "./components/TopBar";
+import { ModeIcons } from "./components/ModeIcon";
 
-import {RESTAuthenticationAndAuthorizationApi} from 'opendcs-api'
-import { useApi } from './contexts/ApiContext';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { SideBar } from './components/SideBar';
-import { Platforms } from './pages/Platforms';
-import { Sites } from './pages/Sites';
-import { Container } from 'react-bootstrap';
-import { Algorithms } from './pages/Algorithms';
+import { RESTAuthenticationAndAuthorizationApi } from "opendcs-api";
+import { useApi } from "./contexts/ApiContext";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { SideBar } from "./components/SideBar";
+import { Platforms } from "./pages/Platforms";
+import { Sites } from "./pages/Sites";
+import { Container } from "react-bootstrap";
+import { Algorithms } from "./pages/Algorithms";
 
-function App() {  
+function App() {
   const navigate = useNavigate();
-  const {setUser} = useAuth();
+  const { setUser } = useAuth();
   const api = useApi();
-
 
   useEffect(() => {
     const auth = new RESTAuthenticationAndAuthorizationApi(api.conf);
-    auth.checkSessionAuthorization()
-        // The current API spec (in the generated api) shows string as the return still
-        // the endpoint *correctly* returns a user object. likely just an issue
-        // with the autocomplete cache on my system.
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        .then((value: any) => setUser(value))
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        .catch((_error: any) => navigate("/login"));
+    auth
+      .checkSessionAuthorization()
+      // The current API spec (in the generated api) shows string as the return still
+      // the endpoint *correctly* returns a user object. likely just an issue
+      // with the autocomplete cache on my system.
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then((value: any) => setUser(value))
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      .catch((_error: any) => navigate("/login"));
   }, [api.conf, setUser]);
-
 
   return (
     <>
-        <ModeIcons />
-        <TopBar />
-        <Container fluid className="page-content d-flex">
-          <Routes>
-            <Route path="/login" element={<Login />}/>
-            <Route element={<SideBar />}>
-              <Route path="/platforms" element={<Platforms />}/>
-              <Route path="/sites" element={<Sites />}/>
-              <Route path="/algorithms" element={<Algorithms />}/>
-            </Route>
-          </Routes>
-        </Container>
-      </>
-  )
+      <ModeIcons />
+      <TopBar />
+      <Container fluid className="page-content d-flex">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<SideBar />}>
+            <Route path="/platforms" element={<Platforms />} />
+            <Route path="/sites" element={<Sites />} />
+            <Route path="/algorithms" element={<Algorithms />} />
+          </Route>
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
--- javascript/opendcs-web-ui-react/src/contexts/ThemeProvider.tsx
+++ javascript/opendcs-web-ui-react/src/contexts/ThemeProvider.tsx
@@ -2,19 +2,12 @@ import { useState, type ReactNode } from "react";
 import { ThemeContext, type ColorMode, type Theme } from "./ThemeContext";
 
 interface ProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const ThemeProvider = ({children}: ProviderProps) => {
-    const stored: ColorMode = localStorage.getItem('theme-mode') as ColorMode || 'auto';
+export const ThemeProvider = ({ children }: ProviderProps) => {
+  const stored: ColorMode = (localStorage.getItem("theme-mode") as ColorMode) || "auto";
 
-
-    const [theme, setTheme] = useState<Theme>({colorMode: stored});    
-    return (
-        <ThemeContext value={{theme, setTheme}}>
-            {children}
-        </ThemeContext>
-    );
+  const [theme, setTheme] = useState<Theme>({ colorMode: stored });
+  return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
 };
-
-
