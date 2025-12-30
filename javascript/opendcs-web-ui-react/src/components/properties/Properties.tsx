c8ea586b48bb5e5b12429c89836019efd066cb48
Michael Neilson <michael.a.neilson@usace.army.mil>
2025-12-30 11:29:09 -0800

    Working out rendering the cells they way desired.

--- javascript/opendcs-web-ui-react/src/components/properties/Properties.tsx
+++ javascript/opendcs-web-ui-react/src/components/properties/Properties.tsx
@@ -1,6 +1,7 @@
-import DataTable, { type DataTableProps } from "datatables.net-react";
-import DT from "datatables.net-bs5";
-import { Button, ButtonGroup, Container } from "react-bootstrap";
+import DataTable, { type DataTableProps, type DataTableSlots } from "datatables.net-react";
+import DT, { type ConfigColumns } from "datatables.net-bs5";
+import dtButtons from "datatables.net-buttons-bs5"
+import { Button, Container } from "react-bootstrap";
 import 'datatables.net-responsive';
 import { Pencil, Trash } from "react-bootstrap-icons";
 import type { ApiPropSpec } from "../../../../../java/api-clients/api-client-typescript/build/generated/openApi/dist";
@@ -8,6 +9,7 @@ import type { ApiPropSpec } from "../../../../../java/api-clients/api-client-typ
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 DataTable.use(DT);
+DataTable.use(dtButtons);
 
 
 export interface Property {
@@ -26,13 +28,14 @@ export interface PropertiesTableProps {
 };
 export interface ActionProps {
     data: Property,
-    removeProp: (prop: string) => void
+    removeProp: (prop: string) => void,
+    editProp: (prop: string) => void
 }
 
-export const PropertyActions: React.FC<ActionProps> = ({data, removeProp}) => {
+export const PropertyActions: React.FC<ActionProps> = ({data, removeProp, editProp}) => {
     return (
         <div>
-            <Button onClick={() => console.log("would edit")} aria-label="edit property" size='sm'><Pencil /></Button>
+            <Button onClick={() => editProp(data.name)} variant="warning" aria-label="edit property" size='sm'><Pencil /></Button>
             <Button onClick={() => removeProp(data.name)} variant='danger' size='sm'
                     aria-label="delete property"><Trash /></Button>
         </div>
@@ -40,22 +43,40 @@ export const PropertyActions: React.FC<ActionProps> = ({data, removeProp}) => {
 };
 
 export const PropertiesTable: React.FC<PropertiesTableProps> = ({theProps, addProp, removeProp, width = '20em', height = '100vh', classes = ''}) => {
-    const columns = [
-        {data: "name"},
-        {data: "value"},
-        {data: null}
+    const columns: ConfigColumns[]  = [
+        {data: "name", render: (data, type, row, meta) => {
+            console.log("Data: " + JSON.stringify(data));
+            console.log("Type: " + type);
+            console.log("Row: " + JSON.stringify(row));
+            console.log(meta);
+            return data;
+        }},
+        {data: "value",},
+        {data: null, name:"actions"}
     ];
     const options: DataTableProps["options"] = {
         paging: false,
         stripeClasses: ["bg-primary-subtle", "bg-subtle"],
-        responsive: true
+        responsive: true,
+        layout: {
+            top1Start: {
+                buttons: [
+                    {
+                        text:'+',
+                        action: () => {addProp()},
+                    }
+                ]
+            }
+        }
     };
 
-    const slots = {
+    const slots: DataTableSlots = {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        Actions: (data: Property, type: unknown, _row: unknown) => {
+        actions: (data: Property, type: unknown, row: unknown) => {
             if (type === 'display') {
-                return (<PropertyActions data={data} removeProp={removeProp}/>)
+                return (<PropertyActions data={data} removeProp={removeProp} editProp={(name: string) => {
+                    
+                }}/>)
             } else {
                 return data;
             }
@@ -68,11 +89,7 @@ export const PropertiesTable: React.FC<PropertiesTableProps> = ({theProps, addPr
                            className="table table-hover table-striped tablerow-cursor w-100 border">
                 <caption className="captionTitleCenter">
 					Properties
-                    <Button variant="secondary" className="float-right captionButton mt-1"
-                        onClick={
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        (_e: unknown) => addProp()
-                    } aria-label="add property">+</Button>
+                    
                 </caption>
                 <thead>
                     <tr>
