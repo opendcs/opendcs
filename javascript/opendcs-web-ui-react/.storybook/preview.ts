40bb9e5fcb36006f264d949058920757b3c8bc8d
Mike Neilson <m.allan.neilson@gmail.com>
2026-01-01 16:10:02 -0800

    Additional cleanup and attempts at getting the test to work.

--- javascript/opendcs-web-ui-react/.storybook/preview.ts
+++ javascript/opendcs-web-ui-react/.storybook/preview.ts
@@ -1,9 +1,11 @@
 import type { Preview } from '@storybook/react-vite'
+//import { useArgs } from '@storybook/preview-api';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'datatables.net-bs5'
 import 'datatables.net-responsive-bs5'
 import '../src/main.css'
 import '../src/assets/opendcs-shim.css'
+import { useArgs } from 'storybook/internal/preview-api';
 
 
 const preview: Preview = {
@@ -22,6 +24,13 @@ const preview: Preview = {
       test: 'todo'
     }
   },
+  decorators: [
+    (story, context) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_, updateArgs] = useArgs();
+      return story({ ...context, updateArgs });
+    },
+  ],
 };
 
 export default preview;
