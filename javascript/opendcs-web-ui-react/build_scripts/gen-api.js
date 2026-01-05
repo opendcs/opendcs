7e807f31293c2ea852b84a32987c19e0f3ab5567
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-05 10:58:57 -0800

    Use node script for api generate and run tasks to handle cross platform and recursion.

---
+++ javascript/opendcs-web-ui-react/build_scripts/gen-api.js
@@ -0,0 +1,18 @@
+import {execSync} from 'child_process';
+import path from 'path';
+
+const inGradle = process.env.IN_GRADLE? true : false;
+
+const workingDirectory = path.resolve(process.cwd(),"..","..");
+
+if (!inGradle) { // if we're in a gradle environment, it would've done this for us already
+    try {
+        execSync("gradlew :api-client-typescript:build --info -Pno.docs=true", {cwd: workingDirectory, stdio: 'inherit'});
+        process.exit(0);
+    } catch (error) {
+        console.error(`Command failed ${error.message}`);
+        process.exit(1);
+    }
+} else {
+    console.log("In gradle build, assuming gradle peformed this step for us.");
+}
