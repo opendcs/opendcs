d2cae6f8a0d66ae1f919b265a7fbe9b4ac2ff607
Andrew Gilmore <andrew@precisionwre.com>
2026-01-09 22:50:51 -0700

    Implement Before/After export/import and cleaner test definitions
    Signed-off-by: Andrew Gilmore <andrew@precisionwre.com>

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/regression_tests/DBImportExportTest.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/regression_tests/DBImportExportTest.java
@@ -3,6 +3,8 @@ package org.opendcs.regression_tests;
 import decodes.dbimport.DbExport;
 import decodes.dbimport.DbImport;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.opendcs.fixtures.AppTestBase;
@@ -15,11 +17,9 @@ import uk.org.webcompere.systemstubs.SystemStubs;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,25 +29,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DBImportExportTest extends AppTestBase
 {
     private static final Logger log = OpenDcsLoggerFactory.getLogger();
-    //TODO: try DBExport @Before this complete test, and DBImport @After, so other tests aren't impacted by the clean imports.
-    //TODO: remove golden comparisons since they have indeterminate orders and won't compare cleanly crossplatform
-    //TODO: change name of class since DBExport tests will not be included
-    //TODO: move try handling to assertdoesnotthrow per PR review comments
+    private static int prevPlats = 0;
+    private static int prevRS = 0;
+    // Since the order of DBExport results are non-determinate, we can't use a golden comparison.
+    // If tests after this one start failing, the DBExport/Import functions may have been impacted.
+    @BeforeAll
+    public void beforeTests() throws Throwable
+    {
+        prevPlats = configuration.getDecodesDatabase().platformList.size();
+        prevRS = configuration.getDecodesDatabase().routingSpecList.size();
+
+        File logFile = new File(TestResources.getResource(configuration,"DBImportExportTest/ExportBefore.log"));
+
+        String prevDb = SystemStubs.tapSystemOut(
+                () -> exit.execute(() ->
+                        DbExport.main(
+                                args("-l", logFile.getAbsolutePath(), "-d3")
+                        )
+                )
+        );
+        assertExitNullOrZero();
+
+        FileWriter exportXMLFile = new FileWriter(TestResources.getResource(configuration, "DBImportExportTest/prevexport.xml"));
+        IOUtils.write(prevDb, exportXMLFile);
+        IOUtils.closeQuietly(exportXMLFile);
+    }
 
     @ParameterizedTest
-    @CsvSource(useHeadersInDisplayName = true, value = { // would check datasources here too, but they're always zero in the DB for some reason
-        "Name, Expected DBExport File, Expected Platforms, Expected Routing Specs",
-        "normal,DBImportExportTest/goldennormal${impl}.xml,3,2",
-        "platonly,DBImportExportTest/goldenplatonly${impl}.xml,3,1",
-        "platover,DBImportExportTest/goldenplatover${impl}.xml,2,1",
+    @CsvSource(useHeadersInDisplayName = true, value = {
+        "Name, Description, DBImport Arguments, Expected Platforms, Expected Routing Specs",
+        "normal,DBImport with no extra flags,,3,2",
+        "platOnly,DBImport with platform only flag,-p,3,1",
+        "platOver,DBImport with platform and overwrite flag,-p -W -y,2,1",
     })
-    public void test_normal(String type, String expectedResultFile, int plats, int rs) throws Exception
+    public void test_normal(String type, String desc, String testArgs, int expectedPlats, int expectedRS) throws Throwable
     {
         Configuration config = this.configuration;
         File logFile = new File(config.getUserDir(),"/dbimport-" + type + ".log");
 
-        assertExitNullOrZero();
-
+        // uses overwrite flags to reset database to completely known state.
         String clearDB = SystemStubs.tapSystemOut(
                 () -> exit.execute(() ->
                         DbImport.main(
@@ -58,72 +78,48 @@ public class DBImportExportTest extends AppTestBase
         );
         assertExitNullOrZero();
 
-        List<String> platargs =  new ArrayList<>(Arrays.asList("-l", logFile.getAbsolutePath(), "-d3"));
-        switch(type) {
-            case "normal":
-                log.info("Loading platform, routing spec, etc with no extra flags.");
-                break;
-            case "platonly":
-                log.info("Loading platforms with platform only flag.");
-                platargs.add("-p");
-                break;
-            case "platover":
-                log.info("Loading platforms with platform only and overwrite flags.");
-                platargs.addAll(Arrays.asList("-W", "-y", "-p"));
-                break;
+        List<String> platArgs =  new ArrayList<>(Arrays.asList("-l", logFile.getAbsolutePath(), "-d3"));
+        if (testArgs != null) {
+            platArgs.addAll(Arrays.asList(testArgs.split(" ")));
         }
-        String newFile = TestResources.getResource(configuration,"DBImportExportTest/new.xml");
-        platargs.add(newFile);
 
+        String newFile = TestResources.getResource(configuration,"DBImportExportTest/new.xml");
+        platArgs.add(newFile);
+
+        log.info("Running Test: {}", desc);
         String importOut = SystemStubs.tapSystemOut(
                             () -> exit.execute(() ->
                                     DbImport.main(
-                                            platargs.toArray(new String[0])
+                                            platArgs.toArray(new String[0])
                                     )
                             )
                         );
-
         assertExitNullOrZero();
 
-        String exportOut = SystemStubs.tapSystemOut(
+        // would check the number of datasources here too, but that list is always length zero in the DB for some reason
+        int existingPlats = configuration.getDecodesDatabase().platformList.size();
+        int existingRS = configuration.getDecodesDatabase().routingSpecList.size();
+        assertEquals(expectedPlats, existingPlats, "Expected number of platforms not found!");
+        assertEquals(expectedRS, existingRS, "Expected number of data sources not found!");
+    }
+
+    @AfterAll
+    public void afterTests() throws Throwable
+    {
+        File logFile = new File(TestResources.getResource(configuration,"DBImportExportTest/ImportAfter.log"));
+
+        String restoreDB = SystemStubs.tapSystemOut(
                 () -> exit.execute(() ->
-                        DbExport.main(
-                                args("-l", logFile.getAbsolutePath(), "-d3")
+                        DbImport.main(
+                                args("-l", logFile.getAbsolutePath(), "-d3", "-W", "-y",
+                                        TestResources.getResource(configuration, "DBImportExportTest/prevexport.xml"))
                         )
                 )
         );
         assertExitNullOrZero();
-
-        //String cleanedOut = cleanString(exportOut);
-
-        //File goldenFile = new File(TestResources.getResource(configuration,expectedResultFile));
-        //String golden = cleanString(IOUtils.toString(goldenFile.toURI().toURL().openStream(), StandardCharsets.UTF_8));
-        // code to generate golden results
-        //FileWriter exportXMLFile = new FileWriter(TestResources.getResource(configuration,type + "export.xml"));
-        //IOUtils.write(exportOut, exportXMLFile);
-        //IOUtils.closeQuietly(exportXMLFile);
-        //assertEquals(golden, cleanedOut, "Output Doesn't match expected data."); //how to handle modified times?!?
-
-        try
-        {
-            int existingplats = configuration.getDecodesDatabase().platformList.size();
-            int existingrs = configuration.getDecodesDatabase().routingSpecList.size();
-            assertEquals(plats, existingplats, "Expected number of platforms not found!");
-            assertEquals(rs, existingrs, "Expected number of data sources not found!");
-        } catch (Throwable e)
-        {
-            throw new RuntimeException(e);
-        }
+        int newPlats = configuration.getDecodesDatabase().platformList.size();
+        int newRs = configuration.getDecodesDatabase().routingSpecList.size();
+        assertEquals(prevPlats, newPlats, "Expected number of platforms not found!");
+        assertEquals(prevRS, newRs, "Expected number of data sources not found!");
     }
-
-    private String cleanString(String exportOut)
-    {
-        List<String> exportLines = new ArrayList<String>(Arrays.asList(exportOut.split("\\R")));
-        return exportLines.stream().filter(line -> !line.contains("LastModifyTime"))
-                .filter(line -> !line.contains("PlatformId"))
-                .filter(line -> !line.contains("password"))
-                .collect(Collectors.joining(System.lineSeparator()));
-    }
-
-
 }
