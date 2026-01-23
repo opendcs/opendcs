b21dfcd843b834d44021f96475baf0e5c8d6194f
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-23 15:01:28 -0800

    Correct formatting.

--- javascript/opendcs-web-ui-react/src/util/Actions.tsx
+++ javascript/opendcs-web-ui-react/src/util/Actions.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> d3e6e680 (Reorg of the 'actions'. Support for read only table.)
 /**
  * Interface that provides for various actions. All actions are
  * optional indicating that that action is not supported and those controls should
@@ -10,8 +6,7 @@
  * @param T Is the primary type that will be interacted with. The whole object
  * @param V can be either the same as T, or another type such as for a "key"
  */
-<<<<<<< HEAD
-export interface CollectionActions<T, V = T> {
+export interface Actions<T, V = T> {
   /**
    * Peform appropriate operation to "save" and item
    * @param item The item to save
@@ -36,30 +31,3 @@ export interface CollectionActions<T, V = T> {
    */
   edit?: (item: V) => void;
 }
-=======
-export interface Actions<T, V=T> {
-    /**
-     * Peform appropriate operation to "save" and item
-     * @param item The item to save
-     * @returns nothing
-     */
-    save?: (item: T) => void;
-    /**
-     * Perform an appropriate operation to remove a given item from it's source
-     * @param item can be full item, or a key type
-     * @returns nothing
-     */
-    remove?: (item: V) => void;
-    /**
-     * Add a new item to a given source
-     * @returns nothing
-     */
-    add?: (item?: T) => void;
-    /**
-     * Inform whatever needs to here it that this item needs to be editted
-     * @param item The item, or a key to the item.
-     * @returns nothing
-     */
-    edit?: (item: V) => void;
-}
->>>>>>> d3e6e680 (Reorg of the 'actions'. Support for read only table.)
