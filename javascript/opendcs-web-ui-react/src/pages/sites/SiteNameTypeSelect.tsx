a736e431f9f3799371f9b952bff1d81eb4708c64
Michael Neilson <m.allan.neilson@gmail.com>
2026-01-25 14:47:28 -0800

    Created basic context for reflist for initial use in sitenamelist and type selection.

---
+++ javascript/opendcs-web-ui-react/src/pages/sites/SiteNameTypeSelect.tsx
@@ -0,0 +1,26 @@
+import { FormSelect } from "react-bootstrap";
+import { REFLIST_SITE_NAME_TYPE, useRefList } from "../../contexts/RefListContext";
+
+
+export interface SiteNameTypeSelectProperties {
+    current: string | undefined;
+}
+
+export const SiteNameTypeSelect: React.FC<SiteNameTypeSelectProperties> = ({current}) => {
+    const {refList} = useRefList();
+    return (
+        <FormSelect name="siteNameType">
+            {refList("nametype").items ?
+                Object.values(refList(REFLIST_SITE_NAME_TYPE).items!)
+                      .map((item) => {
+                            return <option key={item.value} value={item.value} selected={current === item.value}>{item.value}</option>
+                      })
+            :
+            null
+            }
+        </FormSelect>
+
+    );
+};
+
+export default SiteNameTypeSelect;
