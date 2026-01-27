7451db2347f37ed5b23af5a3cf10ff5ea28d0ff6
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-27 14:10:48 -0800

    Implement properties of the reducer.

--- javascript/opendcs-web-ui-react/src/pages/sites/SiteReducer.ts
+++ javascript/opendcs-web-ui-react/src/pages/sites/SiteReducer.ts
@@ -1,36 +1,58 @@
 import type { UiSite } from "./Site";
 
+export type SiteAction =
+  | { type: "add_name"; payload: { type: string; name: string } }
+  | { type: "change_type"; payload: { old_type: string; new_type: string } }
+  | { type: "delete_name"; payload: { type: string } }
+  // save/edit/add prop functionally equivalent
+  | { type: "save_prop"; payload: { name: string; value: string } }
+  | { type: "delete_prop"; payload: { name: string } }
+  | { type: "save"; payload: { site: UiSite } };
 
-
-export type Action = {type: "add_name"; playload: {type: string, name: string}} |
-              {type: "change_type"; playload: {old_type: string, new_type: string}};
-
-
-export function SiteReducer(currentSite: UiSite, action: Action): UiSite {
-    switch (action.type) {
-        case 'add_name': {
-            return {
-                ...currentSite,
-                sitenames: {
-                    ...currentSite.sitenames, 
-                    [action.playload.type]: action.playload.name        
-                }
-            }
-        }
-        case 'change_type': {
-            type sitenames = {[k: string]: string};
-            const curKey: keyof sitenames = action.playload.old_type;
-            const curValue = currentSite.sitenames![action.playload.old_type];
-            const {[curKey]: _, ...currentSiteNames} = currentSite.sitenames;
-
-            return {
-                ...currentSite,
-                sitenames: {
-                    ...currentSiteNames,
-                    [action.playload.new_type]: curValue
-                }
-                
-            }
-        }
+export function SiteReducer(currentSite: UiSite, action: SiteAction): UiSite {
+  switch (action.type) {
+    case "add_name": {
+      return {
+        ...currentSite,
+        sitenames: {
+          ...currentSite.sitenames,
+          [action.payload.type]: action.payload.name,
+        },
+      };
     }
+    case "change_type": {
+      type sitenamesTypes = { [k: string]: string };
+      const curKey: keyof sitenamesTypes = action.payload.old_type;
+      const curValue = currentSite.sitenames![action.payload.old_type];
+      const { [curKey]: tmp, ...currentSiteNames } = currentSite.sitenames!;
+
+      return {
+        ...currentSite,
+        sitenames: {
+          ...currentSiteNames,
+          [action.payload.new_type]: curValue,
+        },
+      };
+    }
+    case "save_prop": {
+      return {
+        ...currentSite,
+        properties: {
+          ...currentSite.properties,
+          [action.payload.name]: action.payload.value,
+        },
+      };
+    }
+    case "delete_prop": {
+      const { [action.payload.name]: _, ...props } = currentSite.properties!;
+      return {
+        ...currentSite,
+        properties: {
+          ...props,
+        },
+      };
+    }
+    default:
+      throw new Error(`Type ${action.type} not yet implemented.`);
+  }
 }
