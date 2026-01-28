42507c766e403166627a47fbc5c460e52ace3479
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-28 10:50:29 -0800

    Adjust SiteNameList data type.

--- javascript/opendcs-web-ui-react/src/pages/sites/Site.stories.tsx
+++ javascript/opendcs-web-ui-react/src/pages/sites/Site.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import Site from "./Site";
+import { expect } from "storybook/test";
 
 const meta = {
   component: Site,
@@ -11,21 +12,37 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    site: {}
+  },
+  play: async ({ canvas, mount, parameters }) => {
+    const {i18n} = parameters;
+    await mount();
+    expect(canvas.queryByText(i18n.t("latitude"))).toBeInTheDocument();
+  }
 };
+
+const site1 = {
+    siteId: 1,
+    description: "A test",
+    properties: {
+      prop1: "value1",
+    },
+    sitenames: {
+      CWMS: "Alder Springs",
+      NWSHB5: "ALS",
+    },
+  };
 
 export const WithSite: Story = {
   args: {
-    site: {
-      siteId: 1,
-      description: "A test",
-      properties: {
-        prop1: "value1",
-      },
-      sitenames: {
-        CWMS: "Alder Springs",
-        NWSHB5: "ALS",
-      },
-    },
+    site: site1
   },
 };
+
+
+export const WithSiteAsPromise: Story = {
+  args: {
+    site: Promise.resolve(site1)
+  }
+}
