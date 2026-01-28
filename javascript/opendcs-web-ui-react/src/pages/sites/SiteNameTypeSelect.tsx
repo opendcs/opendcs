3eb3d40e82a60e58221b9bbaa6bded37bf0a6261
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-28 13:53:46 -0800

    Establish general behavior for SiteNameList. Initial i18n keys.

--- javascript/opendcs-web-ui-react/src/pages/sites/SiteNameTypeSelect.tsx
+++ javascript/opendcs-web-ui-react/src/pages/sites/SiteNameTypeSelect.tsx
@@ -1,30 +1,33 @@
 import { FormSelect } from "react-bootstrap";
 import { REFLIST_SITE_NAME_TYPE, useRefList } from "../../contexts/data/RefListContext";
 
-
 export interface SiteNameTypeSelectProperties {
-    current: string | undefined;
+  current: string | undefined;
 }
 
-export const SiteNameTypeSelect: React.FC<SiteNameTypeSelectProperties> = ({current}) => {
-    const {refList} = useRefList();
-    const siteNameTypes = refList(REFLIST_SITE_NAME_TYPE);
-    
-    console.log(refList);
-    console.log(siteNameTypes);
+export const SiteNameTypeSelect: React.FC<SiteNameTypeSelectProperties> = ({
+  current,
+}) => {
+  const { refList } = useRefList();
+  const siteNameTypes = refList(REFLIST_SITE_NAME_TYPE);
 
-    return (
-        <FormSelect name="siteNameType">
-            {siteNameTypes.items ?
-                Object.values(siteNameTypes.items!)
-                      .map((item) => {
-                            return <option key={item.value} value={item.value} selected={current === item.value}>{item.value}</option>
-                      })
-            :
-            null
-            }
-        </FormSelect>
-    );
+  return (
+    <FormSelect name="siteNameType">
+      {siteNameTypes.items
+        ? Object.values(siteNameTypes.items!).map((item) => {
+            return (
+              <option
+                key={item.value}
+                value={item.value}
+                selected={current === item.value}
+              >
+                {item.value}
+              </option>
+            );
+          })
+        : null}
+    </FormSelect>
+  );
 };
 
 export default SiteNameTypeSelect;
