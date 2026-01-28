92082009133feea146fd4522242b8164220ad162
Michael Neilson <michael.a.neilson@usace.army.mil>
2026-01-28 13:53:46 -0800

    Establish general behavior for SiteNameList. Initial i18n keys.

--- javascript/opendcs-web-ui-react/src/pages/sites/Site.stories.tsx
+++ javascript/opendcs-web-ui-react/src/pages/sites/Site.stories.tsx
@@ -13,36 +13,35 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    site: {}
+    site: {},
   },
   play: async ({ canvas, mount, parameters }) => {
-    const {i18n} = parameters;
+    const { i18n } = parameters;
     await mount();
-    expect(canvas.queryByText(i18n.t("latitude"))).toBeInTheDocument();
-  }
+    expect(await canvas.findByText(i18n.t("latitude"))).toBeInTheDocument();
+  },
 };
 
 const site1 = {
-    siteId: 1,
-    description: "A test",
-    properties: {
-      prop1: "value1",
-    },
-    sitenames: {
-      CWMS: "Alder Springs",
-      NWSHB5: "ALS",
-    },
-  };
-
-export const WithSite: Story = {
-  args: {
-    site: site1
+  siteId: 1,
+  description: "A test",
+  properties: {
+    prop1: "value1",
+  },
+  sitenames: {
+    CWMS: "Alder Springs",
+    NWSHB5: "ALS",
   },
 };
 
+export const WithSite: Story = {
+  args: {
+    site: site1,
+  },
+};
 
 export const WithSiteAsPromise: Story = {
   args: {
-    site: Promise.resolve(site1)
-  }
-}
+    site: Promise.resolve(site1),
+  },
+};
