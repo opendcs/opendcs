8b787e43663d97660ad5c667f35e7c56476dca76
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-03 21:58:20 +0000

    Minor cleanup.

---
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/fixtures/helpers/Constants.java
@@ -0,0 +1,11 @@
+package org.opendcs.fixtures.helpers;
+
+public final class Constants
+{
+    public static final String JSESSIONID = "JSESSIONID";
+
+    private Constants() 
+    {
+        /* This utility class should not be instantiated */
+    }
+}
