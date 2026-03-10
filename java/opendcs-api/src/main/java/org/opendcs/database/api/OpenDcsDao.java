de44b6f87a6be2df604de6e019edcb1d1d342209
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-10 16:56:09 +0000

    Move organization DAO to new DAO design standards. Correct API output definition.

--- java/opendcs-api/src/main/java/org/opendcs/database/api/OpenDcsDao.java
+++ java/opendcs-api/src/main/java/org/opendcs/database/api/OpenDcsDao.java
@@ -4,6 +4,7 @@ package org.opendcs.database.api;
  * Primarily Marker interface to assist with project restructuring.
  * If 
  */
-public interface OpenDcsDao {
+public interface OpenDcsDao
+{
     
 }
