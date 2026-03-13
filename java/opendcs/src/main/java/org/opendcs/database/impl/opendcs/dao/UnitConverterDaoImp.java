5d630372f8d4c72e6c171c15177bfd684904bbe9
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-13 20:14:58 +0000

    Initial unit converter retrieve with 'prepare for exec'.

--- java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/UnitConverterDaoImp.java
+++ java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/UnitConverterDaoImp.java
@@ -6,17 +6,18 @@ import java.util.Optional;
 import org.jdbi.v3.core.Handle;
 import org.opendcs.database.api.DataTransaction;
 import org.opendcs.database.api.OpenDcsDataException;
-import org.opendcs.database.dai.UnitConversionDao;
-import org.opendcs.database.model.mappers.datatype.DataTypeMapper;
+import org.opendcs.database.dai.UnitConverterDao;
 import org.opendcs.database.model.mappers.unitconverter.UnitConverterMapper;
 import org.opendcs.utils.sql.GenericColumns;
 import org.opendcs.utils.sql.SqlErrorMessages;
+import org.openide.util.lookup.ServiceProvider;
 
 import decodes.db.EngineeringUnit;
 import decodes.db.UnitConverterDb;
 import decodes.sql.DbKey;
 
-public class UnitConverterDaoImp implements UnitConversionDao
+@ServiceProvider(service = UnitConverterDao.class)
+public class UnitConverterDaoImp implements UnitConverterDao
 {
 
     @Override
@@ -25,7 +26,13 @@ public class UnitConverterDaoImp implements UnitConversionDao
         var handle = tx.connection(Handle.class)
                        .orElseThrow(() -> new OpenDcsDataException(SqlErrorMessages.NO_JDBI_HANDLE));
         final String querySql = """
-                    select id, fromunitsabbr, tounitsabbr, algorithms, a,b,c,d,e,f from unitconverter where id =:id
+                    select uc.id, uc.fromunitsabbr, uc.tounitsabbr, uc.algorithm, uc.a,uc.b,uc.c,uc.d,uc.e,uc.f,
+                        from_eu.unitabbr from_unitabbr, from_eu.name from_name, from_eu.family from_family, from_eu.measures from_measures,
+                        to_eu.unitabbr to_unitabbr, to_eu.name to_name, to_eu.family to_family, to_eu.measures to_measures
+                      from unitconverter uc 
+                       join engineeringunit from_eu on from_eu.unitabbr = uc.fromunitsabbr
+                       join engineeringunit to_eu on to_eu.unitabbr = uc.tounitsabbr
+                      where id =:id
                 """;
         try (var query = handle.createQuery(querySql))
         {
@@ -37,13 +44,15 @@ public class UnitConverterDaoImp implements UnitConversionDao
     }
 
     @Override
-    public UnitConverterDb save(DataTransaction tx, UnitConverterDb unitConverter) throws OpenDcsDataException {
+    public UnitConverterDb save(DataTransaction tx, UnitConverterDb unitConverter) throws OpenDcsDataException
+    {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'save'");
     }
 
     @Override
-    public void delete(DataTransaction tx, DbKey id) throws OpenDcsDataException {
+    public void delete(DataTransaction tx, DbKey id) throws OpenDcsDataException
+    {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
--- java/opendcs/src/main/java/org/opendcs/database/model/mappers/unitconverter/UnitConverterMapper.java
+++ java/opendcs/src/main/java/org/opendcs/database/model/mappers/unitconverter/UnitConverterMapper.java
@@ -1,4 +1,4 @@
-package org.opendcs.database.model.mappers.datatype;
+package org.opendcs.database.model.mappers.unitconverter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -6,45 +6,88 @@ import java.sql.SQLException;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 import org.opendcs.database.model.mappers.PrefixRowMapper;
+import org.opendcs.database.model.mappers.engineeringunit.EngineeringUnitMapper;
 import org.opendcs.utils.sql.GenericColumns;
 
-import decodes.db.DataType;
+import decodes.db.Constants;
 import decodes.db.DatabaseException;
+import decodes.db.LinearConverter;
+import decodes.db.NullConverter;
+import decodes.db.Poly5Converter;
+import decodes.db.UnitConverterDb;
+import decodes.db.UsgsStdConverter;
 import decodes.sql.DbKey;
 
-public final class DataTypeMapper extends PrefixRowMapper<DataType>
+public final class UnitConverterMapper extends PrefixRowMapper<UnitConverterDb>
 {
 
-    private DataTypeMapper(String prefix)
+    private UnitConverterMapper(String prefix)
     {
         super(prefix);
     }
 
-    public static DataTypeMapper withPrefix(String prefix)
+    public static UnitConverterMapper withPrefix(String prefix)
     {
-        return new DataTypeMapper(prefix);
+        return new UnitConverterMapper(prefix);
     }
 
     @Override
-    public DataType map(ResultSet rs, StatementContext ctx) throws SQLException
+    public UnitConverterDb map(ResultSet rs, StatementContext ctx) throws SQLException
     {
         ColumnMapper<DbKey> dbKeyMapper = ctx.findColumnMapperFor(DbKey.class)
                                              .orElseThrow(() -> new SQLException("No mapper registered for DbKey class."));
+        var fromMapper = EngineeringUnitMapper.withPrefix("from");
+        var toMapper = EngineeringUnitMapper.withPrefix("to");
         final DbKey id = dbKeyMapper.map(rs, prefix + GenericColumns.ID, ctx);
-        String standard = rs.getString(prefix + "standard");
-        String code = rs.getString(prefix + "code");
-        String displayname = rs.getString(prefix + "display_name");
-        DataType dt = new DataType(standard, code);
+        String fromUnits = rs.getString(prefix + "fromunitsabbr");
+        String toUnits = rs.getString(prefix + "tounitsabbr");
+        String algorithm = rs.getString(prefix + "algorithm");
+        double a = rs.getDouble(prefix + "a");
+        double b = rs.getDouble(prefix + "b");
+        double c = rs.getDouble(prefix + "c");
+        double d = rs.getDouble(prefix + "d");
+        double e = rs.getDouble(prefix + "e");
+        double f = rs.getDouble(prefix + "f");
+        var converter = new UnitConverterDb(fromUnits, toUnits);
         try
         {
-            dt.setId(id);
+            converter.setId(id);
+            converter.algorithm = algorithm;
+            converter.coefficients = new double[]{a,b,c,d,e,f};
+            
+            var from = fromMapper.map(rs, ctx);
+            var to = toMapper.map(rs, ctx);
+
+
+            if (algorithm.equalsIgnoreCase(Constants.eucvt_none))
+            {
+    			converter.execConverter = new NullConverter(from, to);
+            }
+            else if (algorithm.equalsIgnoreCase(Constants.eucvt_linear))
+            {
+                converter.execConverter = new LinearConverter(from, to);
+            }
+            else if (algorithm.equalsIgnoreCase(Constants.eucvt_usgsstd))
+            {
+                converter.execConverter = new UsgsStdConverter(from, to);
+            }
+            else if (algorithm.equalsIgnoreCase(Constants.eucvt_poly5))
+            {
+                converter.execConverter = new Poly5Converter(from, to);
+            }
+            else
+            {
+                // Possibly some user-custom converter. Try to use an enum
+                // to construct the class dynamically.
+                // TBD
+            }
+		    converter.execConverter.setCoefficients(converter.coefficients);
         }
         catch (DatabaseException ex)
         {
-            throw new SQLException("Unable to set id field on newly created DataType", ex);
+            throw new SQLException("Unable create unit converter", ex);
         }
-        dt.setDisplayName(displayname);
-        return dt;
+        return converter;
     }
     
 }
