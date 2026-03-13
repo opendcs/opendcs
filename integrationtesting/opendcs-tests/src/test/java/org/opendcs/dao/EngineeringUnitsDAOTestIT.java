7a35d42c529d8eb71b5e3b0d9fab6eda6ee657df
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-13 14:48:08 +0000

    Update API to use new DAO.

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/EngineeringUnitsDAOTestIT.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/EngineeringUnitsDAOTestIT.java
@@ -1,5 +1,6 @@
 package org.opendcs.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,7 +9,11 @@ import org.opendcs.database.api.OpenDcsDatabase;
 import org.opendcs.database.dai.EngineeringUnitDao;
 import org.opendcs.fixtures.AppTestBase;
 import org.opendcs.fixtures.annotations.ConfiguredField;
+import org.opendcs.fixtures.annotations.EnableIfSql;
 
+import decodes.db.EngineeringUnit;
+
+@EnableIfSql
 class EngineeringUnitsDAOTestIT extends AppTestBase
 {
     @ConfiguredField
@@ -33,6 +38,57 @@ class EngineeringUnitsDAOTestIT extends AppTestBase
         {
             var units = unitDao.getEngineeringUnits(tx, -1, -1);
             assertFalse(units.isEmpty());
+        }
+    }
+
+    /**
+     * While normally I'd prefer to avoid such a contrieved test, this
+     * seems better than needing to remember and update exactly which units end up where.
+     * @throws Exception
+     */
+    @Test
+    void test_unit_pagination() throws Exception
+    {
+        var unitDao = db.getDao(EngineeringUnitDao.class).orElseThrow();
+        try (var tx = db.newTransaction())
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var unit = new EngineeringUnit(String.format("#aaaa.%03d", i), String.format("#aaaa,%03d", i), "univ", "nothing");
+                unitDao.save(tx, unit);
+            }
+
+            var unitsAll = unitDao.getEngineeringUnits(tx, 20, 0);
+            assertFalse(unitsAll.isEmpty());
+            assertEquals("#aaaa.019", unitsAll.get(19).getAbbr(), () -> {
+                return String.join(",", unitsAll.stream().map(Object::toString).toList());
+            });
+
+            var firstHalf = unitDao.getEngineeringUnits(tx, 10, 0);
+            assertEquals("#aaaa.009", firstHalf.get(9).getAbbr());
+
+            var secondHalf = unitDao.getEngineeringUnits(tx, 10, 10);
+            assertEquals("#aaaa.019", secondHalf.get(9).getAbbr());
+
+            tx.rollback();
+        }
+    }
+
+    @Test
+    void test_create_delete() throws Exception
+    {
+        var unitIn = new EngineeringUnit("flurgl", "Flurgle", "TotallyFake", "Length");
+        var unitDao = db.getDao(EngineeringUnitDao.class).orElseThrow();
+        try (var tx = db.newTransaction())
+        {
+            var unitOut = unitDao.save(tx, unitIn);
+            assertEquals(unitIn, unitOut);
+
+
+            unitDao.delete(tx, unitIn.getAbbr());
+
+            var unitShouldNotExist = unitDao.lookup(tx, unitIn.getName());
+            assertFalse(unitShouldNotExist.isPresent());
         }
     }
 }
--- java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/EngineeringUnitDaoImp.java
+++ java/opendcs/src/main/java/org/opendcs/database/impl/opendcs/dao/EngineeringUnitDaoImp.java
@@ -5,14 +5,19 @@ import java.util.Optional;
 
 import org.jdbi.v3.core.Handle;
 import org.opendcs.database.api.DataTransaction;
+import org.opendcs.database.api.DatabaseEngine;
 import org.opendcs.database.api.OpenDcsDataException;
 import org.opendcs.database.dai.EngineeringUnitDao;
+import org.opendcs.database.model.mappers.datatype.DataTypeMapper;
 import org.opendcs.database.model.mappers.engineeringunit.EngineeringUnitMapper;
+import org.opendcs.utils.sql.GenericColumns;
 import org.opendcs.utils.sql.SqlErrorMessages;
 import org.opendcs.utils.sql.SqlKeywords;
 import org.openide.util.lookup.ServiceProvider;
 
+import decodes.db.DatabaseException;
 import decodes.db.EngineeringUnit;
+import decodes.sql.DbKey;
 
 import static org.opendcs.utils.sql.SqlQueries.addLimitOffset;
 
@@ -22,15 +27,43 @@ public class EngineeringUnitDaoImp implements EngineeringUnitDao
     @Override
     public EngineeringUnit save(DataTransaction tx, EngineeringUnit unit) throws OpenDcsDataException
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'save'");
+        var handle = tx.connection(Handle.class)
+                       .orElseThrow(() -> new OpenDcsDataException(SqlErrorMessages.NO_JDBI_HANDLE));
+        var ctx = tx.getContext();
+        var dbEngine = ctx.getDatabase();
+        
+        final String insertSql = """
+                merge into engineeringunit eu
+                using (select :unitabbr unitabbr, :name name, :family family, :measures measures <dual>) input
+                on (eu.unitabbr = input.unitabbr)
+                when matched then
+                    update set name = input.name, family = input.family, measures = input.measures
+                when not matched then
+                    insert(unitabbr, name, family, measures)
+                    values(input.unitabbr, input.name, input.family, input.measures)
+                """;
+        try (var query = handle.createUpdate(insertSql)
+                               .define("dual", dbEngine == DatabaseEngine.ORACLE ? "from dual" : ""))
+        {
+            query.bind("unitabbr", unit.getAbbr())
+                 .bind(GenericColumns.NAME, unit.getName())
+                 .bind("family", unit.getFamily())
+                 .bind("measures", unit.getMeasures())
+                 .execute();
+            return lookup(tx, unit.abbr).orElseThrow(() -> new OpenDcsDataException("Unable to retrieve Engineering Unit we just saved."));
+        }
     }
 
     @Override
     public void delete(DataTransaction tx, String unitAbbreviation) throws OpenDcsDataException
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        var handle = tx.connection(Handle.class)
+                       .orElseThrow(() -> new OpenDcsDataException(SqlErrorMessages.NO_JDBI_HANDLE));
+        final var deleteDataTypeSql = "delete from engineeringunit where unitabbr = :unitabbr";
+        try (var deleteDataType = handle.createUpdate(deleteDataTypeSql))
+        {
+            deleteDataType.bind("unitabbr", unitAbbreviation).execute();
+        }
     }
 
     @Override
@@ -62,9 +95,11 @@ public class EngineeringUnitDaoImp implements EngineeringUnitDao
         final String querySql = """
                     select unitabbr, name, family, measures 
                       from engineeringunit
+                      order by unitabbr <collate> asc
                       <limit>
+
                 """;
-            
+        var dbType = tx.getContext().getDatabase();
         try (var query = handle.createQuery(querySql))
         {
             if (limit != -1)
@@ -77,6 +112,7 @@ public class EngineeringUnitDaoImp implements EngineeringUnitDao
                 query.bind(SqlKeywords.OFFSET, offset);
             }
             return query.define("limit", addLimitOffset(limit, offset))
+                        .define("collate", dbType == DatabaseEngine.POSTGRES ? "COLLATE \"C\"" : "COLLATE BINARY")
                         .registerRowMapper(EngineeringUnit.class, EngineeringUnitMapper.withPrefix(""))
                         .mapTo(EngineeringUnit.class)
                         .list();
