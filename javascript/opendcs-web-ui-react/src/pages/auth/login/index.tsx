787881042f9759dcb683327b5c0d47aa8be021b1
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-19 20:35:56 +0000

    initial shift to seperate login type components.

--- javascript/opendcs-web-ui-react/src/pages/auth/login/index.tsx
+++ javascript/opendcs-web-ui-react/src/pages/auth/login/index.tsx
@@ -6,11 +6,13 @@ import {
   ApiOrganization,
   Credentials,
   RESTAuthenticationAndAuthorizationApi,
+  User,
 } from "opendcs-api";
 import { useApi } from "../../../contexts/app/ApiContext";
 import { useOrganizations } from "../../../contexts/app/OrganizationsContext";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import FormLogin from "./FormLogin";
 
 export default function Login() {
   const { t } = useTranslation();
@@ -19,43 +21,13 @@ export default function Login() {
   const { setUser } = useAuth();
   const { organizations } = useOrganizations();
   const api = useApi();
-  var errorMsg = "";
-  const auth = new RESTAuthenticationAndAuthorizationApi(api.conf);
+  let errorMsg = "";
 
-  function handleLogin(event: FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
-    event.stopPropagation();
-    const form = event.currentTarget;
-
-    const formData = new FormData(form);
-
-    const dataObject = Object.fromEntries(formData.entries());
-    // Convert FormData to a plain object for easier use
-    const credentials: Credentials = {
-      username: dataObject.username.toString(),
-      password: dataObject.password.toString(),
-    };
-
-    const orgString: string = dataObject.organization.toString();
-    const orgObj: ApiOrganization = orgString
-      ? (JSON.parse(orgString) as ApiOrganization)
-      : {};
-    const org: string = orgObj.name || "";
-
-    auth
-      .postCredentials(org, credentials)
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      .then((user_value: any) => {
-        setUser(user_value);
-        api.setOrg(orgObj);
-        const redirectPath = location.state?.from || "/platforms";
-        navigate(redirectPath, { replace: true });
-      })
-      .catch((error_: { toString: () => string }) => {
-        setShowErrorModal(true);
-        errorMsg = error_.toString();
-      });
-  }
+  // const orgString: string = dataObject.organization.toString();
+  // const orgObj: ApiOrganization = orgString
+  //   ? (JSON.parse(orgString) as ApiOrganization)
+  //   : {};
+  // const org: string = orgObj.name || "";
 
   const [showErrorModal, setShowErrorModal] = useState(false);
 
@@ -70,47 +42,18 @@ export default function Login() {
               <h4 className="mt-3 fw-semibold">OpenDCS</h4>
               <p className="text-muted small">{t("login")}</p>
             </div>
-            <Form onSubmit={handleLogin} className="fade-in second">
-              <Form.Group className="mb-3">
-                <Form.Label className="small fw-medium">{t("username")}</Form.Label>
-                <Form.Control
-                  type="text"
-                  id="username"
-                  required
-                  name="username"
-                  placeholder={t("username")}
-                />
-              </Form.Group>
-              <Form.Group className="mb-4">
-                <Form.Label className="small fw-medium">{t("password")}</Form.Label>
-                <Form.Control
-                  type="password"
-                  id="password"
-                  required
-                  name="password"
-                  placeholder={t("password")}
-                />
-              </Form.Group>
-              {organizations.length > 0 ? (
-                <Form.Group className="mb-3">
-                  <Form.Label>{t("organization")}</Form.Label>
-                  <Form.Select id="organization" name="organization" required>
-                    {organizations.map((org) => (
-                      <option key={org.name} value={JSON.stringify(org)}>
-                        {org.name}
-                      </option>
-                    ))}
-                  </Form.Select>
-                </Form.Group>
-              ) : (
-                <input type="hidden" name="organization" value="" />
-              )}
-              <div className="d-grid fade-in third">
-                <Button variant="primary" type="submit" className="py-2">
-                  {t("login")}
-                </Button>
-              </div>
-            </Form>
+            <FormLogin
+              success={(user: User, org: ApiOrganization) => {
+                setUser(user);
+                api.setOrg(org);
+                const redirectPath = location.state?.from || "/platforms";
+                navigate(redirectPath, { replace: true });
+              }}
+              failure={(error_: { toString: () => string }) => {
+                setShowErrorModal(true);
+                errorMsg = error_.toString();
+              }}
+            />
           </Card.Body>
         </Card>
         <Modal show={showErrorModal} onHide={() => setShowErrorModal(false)} centered>
