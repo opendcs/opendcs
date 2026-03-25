01416c9f7f2c5f2f164a251beb7e2a1cf7f5eec8
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-03-25 17:58:02 +0000

    Don't request external resources until actually required.

--- java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/oidc/OidcIdentityProvider.java
+++ java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/oidc/OidcIdentityProvider.java
@@ -33,11 +33,7 @@ import com.nimbusds.jose.proc.BadJOSEException;
 import com.nimbusds.jwt.JWTClaimsSet;
 
 import decodes.sql.DbKey;
-import io.swagger.v3.oas.models.security.OAuthFlow;
-import io.swagger.v3.oas.models.security.OAuthFlows;
-import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 public final class OidcIdentityProvider implements IdentityProvider
@@ -66,15 +62,7 @@ public final class OidcIdentityProvider implements IdentityProvider
         this.clientSecret = (String)configMap.get("clientSecret");
         this.clientId = (String)configMap.get("clientId");
         this.redirectUri = (String)configMap.get("redirectUri");
-        try
-        {
-            this.oidcConfig = new OpenIdConfiguration(URI.create((String)configMap.get("wellKnown")));
-        }
-        catch (IOException ex)
-        {
-            throw new OidcConfigurationException("Unable to process well known configuration for provider " + name, ex);
-        }
-        
+        this.oidcConfig = new OpenIdConfiguration(URI.create((String)configMap.get("wellKnown")));
     }
 
     @Override
@@ -132,12 +120,12 @@ public final class OidcIdentityProvider implements IdentityProvider
           .append("redirect_uri=").append(URLEncoder.encode(redirectUri, StandardCharsets.UTF_8))
         ;
 
-        var request = HttpRequest.newBuilder(oidcConfig.tokenUri)
+        var request = HttpRequest.newBuilder(oidcConfig.getTokenUri())
                                 .header("Content-Type", "application/x-www-form-urlencoded")
                                 .POST(HttpRequest.BodyPublishers.ofString(sb.toString()))
                                 .build();
-        
-        
+
+
         try (var client = HttpClient.newBuilder()
                                     .sslContext(WebUtility.sslContext(WebUtility.TRUST_EXISTING_CERTIFICATES))
                                     .build())
@@ -199,8 +187,8 @@ public final class OidcIdentityProvider implements IdentityProvider
 
     private JWTClaimsSet verifyTokenAndGetClaims(OpenIdConfiguration config, String token) throws MalformedURLException, BadJOSEException, ParseException, JOSEException
     {
-        var keySource = JWKSourceBuilder.create(config.jwksUri.toURL()).retrying(true).build();
-        return JwtVerifier.getInstance().getClaimsSet(keySource, token, oidcConfig.issuer);
+        var keySource = JWKSourceBuilder.create(config.getJwksUri().toURL()).retrying(true).build();
+        return JwtVerifier.getInstance().getClaimsSet(keySource, token, oidcConfig.getIssuer());
     }
 
     @Override
@@ -209,7 +197,7 @@ public final class OidcIdentityProvider implements IdentityProvider
     {
         /* unable, and can update credentials will return false */
     }
-    
+
     @Override
     public SecurityScheme getSecurityScheme()
     {
--- java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/oidc/OpenIdConfiguration.java
+++ java/opendcs/src/main/java/org/opendcs/authentication/identityprovider/impl/oidc/OpenIdConfiguration.java
@@ -17,25 +17,108 @@ public final class OpenIdConfiguration
     private static final Logger log = OpenDcsLoggerFactory.getLogger();
 
     public final URI wellKnownUri;
-    public final String issuer;
-    public final URI authUri;
-    public final URI tokenUri;
-    public final URI userInfoUri;
-    public final URI logoutUri;
-    public final URI jwksUri;
+    private String issuer = null;
+    private URI authUri;
+    private URI tokenUri;
+    private URI userInfoUri;
+    private URI logoutUri;
+    private URI jwksUri;
 
-    public OpenIdConfiguration(URI wellKnown) throws IOException
+    /**
+     * Establish the required set of well known configuraiton data.
+     * Data will not be retrieved from the URL until use is requested.
+     * @param wellKnown
+     */
+    public OpenIdConfiguration(URI wellKnown)
     {
         this.wellKnownUri = wellKnown;
+    }
 
+    /**
+     * Get the defined issuer
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public String getIssuer()
+    {
+        retrieveData();
+        return issuer;
+    }
+
+    /**
+     *
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public URI getAuthUri()
+    {
+        retrieveData();
+        return authUri;
+    }
+
+    /**
+     *
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public URI getTokenUri()
+    {
+        retrieveData();
+        return tokenUri;
+    }
+
+    /**
+     *
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public URI getUserInfoUri()
+    {
+        retrieveData();
+        return userInfoUri;
+    }
+
+    /**
+     *
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public URI getJwksUri()
+    {
+        retrieveData();
+        return jwksUri;
+    }
+
+    /**
+     *
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    public URI getLogoutUri()
+    {
+        retrieveData();
+        return logoutUri;
+    }
+
+    /**
+     * Retrieve and processed the well-known configuration data
+     * @return
+     * @throws OidcConfigurationException if the well known data could not be processed.
+     */
+    private void retrieveData()
+    {
+        if (issuer != null)
+        {
+            return;
+        }
         HttpURLConnection http = null;
         try
         {
             http = (HttpURLConnection)wellKnownUri.toURL().openConnection();
             http.setRequestMethod("GET");
-            http.setInstanceFollowRedirects(true);            
-            http.setConnectTimeout(5_000); 
-            http.setReadTimeout(5_000);   
+            http.setInstanceFollowRedirects(true);
+            http.setConnectTimeout(5_000);
+            http.setReadTimeout(5_000);
             int status = http.getResponseCode();
             if (status == 200)
             {
@@ -54,7 +137,11 @@ public final class OpenIdConfiguration
                 throw new IOException("Unable to retrieve OpenIdConnect configuration");
             }
         }
-        finally 
+        catch (IOException ex)
+        {
+            throw new OidcConfigurationException("Unable to retrieve or process required OpenID information", ex);
+        }
+        finally
         {
             if (http != null)
             {
@@ -62,6 +149,4 @@ public final class OpenIdConfiguration
             }
         }
     }
-
-    
 }
