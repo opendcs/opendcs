410f1d5499a927f0e9b26dcc9b07a7699abd083a
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-04-07 17:34:48 +0000

    Some additional work to handle the conversions.

--- java/opendcs/src/main/java/org/opendcs/utils/properties/conversion/PropertyConverter.java
+++ java/opendcs/src/main/java/org/opendcs/utils/properties/conversion/PropertyConverter.java
@@ -1,7 +1,12 @@
 package org.opendcs.utils.properties.conversion;
 
+import java.util.ServiceLoader;
+
 public interface PropertyConverter<T>
 {
+
+    Class<T> getType();
+
     /**
      * Given a string value of a property, converter to the required Type.
      * @param value
@@ -15,4 +20,22 @@ public interface PropertyConverter<T>
      * @return
      */
     String toString(T value);
+
+
+    @SuppressWarnings("unchecked") // type is specifically checked manually.
+    static <T> PropertyConverter<T> forType(Class<T> type)
+    {
+        PropertyConverter<T> ret = null;
+        final var converters = ServiceLoader.load(PropertyConverter.class);
+        for (var converter: converters)
+        {
+            if (converter.getType().equals(type))
+            {
+                ret = converter;
+                break;
+            }
+        }
+
+        return ret;
+    }
 }
