cb98f019a4ab71f29674747aee79dfea30018118
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-04-14 16:11:43 +0000

    Add another property usage, with a different type.

---
+++ java/opendcs/src/main/java/org/opendcs/utils/properties/conversion/BooleanPropertyConverter.java
@@ -0,0 +1,30 @@
+package org.opendcs.utils.properties.conversion;
+
+import ilex.util.TextUtil;
+
+
+/**
+ * Uses TextUtil to allow multiple truty values {@see TextUtil.str2boolean}
+ * but always uses "true" and "false" on output.
+ */
+public final class BooleanPropertyConverter implements PropertyConverter<Boolean>
+{
+
+    @Override
+    public Class<Boolean> getType()
+    {
+        return Boolean.class;
+    }
+
+    @Override
+    public Boolean fromString(String value)
+    {
+       return TextUtil.str2boolean(value);
+    }
+
+    @Override
+    public String toString(Boolean value)
+    {
+        return Boolean.TRUE.equals(value) ? "true" : "false";
+    }
+}
