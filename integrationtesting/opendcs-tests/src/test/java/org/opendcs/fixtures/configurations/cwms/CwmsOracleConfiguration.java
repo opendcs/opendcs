8606eb1075de593dc0c1a20144fae0e59c59b042
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-04-24 19:07:54 +0000

    Correct schema image default reference.

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/fixtures/configurations/cwms/CwmsOracleConfiguration.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/fixtures/configurations/cwms/CwmsOracleConfiguration.java
@@ -40,7 +40,7 @@ public class CwmsOracleConfiguration implements Configuration
 
     private static final String CWMS_ORACLE_IMAGE = System.getProperty("opendcs.cwms.oracle.image","ghcr.io/hydrologicengineeringcenter/cwms-database/cwms/database-ready-ora-23.5:25-07-01-RC06");
     private static final String CWMS_ORACLE_VOLUME = System.getProperty("opendcs.cwms.oracle.volume","cwms_opendcs_volume");
-    private static final String CWMS_SCHEMA_IMAGE = System.getProperty("opendcs.cwms.schema.image","ghcr.io/hydrologicengineeringcenter/cwms-database/cwms/database-ready-ora-23.5:25-07-01-RC06");
+    private static final String CWMS_SCHEMA_IMAGE = System.getProperty("opendcs.cwms.schema.image","ghcr.io/hydrologicengineeringcenter/cwms-database/cwms/schema_installer:25-07-01-RC06");
 
     public static final String NAME = "CWMS-Oracle";
 
