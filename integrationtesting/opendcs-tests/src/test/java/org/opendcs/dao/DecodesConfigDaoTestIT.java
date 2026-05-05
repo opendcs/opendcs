35ac81fada060c91eff4ce8c7d1af91e5ae80383
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-05-05 17:25:58 +0000

    Basic pagination test. Cleanup.

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/DecodesConfigDaoTestIT.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/dao/DecodesConfigDaoTestIT.java
@@ -7,13 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import org.junit.jupiter.api.Test;
-import org.opendcs.database.api.DataTransaction;
 import org.opendcs.database.api.OpenDcsDatabase;
-import org.opendcs.database.dai.DataTypeDao;
 import org.opendcs.database.dai.DecodesConfigDao;
 import org.opendcs.fixtures.AppTestBase;
 import org.opendcs.fixtures.annotations.ConfiguredField;
@@ -22,11 +17,10 @@ import org.opendcs.fixtures.annotations.EnableIfTsDb;
 
 import decodes.db.ConfigSensor;
 import decodes.db.Constants;
+import decodes.db.DataType;
 import decodes.db.DecodesScript;
 import decodes.db.FormatStatement;
 import decodes.db.PlatformConfig;
-import decodes.db.ScriptSensor;
-import decodes.db.UnitConverterDb;
 
 @EnableIfTsDb
 @DecodesConfigurationRequired({
@@ -36,15 +30,9 @@ import decodes.db.UnitConverterDb;
 })
 class DecodesConfigDaoTestIT extends AppTestBase
 {
-
-    private static final String[] SENSORS = new String[]{"Stage", "Elev", "Flow"};
-    private static final String[] UNITS = new String[]{"in", "ft", "cfs"};
-
     @ConfiguredField
     OpenDcsDatabase db;
 
-    // this random sources needs to be repeatable, hence static seed.
-    private static final Random RANDOM = new Random(30); // NOSONAR
 
 
     @Test
@@ -85,9 +73,6 @@ class DecodesConfigDaoTestIT extends AppTestBase
         var decodesConfigDao = db.getDao(DecodesConfigDao.class)
                                  .orElseThrow();
 
-        var dataTypeDao = db.getDao(DataTypeDao.class)
-                            .orElseThrow();
-
         try (var tx = db.newTransaction())
         {
             final var pcIn = new PlatformConfig("TestConfig-1");
@@ -97,7 +82,7 @@ class DecodesConfigDaoTestIT extends AppTestBase
             sensor1.recordingInterval = 0;
             sensor1.recordingMode = Constants.recordingModeFixed;
             sensor1.setProperty("cwmsInterval", "15Minutes");
-            sensor1.addDataType(dataTypeDao.lookup(tx, "CWMS", "Stage").orElseThrow());
+            sensor1.addDataType(DataType.getDataType("CWMS", "Stage"));
             pcIn.addSensor(sensor1);
             final var script = DecodesScript.empty()
                                             .platformConfig(pcIn)
@@ -127,9 +112,6 @@ class DecodesConfigDaoTestIT extends AppTestBase
         var decodesConfigDao = db.getDao(DecodesConfigDao.class)
                                  .orElseThrow();
 
-        var dataTypeDao = db.getDao(DataTypeDao.class)
-                            .orElseThrow();
-
         try (var tx = db.newTransaction())
         {
             var all = decodesConfigDao.getAll(tx, -1, -1);
@@ -139,101 +121,6 @@ class DecodesConfigDaoTestIT extends AppTestBase
             assertEquals(1, onlyOne.size());
             assertEquals("OKVI4", onlyOne.getFirst().getName());
             assertEquals("ST", onlyOne.getFirst().decodesScripts.getFirst().scriptName);
-
-            final var numConfigs = 30;
-
-            final var savedConfigs = new ArrayList<PlatformConfig>();
-
-            for (var i = 0; i < numConfigs; i++)
-            {
-                final var name = String.format("0000-Config-%02d", i);
-
-                var pc = createConfig(name, RANDOM.nextInt(3), RANDOM.nextInt(2), RANDOM.nextInt(3), (i % 7) == 0, tx, dataTypeDao);
-                var pcOut = decodesConfigDao.save(tx, pc);
-                savedConfigs.add(pcOut);
-            }
-
-            final var allSaved = decodesConfigDao.getAll(tx, numConfigs, 0);
-            assertEquals(numConfigs, allSaved.size());
-
-
-            final var first10 = decodesConfigDao.getAll(tx, 10, 0);
-            assertEquals(savedConfigs.getFirst().configName, first10.getFirst().configName);
-            assertEquals(savedConfigs.get(9).configName, first10.getLast().configName);
-
-            final var second10 = decodesConfigDao.getAll(tx, 10, 10);
-            assertEquals(savedConfigs.get(10).configName, second10.getFirst().configName);
-            assertEquals(savedConfigs.get(19).configName, second10.getLast().configName);
-
-            tx.rollback();
         }
-    }
-
-
-    private PlatformConfig createConfig(String name, int numScripts, int numStatements, int numSensors, boolean sensorProperties, DataTransaction tx, DataTypeDao dataTypeDao) throws Exception
-    {
-        
-
-        var pc = new PlatformConfig(name);
-
-        for (var i = 0; i < numSensors; i++)
-        {
-            var sensor = new ConfigSensor(pc, i + 1);
-            sensor.sensorName = SENSORS[RANDOM.nextInt(SENSORS.length)];
-            sensor.recordingInterval = 900;
-            sensor.recordingMode = 'F';
-            sensor.timeOfFirstSample = 0;
-            sensor.addDataType(dataTypeDao.lookup(tx, "CWMS", sensor.sensorName)
-                                          .orElseGet(() -> fail("DataType for " + sensor.sensorName + " doesn't exist.")));
-            if (sensorProperties)
-            {
-                sensor.setProperty("CwmsInterval", "15Minutes");
-            }
-            pc.addSensor(sensor);
-        }
-
-        for (var i = 0; i < numScripts; i++)
-        {
-            var script = DecodesScript.empty()
-                                      .platformConfig(pc)
-                                      .scriptName(String.format("S-%d", i))
-                                      .withDataOrder('A')
-                                      .build();
-            for (var j = 0; j < numStatements; j++)
-            {
-                var fs = new FormatStatement(script, j + 1);
-                fs.label = "STMT" + j;
-                fs.format = String.format("/%dx", j);
-                script.getFormatStatements().add(fs);
-            }
-
-            for (var k = 0; k < numSensors; k++)
-            {
-                var scriptSensor = new ScriptSensor(script, k + 1);
-
-                final var sensorName = pc.getSensor(k + 1).sensorName;
-                int idx = 0;
-                for (var sensorIdx = 0; sensorIdx < SENSORS.length; sensorIdx++)
-                {
-                    if (SENSORS[sensorIdx].equals(sensorName))
-                    {
-                        idx = sensorIdx;
-                        break;
-                    }
-                }
-                var toUnits = UNITS[idx];
-
-                scriptSensor.rawConverter = new UnitConverterDb("raw", toUnits);
-                scriptSensor.rawConverter.algorithm = Constants.eucvt_none;
-
-                script.addScriptSensor(scriptSensor);
-            }
-
-
-
-            pc.addScript(script);
-        }
-
-        return pc;
     }
 }
--- java/opendcs/src/main/java/org/opendcs/database/dai/DecodesConfigDao.java
+++ java/opendcs/src/main/java/org/opendcs/database/dai/DecodesConfigDao.java
@@ -10,13 +10,53 @@ import org.opendcs.database.api.OpenDcsDataException;
 import decodes.db.PlatformConfig;
 import decodes.sql.DbKey;
 
+/**
+ * Access to Decodes Configuration (Also know as PlatformConfig)
+ */
 public interface DecodesConfigDao extends OpenDcsDao
 {
+    /**
+     * Retrieve a given PlatformConfig by Id
+     * @param tx
+     * @param id
+     * @return
+     * @throws OpenDcsDataException
+     */
     Optional<PlatformConfig> getById(DataTransaction tx, DbKey id) throws OpenDcsDataException;
+
+    /**
+     * Retrieve a given PlatformConfig by Name
+     * @param tx
+     * @param id
+     * @return
+     * @throws OpenDcsDataException
+     */
     Optional<PlatformConfig> getByName(DataTransaction tx, String id) throws OpenDcsDataException;
 
+    /**
+     * Save or update a platform config
+     * @param tx
+     * @param config PlatformConfiguration that will be saved as-is to the database.
+     * @return a NEW instance of PlatformConfig containing all of the elements, and the generated identifier. Never null
+     * @throws OpenDcsDataException
+     */
     PlatformConfig save(DataTransaction tx, PlatformConfig config) throws OpenDcsDataException;
+
+    /**
+     * Delete a PlatformConfig from the database, including all sensors and scripts.
+     * @param tx
+     * @param id
+     * @throws OpenDcsDataException
+     */
     void delete(DataTransaction tx, DbKey id) throws OpenDcsDataException;
 
+    /**
+     * Retrieve all, or desired limit and offset, Decodes Configurations from the database.
+     * @param tx
+     * @param limit
+     * @param offset
+     * @return
+     * @throws OpenDcsDataException
+     */
     List<PlatformConfig> getAll(DataTransaction tx, int limit, int offset) throws OpenDcsDataException;
 }
