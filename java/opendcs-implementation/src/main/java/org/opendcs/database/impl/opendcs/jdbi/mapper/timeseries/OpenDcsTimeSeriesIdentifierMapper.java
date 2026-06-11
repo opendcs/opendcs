48ee7601c8f65b28bbcd85602b6891cff7406e38
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-06-11 16:08:25 +0000

    Update Mapper to use Enum system.

--- java/opendcs-implementation/src/main/java/org/opendcs/database/impl/opendcs/jdbi/mapper/timeseries/OpenDcsTimeSeriesIdentifierMapper.java
+++ java/opendcs-implementation/src/main/java/org/opendcs/database/impl/opendcs/jdbi/mapper/timeseries/OpenDcsTimeSeriesIdentifierMapper.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.jdbi.v3.core.mapper.ColumnMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 import org.opendcs.database.model.mappers.PrefixRowMapper;
+import org.opendcs.database.sql.TableColumnDefinition;
 import org.opendcs.utils.sql.GenericColumns;
 import org.opendcs.utils.sql.SqlErrorMessages;
 
@@ -15,12 +16,12 @@ import decodes.sql.DbKey;
 import decodes.tsdb.TimeSeriesIdentifier;
 import opendcs.opentsdb.OffsetErrorAction;
 
-public class OpenDcsTimeSeriesIdentifierMapper extends PrefixRowMapper<TimeSeriesIdentifier>
+public class OpenDcsTimeSeriesIdentifierMapper extends PrefixRowMapper<TimeSeriesIdentifier,OpenDcsTimeSeriesIdentifierMapper.Columns>
 {
 
     protected OpenDcsTimeSeriesIdentifierMapper(String prefix)
     {
-        super(prefix);
+        super(prefix, Columns.class);
     }
 
     @Override
@@ -31,27 +32,63 @@ public class OpenDcsTimeSeriesIdentifierMapper extends PrefixRowMapper<TimeSerie
         ColumnMapper<Date> dateMapper = ctx.findColumnMapperFor(Date.class)
                                 .orElseThrow(() -> new SQLException(SqlErrorMessages.TIME_MAPPER_NOT_FOUND));
         var ret = new CwmsTsId();
-        ret.setKey(columnMapperForKey.map(rs, prefix + GenericColumns.ID, ctx));
-        ret.setUniqueString(rs.getString(prefix + "unique_string"));
-        ret.setSiteName(rs.getString(prefix + "site_name"));
-        ret.setDescription(rs.getString(prefix + GenericColumns.DESCRIPTION));
-        ret.setActive(rs.getBoolean(prefix + "active_flag"));
-        ret.setStorageTable(rs.getInt(prefix + "storage_table"));
-        ret.setStorageType(rs.getString(prefix + "storage_type").charAt(0));
-        ret.setStorageUnits(rs.getString(prefix + "storage_units"));
-        ret.setInterval(rs.getString(prefix + "interval"));
-        ret.setDuration(rs.getString(prefix + "duration"));
-        ret.setLastModified(dateMapper.map(rs, prefix + "modify_time", ctx));
-        ret.setUtcOffset(rs.getInt(prefix + "utc_offset"));
-        ret.setAllowDstOffsetVariation(rs.getBoolean(prefix + "allow_dst_offset_variation"));
-        ret.setOffsetErrorAction(OffsetErrorAction.fromString(rs.getString(prefix + "offset_error_action")));
+        ret.setKey(columnMapperForKey.map(rs, column(Columns.ID), ctx));
+        ret.setUniqueString(rs.getString(column(Columns.UNIQUE_STRING)));
+        ret.setSiteName(rs.getString(column(Columns.SITE_NAME)));
+        ret.setDescription(rs.getString(column(Columns.DESCRIPTION)));
+        ret.setActive(rs.getBoolean(column(Columns.ACTIVE_FLAG)));
+        ret.setStorageTable(rs.getInt(column(Columns.STORAGE_TABLE)));
+        ret.setStorageType(rs.getString(column(Columns.STORAGE_TYPE)).charAt(0));
+        ret.setStorageUnits(rs.getString(column(Columns.STORAGE_UNITS)));
+        ret.setInterval(rs.getString(column(Columns.INTERVAL)));
+        ret.setDuration(rs.getString(column(Columns.DURATION)));
+        ret.setLastModified(dateMapper.map(rs, column(Columns.MODIFY_TIME), ctx));
+        ret.setUtcOffset(rs.getInt(column(Columns.UTC_OFFSET)));
+        ret.setAllowDstOffsetVariation(rs.getBoolean(column(Columns.ALLOW_DST_OFFSET_VARIATION)));
+        ret.setOffsetErrorAction(OffsetErrorAction.fromString(rs.getString(column(Columns.OFFSET_ERROR_ACTION))));
 
         return ret;
     }
-    
 
     public static OpenDcsTimeSeriesIdentifierMapper withPrefix(String prefix)
     {
         return new OpenDcsTimeSeriesIdentifierMapper(prefix);
+    }
+
+    public enum Columns implements TableColumnDefinition
+    {
+        ID(GenericColumns.ID),
+        UNIQUE_STRING("unique_string"),
+        SITE_NAME("site_name"),
+        DESCRIPTION(GenericColumns.DESCRIPTION),
+        ACTIVE_FLAG("active_flag"),
+        STORAGE_TABLE("storage_table"),
+        STORAGE_TYPE("storage_type"),
+        STORAGE_UNITS("storage_units"),
+        INTERVAL("interval"),
+        DURATION("duration"),
+        MODIFY_TIME("modify_time"),
+        UTC_OFFSET("utc_offset"),
+        ALLOW_DST_OFFSET_VARIATION("allow_dst_offset_variation"),
+        OFFSET_ERROR_ACTION("offset_error_action")
+        ;
+
+        private final String column;
+
+        Columns(String column)
+        {
+            this.column = column;
+        }
+
+        Columns(GenericColumns other)
+        {
+            this.column = other.column();
+        }
+
+        @Override
+        public String column()
+        {
+            return column;
+        }
     }
 }
