dec5b959b89e55068ce234e438993a765a731eac
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-06-15 21:15:28 +0000

    use fully qualified class name.

--- java/opendcs-api/src/main/java/org/opendcs/operations/OpenDcsOperations.java
+++ java/opendcs-api/src/main/java/org/opendcs/operations/OpenDcsOperations.java
@@ -6,12 +6,12 @@ package org.opendcs.operations;
  * Such as Transforming Time Series Identifiers for 
  * group computations.
  * 
- * Like a DAO Implementation methods <b>MAY</b> require a valid {@link DataTransaction}
+ * Like a DAO Implementation methods <b>MAY</b> require a valid {@link org.opendcs.database.api.DataTransaction}
  * to perform the operations. However, it is expected that results say in
  * memory and that these will not <b>save</b> data to the database.
  * 
  * Operations that are declare as "performs no database I/O" must <b>NOT</b>
- * provide a {@link DataTransaction} or any other method to perform database I/O
+ * provide a {@link org.opendcs.database.api.DataTransaction} or any other method to perform database I/O
  */
 public interface OpenDcsOperations
 {
