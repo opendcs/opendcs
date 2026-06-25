825aa68655d442261c326e695d5daadba3fbd71c
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-06-25 21:09:39 +0000

    Full and optional elemnents behave as expected.

--- java/opendcs-implementation/src/main/java/org/opendcs/database/impl/opendcs/dao/PlatformDaoImpl.java
+++ java/opendcs-implementation/src/main/java/org/opendcs/database/impl/opendcs/dao/PlatformDaoImpl.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.statement.SqlLogger;
 import org.jdbi.v3.core.statement.StatementContext;
+import org.jdbi.v3.stringtemplate4.StringTemplateEngine;
 import org.opendcs.annotations.api.InjectDao;
 import org.opendcs.database.api.DataTransaction;
 import org.opendcs.database.api.OpenDcsDataException;
@@ -58,7 +59,7 @@ public class PlatformDaoImpl implements PlatformDao
             select
                 <platform_columns>,
                 <medium_columns>
-                <config_columns>
+                <if(config_columns)><config_columns><endif>
                 <site_columns>
                 <site_name_columns>
                 <site_props>
@@ -72,8 +73,8 @@ public class PlatformDaoImpl implements PlatformDao
             <site_name_join>
             <site_props_join>
             <platform_props_join>
-            <platform_sensor_join>
-            <platform_sensor_property_join>
+            <if(platform_sensor_join)><platform_sensor_join><endif>
+            <platform_sensor_props_join>
 
             order by p.mediumtype <collate> asc, p.mediumid <collate> asc, p.platformdesignator <collate> asc
             """;
@@ -103,6 +104,7 @@ public class PlatformDaoImpl implements PlatformDao
         var dbEngine = ctx.getDatabaseEngine();
         try (var select = handle.createQuery(SELECT_SQL))
         {
+            select.setTemplateEngine(new StringTemplateEngine());
             var platformMapper = PlatformMapper.withPrefix("p");
             var configMapper = DecodesConfigMapper.withPrefix("config");
             var mediumMapper = TransportMediumMapper.withPrefix("tm");
@@ -130,7 +132,7 @@ public class PlatformDaoImpl implements PlatformDao
                                                                               "p", PlatformMapper.Columns.SITE_ID.column()))
                   .define("site_props_join", "left outer join site_property sp on sp.site_id = p.siteid")
                   .define("platform_props_join", "left outer join platformproperty pp on pp.platformid = p.id")
-                  .define("platform_sensors_join", platformSensorMapper.joinStatement(
+                  .define("platform_sensor_join", platformSensorMapper.joinStatement(
                     "left outer", PlatformSensorMapper.Columns.PLATFORM_ID, "p", PlatformMapper.Columns.ID.column()))
                   .define("platform_sensor_props_join", platformSensorPropertiesMapper.joinStatement(
                     "left outer", PlatformSensorPropertyMapper.Columns.PLATFORM_ID, "p", PlatformMapper.Columns.ID.column()))
@@ -156,6 +158,8 @@ public class PlatformDaoImpl implements PlatformDao
                          .registerRowMapper(mediumMapper)
                          .registerRowMapper(siteMapper)
                          .registerRowMapper(siteNameMapper)
+                         .registerRowMapper(platformSensorMapper)
+                         .registerRowMapper(platformSensorPropertiesMapper)
                          .registerRowMapper(PropertiesMapper.withPrefix("sp", true))
                          .registerRowMapper(PlatformReducer.PLATFORM_PROPERTIES, platformPropsMapper)
                          .bind("mediumtype", mediumType)
@@ -188,7 +192,34 @@ public class PlatformDaoImpl implements PlatformDao
     public List<Platform> getAll(DataTransaction tx, int limit, int offset, boolean fillAll, String mediumType)
             throws OpenDcsDataException
     {
-        return List.of();
+        var handle = tx.connection(Handle.class)
+                       .orElseThrow(() -> new OpenDcsDataException(SqlErrorMessages.NO_JDBI_HANDLE));
+        var ctx = tx.getContext();
+        var dbEngine = ctx.getDatabaseEngine();
+        try (var select = handle.createQuery(SELECT_SQL))
+        {
+            select.setTemplateEngine(new StringTemplateEngine());
+            var platformMapper = PlatformMapper.withPrefix("p");
+            var mediumMapper = TransportMediumMapper.withPrefix("tm");
+            select.define("platform_columns", platformMapper.columnsForSelect())
+                  .define("medium_columns", mediumMapper.columnsForSelect())
+                  .define(COLLATE_CLAUSE, collateClauseFor(dbEngine));
+            if (mediumType != null && !mediumType.isBlank())
+            {
+                select.define("medium_filter", " and tm.mediumtype = :mediumtype")
+                      .define(WHERE_CLAUSE, "where mediumtype = :mediumtype")
+                      .bind("mediumtype", mediumType);
+            }
+            else
+            {
+                select.define("medium_filter", "").define(WHERE_CLAUSE,"");
+            }
+
+            return select.registerRowMapper(platformMapper)
+                         .registerRowMapper(mediumMapper)
+                         .reduceRows(new PlatformReducer(platformMapper, null, null))
+                         .toList();
+        }
     }
 
 }
