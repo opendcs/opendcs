a8e218aec568a212d089b0148298daec49bcdd41
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2026-07-15 10:52:28 -0500

    Add DCPMon mock-backed UI tests

--- javascript/dcpmon-web-ui/eslint.config.js
+++ javascript/dcpmon-web-ui/eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "public/mockServiceWorker.js"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
