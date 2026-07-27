0d6b4fba734936ad2324f5dce48bc85bf8fd34d5
Mike Neilson <michael.a.neilson@usace.army.mil>
2026-07-27 12:01:14 -0700

    Strip out the vast majority of the Swing UI. Remaining elements are s… (#2097)
    …till required, at least partially, by the LRGS UIs.
    
    ## Problem Description
    
    <!-- if your PR does not address an open issue you can remove this line
    -->
    Fixes #2096 (mostly). 
    
    <!-- if you are referencing a specific issue a problem description is
    not required -->
    Describe the problem you are trying to solve.
    
    ## Solution
    
    Remove bulk of Swing CODE.
    Some elements were still used by the LRGS UI elements that have no "web"
    replacement at this time.
    
    ## how you tested the change
    
    Existing integration tests. LRGS components were not verified to still
    operate during this PR.
    May go do so later.
    
    ## Where the following done:
    
    - [ ] Tests. Check all that apply:
       - [ ] Unit tests created or modified that run during ant test.
    - [ ] Integration tests created or modified that run during integration
    testing
             (Formerly called regression tests.)
       - [ ] Test procedure descriptions for manual testing
    - [ ] Was relevant documentation updated?
    - [ ] Were relevant config element (e.g. XML data) updated as
    appropriate
    
    If you aren't sure leave unchecked and we will help guide you to want
    needs changing where.

--- java/opendcs/src/main/java/decodes/dbeditor/PlatformListPanel.java
+++ java/opendcs/src/main/java/decodes/dbeditor/PlatformListPanel.java
@@ -26,6 +26,7 @@ import org.slf4j.Logger;
 import ilex.util.LoadResourceBundle;
 
 import decodes.gui.*;
+import decodes.util.DecodesSettings;
 import decodes.db.*;
 
 /**
@@ -34,17 +35,19 @@ Displays a sorting-list of Platform objects in the database.
 public class PlatformListPanel extends JPanel implements ListOpsController
 {
 	private static final Logger log = OpenDcsLoggerFactory.getLogger();
-	static ResourceBundle dbeditLabels = DbEditorFrame.getDbeditLabels();
+	static ResourceBundle dbeditLabels =  
+		LoadResourceBundle.getLabelDescriptions(
+                "decodes/resources/dbedit",
+				DecodesSettings.instance().language
+			);
 
 	ListOpsPanel listOpsPanel;
 	JLabel jLabelTitle = new JLabel();
 	PlatformSelectPanel platformSelectPanel;
-	DbEditorFrame parent;
 	int newIndex = 1;
 
 	/** Constructor. */
 	public PlatformListPanel() {
-		parent = null;
 		listOpsPanel = new ListOpsPanel(this);
 		listOpsPanel.enableCopy(true);
 		try 
@@ -54,16 +57,6 @@ public class PlatformListPanel extends JPanel implements ListOpsController
 		{
 			GuiHelpers.logGuiComponentInit(log, ex);
 		}
-	}
-
-	/**
-	 * Sets the parent frame object. Each list panel needs to know this.
-	 * 
-	 * @param parent the DbEditorFrame
-	 */
-	void setParent(DbEditorFrame parent) 
-	{
-		this.parent = parent;
 	}
 
 	/** Initializes GUI components. */
@@ -82,108 +75,13 @@ public class PlatformListPanel extends JPanel implements ListOpsController
 	/** @return type of entity that this panel edits. */
 	public String getEntityType() { return "Platform"; }
 
-
-	/** Called when the 'Open' button is pressed. */
-	public void openPressed() 
-	{
-		Platform p = platformSelectPanel.getSelectedPlatform();
-		if (p == null)
-		{
-			TopFrame.instance().showError(
-					dbeditLabels.getString("PlatformListPanel.selectOpen"));
-		} 
-		else 
-		{
-			try 
-			{
-				p.read();
-				doOpen(p);
-			} 
-			catch (Exception ex) 
-			{
-				log.atError().setCause(ex).log("Unable to read platform {}", p.makeFileName());
-				TopFrame.instance().showError(
-						LoadResourceBundle.sprintf(
-								dbeditLabels.getString("PlatformListPanel.cannotOpen"),
-								p.makeFileName(), ex.toString()));
-			}
-		}
-	}
-
-	/** Called when the 'New' button is pressed. */
-	public void newPressed()
-	{
-		doNew();
-	}
-
-	public PlatformEditPanel doNew()
-	{
-		Platform ob = new Platform();
-		platformSelectPanel.addPlatform(ob);
-		return doOpen(ob);
-	}
-
-	/** Called when the 'Copy' button is pressed. */
-	public void copyPressed()
-	{
-		Platform p = platformSelectPanel.getSelectedPlatform();
-		if (p == null)
-		{
-			TopFrame.instance().showError(
-					dbeditLabels.getString("PlatformListPanel.selectCopy"));
-		} 
-		else
-		{
-			if (!p.isComplete())
-			{
-				try { p.read(); }
-				catch (DatabaseException ex)
-				{
-					log.atError().setCause(ex).log("Unable to copy platform {}", p.makeFileName());
-					TopFrame.instance().showError(
-							LoadResourceBundle.sprintf(
-									dbeditLabels.getString("PlatformListPanel.cannotRead"),
-									p.makeFileName(), ex.toString()));
-					return;
-				}
-			}
-
-			Platform newOb = p.noIdCopy();
-			platformSelectPanel.addPlatform(newOb);
-			doOpen(newOb);
-		}
-	}
-
 	/** Called when the 'Delete' button is pressed. */
 	public void deletePressed() {
 		Platform ob = platformSelectPanel.getSelectedPlatform();
 		if (ob == null)
+		{
 			TopFrame.instance().showError(
 					dbeditLabels.getString("PlatformListPanel.selectDelete"));
-		else {
-			DbEditorTabbedPane platformsTabbedPane = parent.getPlatformsTabbedPane();
-			DbEditorTab tab = platformsTabbedPane.findEditorFor(ob);
-			if (tab != null) {
-				TopFrame.instance().showError(
-						dbeditLabels.getString("PlatformListPanel.platformEdited"));
-				return;
-			}
-			int ok = JOptionPane.showConfirmDialog(this,
-					dbeditLabels.getString("PlatformListPanel.confirmDelete"),
-					dbeditLabels.getString("PlatformListPanel.confirmDeleteTitle"),
-					JOptionPane.YES_NO_OPTION);
-			if (ok == JOptionPane.YES_OPTION) {
-				platformSelectPanel.deletePlatform(ob);
-				try 
-				{
-					Database.getDb().platformList.write();
-				} catch (DatabaseException ex) 
-				{
-					String msg = dbeditLabels.getString("PlatformListPanel.errorDeleting");
-					log.atError().setCause(ex).log(msg);
-					TopFrame.instance().showError(msg + ex);
-				}
-			}
 		}
 	}
 
@@ -192,43 +90,7 @@ public class PlatformListPanel extends JPanel implements ListOpsController
 	{
 	}
 
-	/**
-	  Opens a PlatformEditPanel for the passed platform.
-	  @param ob the object to be edited.
-	  @return the PlatformEditPanel opened.
-	 */
-	public PlatformEditPanel doOpen(Platform ob)
-	{
-		DbEditorTabbedPane tp = parent.getPlatformsTabbedPane();
-		DbEditorTab tab = tp.findEditorFor(ob);
-		if (tab != null)
-		{
-			tp.setSelectedComponent(tab);
-			return (PlatformEditPanel)tab;
-		}
-		else
-		{
-			PlatformEditPanel newTab = new PlatformEditPanel(ob);
-			newTab.setParent(parent);
-			String title = ob.getPreferredTransportId();
-			if (title.equals("unknown"))
-				title = "New DCP " + (newIndex++);
-			tp.add(title, newTab);
-			tp.setSelectedComponent(newTab);
-			return newTab;
-		}
-	}
 
-	/**
-	  After saving, and edit panel will need to replace the old object
-	  with the newly modified one. It calls this method to do this.
-	  @param oldp the old object
-	  @param newp the new object
-	 */
-	public void replacePlatform(Platform oldp, Platform newp)
-	{
-		platformSelectPanel.replacePlatform(oldp, newp);
-	}
 
 	/**
 	 * Called if a new platform is abandoned before it was ever saved.
@@ -236,6 +98,24 @@ public class PlatformListPanel extends JPanel implements ListOpsController
 	 */
 	void abandonNewPlatform(Platform p)
 	{
-		platformSelectPanel.deletePlatform(p);
+		throw new UnsupportedOperationException("Unimplemented method 'abandonNewPlatform'");
+	}
+
+	@Override
+	public void openPressed()
+	{
+		throw new UnsupportedOperationException("Unimplemented method 'openPressed'");
+	}
+
+	@Override
+	public void newPressed()
+	{
+		throw new UnsupportedOperationException("Unimplemented method 'newPressed'");
+	}
+
+	@Override
+	public void copyPressed() 
+	{
+		throw new UnsupportedOperationException("Unimplemented method 'copyPressed'");
 	}
 }
--- java/opendcs/src/main/java/decodes/tsdb/procmonitor/ProcMonitorException.java
+++
@@ -1,9 +0,0 @@
-package decodes.tsdb.procmonitor;
-
-public class ProcMonitorException extends Exception
-{
-	public ProcMonitorException(String msg)
-	{
-		super(msg);
-	}
-}
