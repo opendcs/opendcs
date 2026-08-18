aaa2c7cfded85a5a21a2bac97d4a06365c410fdc
Mike Neilson <michael.a.neilson@usace.army.mil>
2026-08-18 14:03:54 -0700

    Task/1882 complockdao (#2135)
    ## Problem Description
    
    <!-- if your PR does not address an open issue you can remove this line
    -->
    Fixes #1882. 
    
    ## Solution
    
    Implement CompLock operations in stateless DAO.
    Return `Result` over throwing an exception. Point of usage can throw the
    exception if needed.
    NOTE: copy the implementation directly from the existing LoadingAppDao,
    could likely share the logic, but don't see the point as once the new
    one is used everywhere we'll just delete it.
    
    ## how you tested the change
    
    Existing tests of REST API
    New integration test.
    
    
    ## Where the following done:
    
    - [ ] Tests. Check all that apply:
       - [ ] Unit tests created or modified that run during ant test.
    - [ ] Integration tests created or modified that run during integration
    testing
             (Formerly called regression tests.)
       - [ ] Test procedure descriptions for manual testing
    - [ ] Was relevant documentation updated?
    - [ ] Were relevant config element (e.g. XML data) updated as
    appropriate
    
    If you aren't sure leave unchecked and we will help guide you to want
    needs changing where.
    
    ---------
    
    Co-authored-by: Karl Tarbet <ktarbet@users.noreply.github.com>

--- integrationtesting/opendcs-tests/src/test/java/org/opendcs/odcsapi/res/it/AppResourcesIT.java
+++ integrationtesting/opendcs-tests/src/test/java/org/opendcs/odcsapi/res/it/AppResourcesIT.java
@@ -27,10 +27,11 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.path.json.JsonPath;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import opendcs.dai.LoadingAppDAI;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opendcs.database.api.OpenDcsDataException;
+import org.opendcs.database.dai.CompLockDao;
 import org.opendcs.odcsapi.beans.ApiAppStatus;
 import org.opendcs.odcsapi.filters.W3CTraceFilter;
 import org.opendcs.utils.logging.OpenDcsLoggerFactory;
@@ -331,7 +332,6 @@ final class AppResourcesIT extends BaseApiIT
 		assertNotNull(actual.get("appId"), "Application status was not returned.");
 		// ID not compared as we don't actually care what it is.
 		assertEquals(host, actual.get("hostname"));
-		assertEquals(expected.get("eventPort"), (Integer)actual.get("eventPort"));
 		assertEquals(expected.get("status"), (String)actual.get("status"));
 		assertEquals(expected.get("appName"), (String)actual.get("appName"));
 		assertEquals(expected.get("pid"), (Integer)actual.get("pid"));
@@ -357,23 +357,41 @@ final class AppResourcesIT extends BaseApiIT
 		;
 	}
 
-	TsdbCompLock getCompLock(CompAppInfo compAppInfo, int pid, String host) throws DatabaseException
+	TsdbCompLock getCompLock(CompAppInfo compAppInfo, int pid, String host) throws Exception
 	{
-		try (LoadingAppDAI dai = getTsdb().makeLoadingAppDAO())
+		try
 		{
-			return dai.obtainCompProcLock(compAppInfo, pid, host);
+			var db = getConfig().getOpenDcsDatabase();
+			var dao = db.getDao(CompLockDao.class).orElseThrow();
+			try (var tx = db.newTransaction())
+			{
+				var lock = dao.obtainLock(tx, compAppInfo, pid, host);
+				if (lock.isSuccess())
+				{
+					return lock.success();
+				}
+				else
+				{
+					throw lock.failure();
+				}
+			}
 		}
 		catch (Throwable thr)
 		{
-			throw new DatabaseException("Error getting comp lock", thr);
+			throw new OpenDcsDataException("Error getting comp lock", thr);
 		}
 	}
 
-	void releaseCompLock(TsdbCompLock compLock) throws DatabaseException
+	void releaseCompLock(TsdbCompLock compLock) throws Exception
 	{
-		try (LoadingAppDAI dai = getTsdb().makeLoadingAppDAO())
+		try
 		{
-			dai.releaseCompProcLock(compLock);
+			var db = getConfig().getOpenDcsDatabase();
+			var dao = db.getDao(CompLockDao.class).orElseThrow();
+			try (var tx = db.newTransaction())
+			{
+				dao.releaseLock(tx, compLock);
+			}
 		}
 		catch (Throwable thr)
 		{
