e111f7f5e7eb77943f50dd38cc01cb0554435521
Michael A. Neilson <michael.a.neilson@usace.army.mil>
2026-08-21 14:48:44 +0000

    Update user_roles table constraints with new primary key.

--- schema/opendcs-schemas-cwms-oracle/src/main/resources/db/CWMS-Oracle/schema/V7.6__roles_by_office.sql
+++ schema/opendcs-schemas-cwms-oracle/src/main/resources/db/CWMS-Oracle/schema/V7.6__roles_by_office.sql
@@ -1,26 +1,26 @@
-alter table ${CCP_SCHEMA}.user_roles 
+alter table ${CCP_SCHEMA}.user_roles
 add (office_code number default 0 not null);
--- cannot add foreign key reference to table.
+-- cannot add foreign key reference to table due to permissions.
 
-create unique index user_roles_pk_new_idx
+create unique index office_user_roles_pk_idx
 on ${CCP_SCHEMA}.user_roles (office_code, user_id, role_id)
 invisible online;
 
+-- now drop the old primary key and put the new one in place
 declare
     index_name varchar2(256);
     constraint_name varchar2(256);
 begin
     select index_name, constraint_name into index_name, constraint_name
-      from dba_constraints 
-     where constraint_type = 'P' 
+      from dba_constraints
+     where constraint_type = 'P'
        and lower(table_name) = 'user_roles'
        and owner='${flyway:defaultSchema}';
 
-    execute immediate 'alter table ${CCP_SCHEMA}.user_roles rename constraint ' || constraint_name || 
-                      ' to user_roles_pk';
-    execute immediate 'alter table ${CCP_SCHEMA}.user_roles modify constraint user_roles_pk using index user_roles_pk_new_idx';
+    execute immediate 'alter table ${CCP_SCHEMA}.user_roles drop constraint ' || constraint_name;
 
-    execute immediate 'alter index user_roles_pk_new_idx visible';
-    execute immediate 'alter index ' || index_name || ' invisible' ;
-    execute immediate 'drop index ' || index_name;
+    execute immediate 'alter table ${CCP_SCHEMA}.user_roles add constraint office_user_roles_pk ' ||
+                      'primary key (office_code, user_id, role_id) using index office_user_roles_pk_idx';
+
+    execute immediate 'alter index office_user_roles_pk_idx visible';
 end;
